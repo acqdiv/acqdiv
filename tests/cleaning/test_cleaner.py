6898459926a70cd1cc78b945c21cc8ca690b7be6
Steven Moran <bambooforest@gmail.com>
2015-03-27 20:46:23 +0100

    more spring cleaning

--- tests/cleaning/test_cleaner.py
+++
@@ -1,8 +0,0 @@
-import unittest
-from cleaning2.scripts.clean import Cleaner
-
-class CleanerTestCase(unittest.TestCase):
-    """ Tests for cleaner.py """
-    
-    def setUp(self):
-        self.c = Cleaner("../../cleaning2/input/inuktitut/rules/inuktitut.conf")
