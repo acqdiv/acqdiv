7c2c7bf97471285420091174cc22a22c2aa3d9aa
Steven Moran <bambooforest@gmail.com>
2015-03-27 20:39:04 +0100

    removing old cleaning directory and contents

--- cleaning/scripts/encodings.sh
+++
@@ -1,5 +0,0 @@
-# Get encoding -- make sure to pass the input directory/* when calling this script:
-# sh encodings.sh ../inuktitut/test/*
-file $1 > file.log
-file -i $1 > file-i.log
-chardetect $1 > chardetect.log
