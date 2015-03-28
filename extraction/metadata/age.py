3f0e71ff044a5fa6b0add4e6bbb3a63026f8950e
lingdp <p.danica@gmail.com>
2015-03-28 14:58:52 +0100

    changed output of ages.py to chat date format

--- extraction/metadata/age.py
+++ extraction/metadata/age.py
@@ -1,10 +1,16 @@
 from datetime import datetime
+import dateutil.parser
+from dateutil.relativedelta import relativedelta
 
-def days_between(d1, d2):
+def childes_age_format(d1, d2):
     d1 = datetime.strptime(d1, "%Y-%m-%d")
     d2 = datetime.strptime(d2, "%Y-%m-%d")
-    return abs((d2 - d1).days)
+    #return abs((d2 - d1).days)
+    diff = relativedelta(d2, d1)
+    return("%d;%d.%d" % (diff.years, diff.months, diff.days))
 
 if __name__ == "__main__":
     import sys
-    print(days_between(sys.argv[1], sys.argv[2]))
+    print(childes_age_format(sys.argv[1], sys.argv[2]))
+
+
