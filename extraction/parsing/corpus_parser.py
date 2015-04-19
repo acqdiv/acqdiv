df9dac23ff9a38b9df06e5e8bf8daeb94b6a2548
lingdp <p.danica@gmail.com>
2015-04-19 22:46:07 +0200

    added script for parser testing and updated json goldstandard output files for Indonesian, Inuktitut, Russian and Sesotho.

--- extraction/parsing/corpus_parser.py
+++ extraction/parsing/corpus_parser.py
@@ -24,29 +24,52 @@ else:
 
 # table with subdirectory and format for each corpus
 corpus_dic = {
-    # 'Cree' : {'dir' : 'Cree/test/', 'format' : 'XML'},
-    # 'Japanese_MiiPro' : {'dir' : 'Japanese_MiiPro/test/', 'format' : 'XML'},
-    # 'Japanese_Miyata' : {'dir' : 'Japanese_Miyata/test/', 'format' : 'XML'},
-    # 'Sesotho' : {'dir' : 'Sesotho/test/', 'format' : 'XML'},
+     'Cree' : {'dir' : 'Cree/test/', 'format' : 'XML'},
+     'Japanese_MiiPro' : {'dir' : 'Japanese_MiiPro/test/', 'format' : 'XML'},
+     'Japanese_Miyata' : {'dir' : 'Japanese_Miyata/test/', 'format' : 'XML'},
+     'Sesotho' : {'dir' : 'Sesotho/test/', 'format' : 'XML'},
     'Inuktitut' : {'dir' : 'Inuktitut/test/', 'format' : 'XML'},
-    # 'Turkish_KULLD' : {'dir' : 'Turkish_KULLD/test/', 'format' : 'XML'},
-    # 'Chintang' : {'dir' : 'Chintang/test/', 'format' : 'Toolbox'},
-    # 'Indonesian' : {'dir' : 'Indonesian/test/', 'format' : 'Toolbox'},
-    # 'Russian' : {'dir' : 'Russian/test/', 'format' : 'Toolbox'},
-    # 'Yucatec' : {'dir' : 'Yucatec/test/', 'format' : 'XML'}
+     'Turkish_KULLD' : {'dir' : 'Turkish_KULLD/test/', 'format' : 'XML'},
+     'Chintang' : {'dir' : 'Chintang/test/', 'format' : 'Toolbox'},
+     'Indonesian' : {'dir' : 'Indonesian/test/', 'format' : 'Toolbox'},
+     'Russian' : {'dir' : 'Russian/test/', 'format' : 'Toolbox'},
+     'Yucatec' : {'dir' : 'Yucatec/test/', 'format' : 'XML'}
 }    
 
-if not os.path.exists('corpora_processed/parsed'):
-    os.mkdir('corpora_processed/parsed')
-
-
-# parse corpora using functions from corpus_parser_functions
-for corpus_name in corpus_dic:
-    corpus_dic[corpus_name]['dir'] = rootdir + corpus_dic[corpus_name]['dir']
-    corpus_object = parse_corpus(corpus_name, corpus_dic[corpus_name]['dir'], corpus_dic[corpus_name]['format'])        
+def parser(corpus_name):
+    rootdir='corpora/'
     
-    with open('corpora_processed/parsed/' + corpus_name + '.json', 'w') as file:
-        json.dump(corpus_object, file, ensure_ascii=False)
-    with open('corpora_processed/parsed/' + corpus_name + '_prettyprint.txt', 'w') as file:
-        # careful, sort_keys=True can cause memory errors with bigger corpora such as Japanese_MiiPro
-        print(json.dumps(corpus_object, sort_keys=True, indent=4, ensure_ascii=False), file=file)
+    if not os.path.exists('corpora_processed/parsed'):
+        os.mkdir('corpora_processed/parsed')
+    
+    
+    # parse corpora using functions from corpus_parser_functions
+    if corpus_name in corpus_dic:
+        corpus_dic[corpus_name]['dir'] = rootdir + corpus_dic[corpus_name]['dir']
+        corpus_object = parse_corpus(corpus_name, corpus_dic[corpus_name]['dir'], corpus_dic[corpus_name]['format'])        
+        
+        with open('corpora_processed/parsed/' + corpus_name + '.json', 'w') as file:
+            json.dump(corpus_object, file, ensure_ascii=False, sort_keys=True)
+
+            
+#def parser_main(corpus_name):
+#    rootdir='corpora/'
+#    if not os.path.exists('corpora_processed/parsed'):
+#        os.mkdir('corpora_processed/parsed')
+#    
+#    
+#    # parse corpora using functions from corpus_parser_functions
+#    if corpus_name in corpus_dic:
+#        corpus_dic[corpus_name]['dir'] = rootdir + corpus_dic[corpus_name]['dir']
+#        corpus_object = parse_corpus(corpus_name, corpus_dic[corpus_name]['dir'], corpus_dic[corpus_name]['format'])        
+#        
+#        with open('corpora_processed/parsed/' + corpus_name + '.json', 'w') as file:
+#            json.dump(corpus_object,file, ensure_ascii=False, sort_keys=True)
+#        with open('corpora_processed/parsed/' + corpus_name + '_prettyprint.txt', 'w') as file:
+#            # careful, sort_keys=True can cause memory errors with bigger corpora such as Japanese_MiiPro
+#            print(json.dumps(corpus_object, sort_keys=True, indent=4, ensure_ascii=False), file=file)
+        
+if __name__ == '__main__':
+    corpora_to_parse = ['Inuktitut', 'Russian', 'Sesotho', 'Indonesian']
+    for corpus in corpora_to_parse:
+        parser(corpus)
---
+++ tests/test_parsing.py
@@ -0,0 +1,88 @@
+import unittest
+import codecs
+import os
+import sys
+os.chdir('../acqdiv') ## make script available from main dir of repository
+sys.path.append("extraction/parsing") 
+#from extraction.parsing.corpus_parser_test import parser  ## why doesn't this work (even if I have __init__.py everywhere)??
+from corpus_parser import parser
+
+'''
+test script that can be run with nosetests to compare goldstandard json files with the
+generated json output files from the parser
+
+usage (from main folder of repo): $ nosetests tests/test_parsing.py
+
+ATTENTION: os.chdir() above has to be adapted individually in this script!!
+
+'''
+def files_to_compare(output, gold):
+        with codecs.open(output, "r", "utf-8") as output:
+            output = output.read()
+        with codecs.open(gold, "r", "utf-8") as goldfile:
+            gold = goldfile.read()
+        return(output, gold)
+
+
+class Test_Parser(unittest.TestCase):
+    maxDiff = None #for printing large output
+    
+    def setUp(self):
+        pass
+    
+    
+    ##PROBLEM: Cree, Japanese (both) and Turkish generate UnicodeDecodeError?? How to solve that?
+    
+    
+    
+    def test_inuktitut(self):
+        '''test if Inuktitut output is correct.'''
+        parser("Inuktitut")
+        output,gold = files_to_compare("corpora_processed/parsed/Inuktitut.json", "tests/parsing_updated/Inuktitut.json")
+        self.assertEqual(output, gold)
+    
+    def test_indonesian(self):
+        '''test if Indonesian output is correct.'''
+        parser("Indonesian")
+        output,gold = files_to_compare("corpora_processed/parsed/Indonesian.json", "tests/parsing_updated/Indonesian.json")
+        self.assertEqual(output, gold)
+
+    def test_russian(self):
+        '''test if Russian output is correct.'''
+        parser("Russian")
+        output,gold = files_to_compare("corpora_processed/parsed/Russian.json", "tests/parsing_updated/Russian.json")
+        self.assertEqual(output, gold)
+        
+    def test_sesotho(self):
+        '''test if Sesotho output is correct.'''
+        parser("Sesotho")
+        output,gold = files_to_compare("corpora_processed/parsed/Sesotho.json", "tests/parsing_updated/Sesotho.json")
+        self.assertEqual(output, gold)
+        
+
+    ##def test_japanese_miipro(self):
+    ##    parser("Japanese_MiiPro")
+    ##    output,gold = files_to_compare("corpora_processed/parsed/Japanese_MiiPro.json", "tests/parsing/Japanese_MiiPro.json")
+    ##    self.assertEqual(output, gold)
+    
+    ##def test_japanese_miyata(self):
+    ##    '''test if Japanese_Miyata output is correct.'''
+    ##    parser("Japanese_Miyata")
+    ##    output,gold = files_to_compare("corpora_processed/parsed/Japanese_Miyata.json", "tests/parsing/Japanese_Miyata.json")
+    ##    self.assertEqual(output, gold)
+    
+    ##def test_cree(self):
+    ##    '''test if Cree output is correct.'''
+    ##    parser("Cree")
+    ##    output,gold = files_to_compare("tests/parsing/Cree_goldstandard.xml", "tests/parsing/Cree.json")
+    ##    self.assertEqual(output, gold)
+    
+    ##def test_turkish(self):
+    ##    parser("Turkish_KULLD")
+    ##    output,gold = files_to_compare("corpora_processed/parsed/Turkish_KULLD.json", "tests/parsing/Turkish_KULLD.json")
+    ##    self.assertEqual(output, gold)
+    #
+    
+if __name__ == '__main__':
+    unittest.main()
+
