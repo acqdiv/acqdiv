fa18ce34a0c5a85a05992dd53113c02c378a363b
andreasgerster <andreas.gerster@gmail.com>
2015-05-28 15:57:46 +0200

    update code.py

--- cleaning/turkish/cfg/code.py
+++ cleaning/turkish/cfg/code.py
@@ -58,9 +58,10 @@ def clean_chat_line(s):
     s = re.sub(r"(@New Episode):\s(.+$)", r"\1\n%sit:\t\2", s)
     s = re.sub(r"#", r"(.)", s) # hashtag is equivalent to (.) which is CHAT for notation for pauses.
     s = re.sub(r"\[[X\*]\s?(\d)\]", r"[x \1]", s)
-    s = re.sub(r"([^\n])@\S+", r"\1", s) # many uses of "@" plus following code are inconsistent and are not CHAT compliant. cf. issue #86
+    # many uses of "@" (plus following code) are inconsistent and are not CHAT compliant. cf. issue #86
+    s = re.sub(r"@fp|@e|@s|@oın|@İ|@lsi|@i_ham@i_ham|@oı|@s|@ı|@fmı|@fi|@s|@i_ham@i_ha:m|@omı|@oa|@ia|@fmi|@cmı|@y|@r|@oını|@l'in|@imi|@i_ham@i_ham@i_ham@i_ham@i_ham|@fb|@eng|@eın|@oe|@m|@lyi|@imı|@ie|@:i|@fyı|@fnı|@fa|@en|@eler|@e|@sit|@si_kızını|@oü|@o_şupur|@omu|@olatmadık|@lidi|@is|@iksin|@i_hey@i_hey|@i_ham@i_ham@i_ham@i_ham@i_ham@i_ham|@i_ham@i_ham@i_ham@i_ham|@i_ham@i_ham@i_ham|@i_ham@i_ham|@i_ham@i_ha:m_ham@i_ham@i_ha:m|@ie|@i_benim_kuzum|@ì|@fmu|@fler|@elerde|@e_bye@e|@cnın|@c:e|@yu|@verme|@tıtı|@swi|@S-QUE|@simu|@si_kıvrıla|@se|@ş|@o'yi|@ou|@o_şıkır@o_şıkır|@o_miyav|@olerle|@olatmadık|@oların|@oları|@olar|@ola|@o_kırt@o_kırt@o_kırt@o_kırt|@o_kırt@o_kırt|@o_kırt|@oına|@oımız|@o_huppur|@o&hav|@o[/]hav|@o_havmı|@o_ham@o_ham@o_ham|@o_fırıl|@odan|@o_cuf@o_cuf|@ob|@ö|@mı|@lylen|@lsi|@l^si|@lni|@kurmasınlar|@köpek|@kızım|@i_uf|@i_tu|@ir|@int|@inmi|@inı|@i_ne_güzel|@imuş|@im|@iktim|@iktim|@ikmisin|@ikmisin|@ii(y)im|@iives|@i_ı@i_ı@i_ı|@i_ı@i_ı|@i_ı|@iıhı|@i_ıh|@i@ie|@iı|@ii|@ihıhı|@i_hıh|@ihıha|@i_hı|@ihı|@,ıh|@i_ham@i_ham@i_ham@i_ham@i_ham@i_ham@i_ham|@i_ham@i_ham@i_ham@i_ha:m_ham@i_ham@i_ham@i_ham@i_ha:m|@i_ham@i_ham@i_ham@i_ha:m|@i_ham@i_ham@i_ham@i_ha|@i_ham|@iğinmi|@iee|@iee|@iee|@ie|@i_benim_yy_bunu_ben_yapalım|@iama|@h|@hav|@ha|@h|@fyü|@fylamı|@fyi|@f_yapıyo(r|@fya|@ftan|@fsını|@fsini|@fsı|@f_oldu|@foldu|@fo|@fnin|@fmü|@flara|@fı|@f_ham|@fbıdı|@eye|@:e's|@erın|@en|@emi|@eların|@elar|@ede|@ea|@ea|@e|@e|@dı|@da|@cuna|@cü|@cmi|@clarmı|@c_kut:u|@c_by:e@e|@c_adamalar|@bur(a)da|@b=&laugh|@bi|@b_ga|@bak|@babbab|@ba|@[b26]i|@ai|@0", r"", s)
     s = re.sub(r"@(\s)", r"\1", s) # many uses of "@" (plus following code) are inconsistent and are not CHAT compliant. cf. issue #86
-    s = re.sub(r"([^\n])@(\W+)", r"\1\2", s)
+    s = re.sub(r"([^\n])@(\W+)", r"\1\2", s) #gets rid of "@" at the end of words
     s = re.sub(r'<(.+?)>\s\["\]', r"'\1'", s)
     s = re.sub(r'(\S+)\s\["\]', r"'\1'", s)
     s = re.sub(r'\+/([^/])', r'+//\1', s)
