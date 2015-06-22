d464c788c74760b783004dc28fb018ffeaf022c7
aguacristalina <canedo@ivs.uzh.ch>
2015-06-22 12:12:13 +0100

    updated code.py, ids.csv and docs

--- cleaning/yucatec/cfg/code.py
+++ cleaning/yucatec/cfg/code.py
@@ -23,6 +23,7 @@ def clean_chat_line(s):
     #unification *PARTICIPANT tier
     #correct participants' IDs:
     s=re.sub(r"\*MECH:", r"*MEC:", s)
+    s=re.sub(r"\*:MEC:", r"*MEC:", s)    
     s=re.sub(r"\*GOYO:", r"*GOY:", s)
     s=re.sub(r"\*PEPE:", r"*PEP:", s)
     s=re.sub(r"\*ABUE:", r"*ABU:", s)
@@ -44,6 +45,7 @@ def clean_chat_line(s):
     s=re.sub(r"\*dav\*:", r"*DAV:", s)
     s=re.sub(r"\*dav:", r"*DAV:", s)
     s=re.sub(r"\*dav", r"*DAV:", s)
+    s=re.sub(r"\*:DAV", r"*DAV:", s)
     s=re.sub(r"\*san:", r"*SAN:", s)
     s=re.sub(r"\*fil:", r"*FIL:", s)
     s=re.sub(r"\*fil", r"*FIL:", s)
@@ -54,6 +56,7 @@ def clean_chat_line(s):
     s=re.sub(r"\*mar:", r"*MAR:", s)
     s=re.sub(r"\*nef:", r"*NEF:", s)
     s=re.sub(r"\*lor:", r"*LOR:", s)
+    s=re.sub(r"\*SEÑ:", r"*UNK:", s)
     s=re.sub(r"\*:", r"*UNK:", s)
     s=re.sub(r"\*([A-Z]{3}) :", r"*\1:", s) # if there is a space between the participant's code and the colon, remove it.
     s=re.sub(r"\(\s?(\*[A-Z]{3})\s?\):", r"\1:", s) # participant codes with form e.g. "(*ARM):" or "( *SAN ):" should be transformed into "*ARM:"
@@ -108,23 +111,26 @@ def clean_chat_line(s):
     # cf. ../notes/yua-chars.ods for notes on characters that need manual attention and/or need to be interpreted by Barbara (corpus owner)
     s = re.sub(" ", " ", s) # unification of two different space types
     s=re.sub(r"¨(.*)\?", r"¿\1\?", s) # if the dieresis happens at the beginning of a line and later comes "?", then the dieresis has to be replaced by "¿"
-    s = re.sub("¢", "ó", s)
+    s=re.sub("¢", "ó", s)
+    s=re.sub("σ", "ó", s)
     s = re.sub("’", "'", s)
     s = re.sub("£", "ú", s)
     s = re.sub("\\s‘\\s", "?", s) # other uses of "‘" need manual attention
-    s = re.sub("¤", "ñ", s)
-    s=re.sub("hńn", "hnn", s)
+    s=re.sub("¤", "ñ", s)
+    s=re.sub("ρ", "ñ", s)
+    s=re.sub("с", "ñ", s) # Russian character
+    s=re.sub("hńn", "hnn", s) ########## recheck. Also "hn´n" was found. Maybe there is something else there...
     s=re.sub("ń", "ñ", s)
     s = re.sub("^.+?\\\\.+?$", "", s) # backslashes only occur in lines of jumbled characters (probably information lost from .doc to .txt)
-    s = re.sub("^.+?¸.+?$", "", s) # "¸" only occurs in lines of jumbled characters (probably information lost from .doc to .txt)
-    s = re.sub("ç", "", s)
-    s = re.sub("Æ", "'", s)
-    s = re.sub("^.+?Ø.+?$", "", s) # "Ø" only occurs in lines of jumbled characters (probably information lost from .doc to .txt)
-    s = re.sub("sÏ", "sí", s)
+    s=re.sub("ç", "", s)
+    s=re.sub("Æ", "'", s)
+    s=re.sub("sÏ", "sí", s)
     s = re.sub("ï", "'", s)
-    s = re.sub("Í", "í", s)
-    s = re.sub("ë", "é", s)
-    s = re.sub("°", "", s)
+    #s=re.sub("ë", "é", s) # only one instance. Moved to yua-manual-changes.xls
+    s=re.sub("à", "á", s)    
+    s=re.sub("è", "é", s)
+    s=re.sub("ì", "í", s)
+    #s=re.sub("°", "", s) # only two instances. Moved to yua-manual-changes.xls
     s=re.sub("ż", "¿", s)
 
     #inverted question mark
@@ -132,11 +138,18 @@ def clean_chat_line(s):
     s=re.sub(r"^%xpho:(.*)¿(.*)$", r"%xpho:\1\2", s) # not allowed in a %xpho tier
     s=re.sub(r"%xspa:(.*)¿$", r"%xspa:\1\?", s) # at the end of a %xspa tier, "¿" has to be "?"
 
-    #inverted exclamation mark
+    #inverted exclamation mark (work in progress...)
+    s=re.sub(r"¡$", r"!", s) # at the end of a line, "¡" has to be "!"
     s=re.sub("all¡", "allí", s) # these four refer to consistent errors across the corpus and they simplify the application of other rules below.
     s=re.sub("as¡", "así", s)
     s=re.sub("aqu¡", "aquí", s)
     s=re.sub("ah¡", "ahí", s)
+    s=re.sub("m¡ralo", "míralo", s)
+    s=re.sub("ma¡z", "maíz", s)
+    s=re.sub("m¡o", "mío", s)
+    s=re.sub("j¡cara", "jícara", s)
+    s=re.sub("todav¡a", "todavía", s)
+    s=re.sub("sand¡a", "sandía", s) # only two instances. Move to manual changes... ?
 
 
     #cleanup unwanted tiers
