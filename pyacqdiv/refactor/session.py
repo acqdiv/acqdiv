f4e0198db3694703a008bcc4fe8007d505ebb456
chysi <cazim.hysi@gmail.com>
2015-07-18 18:02:53 -0400

    continue on setting up infrastructure

--- pyacqdiv/refactor/session.py
+++ pyacqdiv/refactor/session.py
@@ -1,17 +1,22 @@
+import os
 import re
 
-class Session():
+class Session(object):
     """
     Session object for acqdiv corpora.
     """
 
-    def __init__(self):
+    def __init__(self, data):
         pass
 
-class SessionLoader():
+class SessionLoader(object):
     """
     Class to process the session objects contained in a corpus object.
     """
 
-    def __init__(self):
-        pass
+    def __init__(self, parse_func, proc_func, path):
+        self.parse = parse_func
+        self.process = proc_func
+        self.path = path
+        self.sessions = (Session(self.process(self.parse(f))) for f in os.walk(self.path))
+
