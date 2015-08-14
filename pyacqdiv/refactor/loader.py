113030ed86373312e32a922299eb1c351074504b
Steven Moran <bambooforest@gmail.com>
2015-08-14 14:45:07 +0200

    uncomments Cazim's code -- works fine now with Cree

--- pyacqdiv/refactor/loader.py
+++ pyacqdiv/refactor/loader.py
@@ -17,8 +17,8 @@ if __name__=="__main__":
 
     # corpora
     #configs = ['Chintang.ini', 'Cree.ini']
-    configs = ['Chintang.ini']
-    # configs = ['Cree.ini']
+    # configs = ['Chintang.ini']
+    configs = ['Cree.ini']
     for config in configs:
         # Parse the config file and call the sessions processor
         cfg = CorpusConfigParser()
