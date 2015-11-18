b9bb31b2deaef6896dbcbca36f1816713fd81024
chysi <cazim.hysi@gmail.com>
2015-11-18 16:02:01 +0100

    work on xmlreader

--- pipeline/XMLreader.py
+++ pipeline/XMLreader.py
@@ -1,7 +1,8 @@
-#a skeleton xml parser subclassing the nltk childes corpus reader, to play around with
-#see http://www.nltk.org/_modules/nltk/corpus/reader/childes.html
+# a skeleton xml parser subclassing the nltk childes corpus reader, to play around with
+# see http://www.nltk.org/_modules/nltk/corpus/reader/childes.html
 
 import re
+import json
 from collections import defaultdict
 
 from nltk.util import flatten
@@ -18,10 +19,61 @@ class ACQDIVCorpusReader(CHILDESCorpusReader):
     Class to read in Childes XML Corpora and extract ACQDIV-specific information from them.
     """
 
-    mortypes = set(['mortyp', 'actmor', 'tarmor'])
+    mortypes = set(['mortyp', 'mor', 'trn'])
 
     def __init__(self, root, fileids, lazy=True):
         super().__init__(root, fileids, lazy)
+
+    def utterances(self, fileids):
+        return concat([self._get_utts(fileid)
+            for fileid in self.abspaths(fileids)])
+
+    def _get_utts(self, fileid):
+
+        xmldoc = ElementTree.parse(fileid).getroot()
+
+        results = []
+
+        for u in xmldoc.findall('.//{%s}u' % NS):
+            d = {   'speaker':None,
+                    'words':None,
+                    'morphstring':None,
+                    'english':None,
+                    'id':None           }
+
+            words = []
+            trans = None
+            morph = None
+
+            for w in u.findall('.//{%s}w' % NS):
+                words.append(w.text)
+
+            for a in u.findall('.//{%s}a' % NS):
+                if (a.attrib.get('type')
+                        == 'extension' and a.attrib.get('flavor')
+                        in ACQDIVCorpusReader.mortypes):
+                    morph = a.text
+                elif (a.attrib.get('type') == 'coding'):
+                    morph = a.text
+                elif (a.attrib.get('type') == 'english translation'):
+                    trans = a.text
+                else:
+                    continue
+
+
+            d['speaker'] = u.attrib.get('who')
+            d['words'] = words
+            d['morphstring'] = morph
+            d['english'] = trans
+            d['id'] = u.attrib.get('uID')
+            results.append(d)
+
+        return results
+
+    def _s_and_m(self, speaker='ALL', fileids=None):
+        # :^)
+        return zip(self.sents(fileids),
+                self.morphs(fileids))
 
     def morphs(self, speaker='ALL', fileids=None):
         """
@@ -45,8 +97,11 @@ class ACQDIVCorpusReader(CHILDESCorpusReader):
             xmlmorphs = xmlsent.findall('.//{%s}a' % NS) 
 
             for xmlmor in xmlmorphs:
-                if (xmlmor.attrib.get('flavor')
+                if (xmlmor.attrib.get('type')
+                        == 'extension' and xmlmor.attrib.get('flavor')
                         in ACQDIVCorpusReader.mortypes):
+                    morph = xmlmor.text
+                elif (xmlmor.attrib.get('type') == 'coding'):
                     morph = xmlmor.text
                 else:
                     morph = ''
@@ -60,5 +115,10 @@ def test_read(corpus_dir):
     return corpus
 
 if __name__ == '__main__':
-    cree = test_read('../corpora/Cree/xml')
-    print(cree.morphs(cree.fileids()[0]))
+    corpus = test_read('../corpora/test/')
+    acqdiv = {}
+    for subcorpus in corpus.fileids():
+        scname = subcorpus.split('.')[0]
+        acqdiv[scname] = corpus.utterances(subcorpus)
+    with open('test.json', 'w') as tf:
+        json.dump(acqdiv, tf)
