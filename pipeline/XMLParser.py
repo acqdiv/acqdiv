65724632298edda1b0d443bd32a1ac17b6f713b9
chysi <cazim.hysi@gmail.com>
2015-12-02 10:49:10 +0100

    XMLParser infrastructure ideas

---
+++ pipeline/XMLParser.py
@@ -0,0 +1,17 @@
+import re
+from lxml import etree
+
+class XMLParserMaker(object):
+
+    def __init__(self):
+        pass
+
+    def __call__(self):
+        pass
+
+
+class XMLParser(object):
+    pass
+
+class CreeParser(XMLParser):
+    pass
