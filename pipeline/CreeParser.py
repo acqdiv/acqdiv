14b95cce0dcda8c84fbd147a41c0a81ccdc7a5ec
chysi <cazim.hysi@gmail.com>
2016-01-28 14:10:41 +0100

    minor changes to cree parser

--- pipeline/CreeParser.py
+++ pipeline/CreeParser.py
@@ -119,7 +119,9 @@ class CreeParser(XMLParser):
 
         words = filter(lambda w: w != None, words)
         return [{"full_word": w.text, "full_word_target": w.attrib['target'],
-            "utterance_id_fk": u.attrib.get('uID')} for w in words]
+            "utterance_id_fk": u.attrib.get('uID'), 
+            "word_id": (u.attrib.get('uID') + 'w' + str(i))} 
+            for w,i in zip(words, itertools.count())]
 
     def _get_phonetic(self, u):
 
@@ -164,6 +166,8 @@ class CreeParser(XMLParser):
             mortext = re.sub('[\(\)]', '', mortext)
             mortext = re.sub('zéro', 'Ø', mortext)
             morph['morphemes'] = mortext
+        else:
+            morph['morphemes'] = ''
 
         if morph['gloss_raw'] != None:
             gltext = morph['gloss_raw']
@@ -174,6 +178,8 @@ class CreeParser(XMLParser):
             gltext = re.sub('#|%%%|\*|(?<=\\s)\?(?=\\s)', '???', gltext)
             gltext = re.sub('zéro', 'Ø', gltext)
             morph['gloss_raw'] = gltext
+        else:
+            morph['gloss_raw'] = ''
 
         if morph['pos_raw'] != None:
             postext = morph['pos_raw']
@@ -186,11 +192,13 @@ class CreeParser(XMLParser):
             postext = re.sub('\(([^\)]+)\)', '\\1'.upper(), postext)
             postext = re.sub('zéro', 'Ø', postext)
             morph['pos_raw'] = postext
+        else:
+            morph['pos_raw'] = ''
 
         tiers = ['morphemes', 'gloss_raw', 'pos_raw']
 
-        if morph['morphemes'] == None and morph['gloss_raw'
-                ] == None and morph['pos_raw'] == None:
+        if morph['morphemes'] == '' and morph['gloss_raw'
+                ] == '' and morph['pos_raw'] == '':
             u['utterance']['warnings'] = "not glossed"
             return morph
 
@@ -204,7 +212,7 @@ class CreeParser(XMLParser):
 
                 if len(words) != len(u['words']):
                     # log error
-                    print("Alignment error! Oh no!", file=sys.stderr)
+                    #print("Alignment error! Oh no!", file=sys.stderr)
                     if u['utterance']['warnings'] != None:
                         u['utterance']['warnings'] += ("broken alignment: " 
                             "{bad_tier}".format(bad_tier=tier))
@@ -223,20 +231,47 @@ class CreeParser(XMLParser):
                     morph[tier] = mlist
 
             else:
-                morph[tier] = []
-                continue
+                morph[tier] = ['']
 
-        out = [{'morphemes':m, 'gloss_raw':g, 'pos_raw':p} for m,g,p in
-                itertools.zip_longest(morph['morphemes'], 
-                    morph['gloss_raw'], morph['pos_raw'])]
+        #out = [[{'morphemes':m, 'gloss_raw':g, 'pos_raw':p} for m,g,p in mt]
+        #        for mt in filter(lambda x: x != None, 
+        #            itertools.zip_longest(morph['morphemes'], 
+        #            morph['gloss_raw'], morph['pos_raw']))]
+
+        if u['utterance']['warnings'] == (
+        "broken alignment: morphemesbroken alignment: "
+        "gloss_rawbroken alignment: pos_raw"):
+            u['utterance']['warnings'] = "not glossed"
+
+        out = []
+        for mt,gt,pt in itertools.zip_longest(morph['morphemes'],
+                morph['gloss_raw'], morph['pos_raw'], fillvalue=""):
+            try:
+                out.append([{'morpheme':m, 'gloss_raw':g, 'pos_raw':p} for m,g,p in 
+                        itertools.zip_longest(mt,gt,pt)])
+            except TypeError:
+                print(mt,gt,pt)
+                out.append({'morphemes':None, 'gloss_raw':None, 'pos_raw':None})
 
         return out
 
     def _get_timestamps(self, u):
         ts = u.find('.//media')
-        return (ts.attrib.get('start'), ts.attrib.get('end'))
+        if ts != None:
+            return (ts.attrib.get('start'), ts.attrib.get('end'))
+        else:
+            return (None, None)
 
     def next_utterance(self):
         for u in self._get_utts():
-            u['morphology'] = self._morphology_inference(u)
+            morph = self._morphology_inference(u)
+            for w,ms in itertools.takewhile(lambda x: x[1] != None, 
+                    itertools.zip_longest(u['words'], morph)):
+                for m in ms:
+                    try:
+                        m['word_id_fk'] = w['word_id']                
+                    except TypeError:
+                        m = None
+                        continue
+            u['morphology'] = morph
             yield u['utterance'], u['words'], u['morphology']
