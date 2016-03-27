ac32e2511a020cdd41f04f27227987c26c929d9b
Anna Jancso <anna@jancso.ch>
2016-03-27 22:52:01 +0200

    get functions implemented

--- dene/resources.py
+++ dene/resources.py
@@ -1,10 +1,11 @@
+import sys, os, csv, logging, exiftool
+
 ############################
+## exiftool is used for extracting metadata of files
 ## Installation of exiftool wrapper for python:
 ## git clone git://github.com/smarnach/pyexiftool.git
 ## sudo python3 setup.py install
 ############################
-
-import sys, os, csv, logging, exiftool
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -12,7 +13,7 @@ logger.setLevel(logging.INFO)
 handler = logging.FileHandler("resources.log", mode="w")
 handler.setLevel(logging.INFO)
 
-formatter = logging.Formatter("%(object_number)d|%(levelname)s|%(message)s")
+formatter = logging.Formatter("%(levelname)s|%(message)s")
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 
@@ -28,26 +29,47 @@ class Resource:
 
     def __init__(self, path):
 
+        self.path = path
         self.media_file = os.path.split(path)[1]
         self.extension = self.media_file[-3:].upper()
 
-        self.type = self.type = Resource.media_dict[self.extension][0]
-        self.format = Resource.media_dict[self.extension][0] + "/" + self.extension
-        self.location = "Dene/Media/" + self.media_file
-        self.session_code = self.media_file[:-4]
-        self.word_size = ""
 
-        # exiftool used for extracting byte size and duration of file
+    def get_session_code(self):
+        return self.media_file[:-4]
+
+
+    def get_type(self):
+        return Resource.media_dict[self.extension][0]
+
+
+    def get_format(self):
+        return Resource.media_dict[self.extension][0] + "/" + self.extension
+
+
+    def get_duration(self):
+
         with exiftool.ExifTool() as et:
-            metadata = et.get_metadata(path)
-
-            self.byte_size = metadata["File:FileSize"]
-
-            # set duration
-            duration_in_seconds = metadata[Resource.media_dict[self.extension][1] + ":Duration"]
-            m, s = divmod(duration_in_seconds, 60)
+            # get duration in seconds
+            duration = et.get_metadata(self.path)[Resource.media_dict[self.extension][1] + ":Duration"]
+            # converting to hours, minutes and seconds
+            m, s = divmod(duration, 60)
             h, m = divmod(m, 60)
-            self.duration = "%02d:%02d:%02d" % (h, m, s)
+
+            return "%02d:%02d:%02d" % (h, m, s)
+
+
+    def get_byte_size(self):
+
+        with exiftool.ExifTool() as et:
+            return et.get_metadata(self.path)["File:FileSize"]
+
+
+    def get_word_size(self):
+        return ""
+
+
+    def get_location(self):
+        return "Dene/Media/" + self.media_file
 
 
     def in_media_dict(self):
@@ -69,11 +91,16 @@ if __name__ == "__main__":
     for media_file in os.listdir(path):
         resource = Resource(path + "/" + media_file)
 
-        writer.writerow({"Session code": resource.session_code,
-                        "Type": resource.type,
-                        "Format": resource.format,
-                        "Duration": resource.duration,
-                        "Byte size": resource.byte_size,
-                        "Word size": resource.word_size,
-                        "Location": resource.location
-                        })
+        if resource.in_media_dict():
+
+            writer.writerow({"Session code": resource.get_session_code(),
+                            "Type": resource.get_type(),
+                            "Format": resource.get_format(),
+                            "Duration": resource.get_duration(),
+                            "Byte size": resource.get_byte_size(),
+                            "Word size": resource.get_word_size(),
+                            "Location": resource.get_location()
+                            })
+
+        else:
+            logger.warning("File '" + media_file + "' has unknown file extension")
