26057e4b0fc181bb0b11704b5ce0475310d0c395
Steven Moran <bambooforest@gmail.com>
2016-04-08 15:53:16 +0200

    adds routine and checks for column not nulls

--- pipeline/tests/tests.py
+++ pipeline/tests/tests.py
@@ -45,6 +45,7 @@ def check(corpus, attr, target):
 def test_database_integrity():
     # Note: SQL "NULL" returns as Python None
 
+    # Check various value sets
     query = "select sentence_type from utterances group by sentence_type"
     gloss = ["0", "1", "2", "3", "4", "4SYL", "A", "ABIL", "ABL", "ABS", "ACC", "ACROSS", "ACT", "ADESS", "ADJ", "ADJZ", "ADN", "ADV", "ADVZ", "AFF", "AGT", "AGR", "ALL", "ALT", "AMBUL", "ANIM", "ANTIP", "AOR", "APPL", "ART", "ASP", "ASS", "ASSOC", "ATTN", "AUTOBEN", "AUX", "AV", "BABBLE", "BEN", "CAUS", "CHOS", "CLF", "CLIT", "CM", "COM", "COMP", "COMPAR", "COMPL", "CONC", "COND", "CONJ", "CONJ", "CON", "CONT", "CONTEMP", "CONTING", "CONTR", "COP", "CVB", "DAT", "DECL", "DEF", "DEICT", "DEM", "DEP", "DEPR", "DESID", "DESTR", "DET", "DETR", "DIM", "DIR", "DIR", "DIST", "DISTR", "DOWN", "DU", "DUB", "DUR", "DYN", "ECHO", "EMPH", "EQU", "ERG", "EVID", "EXCL", "EXCLA", "EXIST", "EXT", "F", "FILLER", "FOC", "FUT", "GEN", "HAB", "HES", "HHON", "HON", "HORT", "IDEOPH", "IMIT", "IMP", "IMPERS", "INAL", "INAN", "INCEP", "INCH", "INCL", "INCOMPL", "IND", "INDF", "INDIR", "INF", "INS", "INSIST", "INTJ", "INTR", "INTRG", "INV", "IPFV", "IRR", "LNK", "LOC", "M", "MED", "MHON", "MIR", "MOD", "MOOD", "MV", "N", "N", "N", "NAG", "NAME", "NC", "NEG", "NICKNAMER", "NMLZ", "NOM", "NPST", "NSG", "NTVZ", "NUM", "OBJ", "OBJVZ", "OBL", "OBLIG", "OBV", "ONOM", "OPT", "ORD", "P", "PARTIT", "PASS", "PEJ", "PERL", "PERMIS", "PERSIST", "PFV", "PL", "POL", "POSS", "POT", "PRAG", "PRED", "PREDADJ", "PREP", "PREP", "PRF", "PRO", "PROB", "PROG", "PROH", "PROP", "PROX", "PRS", "PST", "PTCL", "PTCP", "PURP", "PV", "PVB", "Q", "QUANT", "QUOT", "RECENT", "RECNF", "RECP", "REF", "REFL", "REL", "REM", "REP", "RES", "REVERS", "S", "SBJ", "SBJV", "SEQ", "SG", "SIM", "SOC", "SPEC", "STAT", "STEM", "SUPERL", "SURP", "TEASER", "TEL", "TEMP", "TENSE", "TERM", "TOP", "TR", "UP", "V", "V2", "V.AUX", "V.CAUS", "V.IMP", "V.ITR", "V.PASS", "V.POS", "V.TR", "VBZ", "VOICE", "VN", "VOC", "VOL", "WH", "???"]
     yield check_values, query, gloss
@@ -69,13 +70,57 @@ def test_database_integrity():
     macrorole = ["Adult", "Child", "Target_Child", "Unknown"]
     yield check_values, query, macrorole
 
+    # Check columns for NULL
+    query = "select count(*) from sessions where date is null"
+    yield check_null, query
+
+    query = "select count(*) from sessions where media is null"
+    yield check_null, query
+
+    query = "select count(*) from utterance where speaker_label is null"
+    yield check_null, query
+
+    query = "select count(*) from utterance where utterance_raw is null"
+    yield check_null, query
+
+    query = "select count(*) from utterance where utterance is null"
+    yield check_null, query
+
+    query = "select count(*) from utterance where sentence_type is null"
+    yield check_null, query
+
+    query = "select count(*) from words where word is null"
+    yield check_null, query
+
+    query = "select count(*) from speakers where speaker_label is null"
+    yield check_null, query
+
+    query = "select count(*) from speakers where name is null"
+    yield check_null, query
+
+    query = "select count(*) from speakers where macrorole is null"
+    yield check_null, query
+
+    query = "select count(*) from unique_speakers where speaker_label is null"
+    yield check_null, query
+
+    query = "select count(*) from unique_speakers where name is null"
+    yield check_null, query
+
+    query = "select count(*) from unique_speakers where corpus is null"
+    yield check_null, query
+
 
 def check_values(query, values):
-    """
-    Checks value sets against queries
-    """
     res = session.execute(query)
     rows = res.fetchall()
     for row in rows:
         label = row[0]
         assert_in(label, values, msg='%s in database, but not in valid labels' % (label))
+
+
+def check_null(query):
+    res = session.execute(query)
+    actual = res.fetchone()[0]
+    assert_equal(actual, 0, msg='Column contains NULL')
+
