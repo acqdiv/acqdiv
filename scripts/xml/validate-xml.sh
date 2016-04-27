f7bb97eb1cd46288609139d0af0a7bb8efe962f7
bambooforest@gmail.com <bambooforest@gmail.com>
2016-04-27 15:49:26 +0200

    adds imdi xml (non)validation example; see #445

--- scripts/xml/validate-xml.sh
+++ scripts/xml/validate-xml.sh
@@ -1,1 +1,10 @@
-xmllint --noout --schema talkbank.xsd aprm19990515.xml
+# Japanese CHILDES
+# xmllint --noout --schema talkbank.xsd aprm19990515.xml
+
+# Russian
+xmllint --noout --schema IMDI_3.0.xsd A09231013.imdi
+# Passes:
+xmllint --noout --schema IMDI_3.0.xsd V10840108.imdi
+
+# Check without xsd
+xmllint --noout A09231013.imdi
