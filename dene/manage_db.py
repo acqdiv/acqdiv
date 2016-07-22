37b705c5d6aeac314150c3f340d07897c454067d
Anna Jancso <anna.jancso@uzh.ch>
2016-07-22 19:42:48 +0200

    fills sessions now, encoding problem fixed

--- dene/manage_db.py
+++ dene/manage_db.py
@@ -1,7 +1,8 @@
 import MySQLdb as db
-import _mysql_exceptions
 import csv
 import argparse
+import traceback
+import sys
 
 
 def commandline_setup():
@@ -10,12 +11,12 @@ def commandline_setup():
     parser = argparse.ArgumentParser(description=arg_description)
 
     ####### files
-    parser.add_argument("-s", "--sessions", help="path to sessions.csv")
-    parser.add_argument("-p", "--participants", help="path to participants.csv")
-    parser.add_argument("-f", "--files", help="path to files.csv")
-    parser.add_argument("-m", "--monitor", help="path to monitor.csv")
+    parser.add_argument("--metadata", action="store_true")
+    parser.add_argument("--sessions", help="path to sessions.csv", default="../Metadata/sessions.csv")
+    parser.add_argument("--participants", help="path to participants.csv", default="../Metadata/participants.csv")
+    parser.add_argument("--files", help="path to files.csv", default="../Metadata/files.csv")
+    parser.add_argument("--monitor", help="path to monitor.csv", default="../Workflow/monitor.csv")
     ####### task options
-
 
     return parser.parse_args()
 
@@ -26,73 +27,108 @@ class DB_Manager:
 
         self.con = con
         self.cur = self.con.cursor()
-
         self.args = commandline_setup()
 
 
-    def fill_participants(self):
-        """Refreshes table 'participants' completely by filling it with data from participants.csv """
-        # wipe table 'participants' first
-        self.cur.execute("""DELETE FROM participants;""")
+    def wipe(self, table):
+        """Delete all rows of a table"""
+        self.cur.execute("""DELETE FROM {};""".format(table))
         self.con.commit()
 
-        # default path for participants.csv
-        participants_path = "../Metadata/participants.csv"
-        # overwrite path if given over command line
-        if self.args.participants:
-            participants_path = self.args.participants
 
-        # open participants.csv for reading
-        with open(participants_path, "r") as participants_file:
-
-            # go through each participant
-            for p in csv.DictReader(participants_file):
-
-                # empty strings should become null values
-                for field in p:
-                    if not p[field]:
-                        p[field] = None
-
-                # extract first-/lastname assuming only the first can consist of more than one word (not foolproof...)
-                *first_name, last_name = p["Full name"].split()
-
-                # create sets for language fields that have more than one value
-                for field in ["Main language", "First languages", "Second languages"]:
-                    if p[field] is not None:
-                        p[field] = set(p[field].split("/"))
+    def empty_str_to_none(self, row):
+        "Set all empty strings to None"
+        for field in row:
+            if not row[field]:
+                row[field] = None
 
 
-                # issues:
-                #   - change email field not working
-                #   - what about phone?
+    def insert(self, table, db_attributes, values, len):
+        """Insert values into a table"""
 
-                try:
-                    self.cur.execute("""INSERT INTO participants (
-                                            short_name, first_name, last_name, birthdate, age, gender,
-                                            education, first_languages, second_languages, main_language,
-                                            language_biography, description, contact_address, email
-                                            )
+        sql_command = "INSERT INTO " + table + db_attributes + " VALUES (" + ",".join(["%s"]*len) + ")"
 
-                                        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)""", (
-                                            p["Short name"], " ".join(first_name), last_name,
-                                            p["Birth date"], p["Age"], p["Gender"], p["Education"],
-                                            p["First languages"], p["Second languages"], p["Main language"],
-                                            p["Language biography"], p["Description"], p["Contact address"], p["E-mail/Phone"]
-                                        )
+        try:
+            self.cur.execute(sql_command, values)
 
-                    )
+        except db.Error:
+            print("\n")
+            traceback.print_exc(limit=0)
+            print("Check the following row in {}:".format(table), values)
 
-                # except _mysql_exceptions.IntegrityError:
-                #     continue
-                # temporary workaround
-                except UnicodeEncodeError:
-                    continue
+        self.con.commit()
 
-                self.con.commit()
+
+    def fill_sessions(self):
+        """Refreshes table 'sessions' completely by filling it with data from sessions.csv"""
+        db_attributes = "(name, date, location, duration, situation, content, notes)"
+
+        self.wipe("sessions")
+
+        # try reading sessions.csv
+        try:
+            sessions_file = open(self.args.sessions, "r")
+        except FileNotFoundError:
+            print("Path to sessions.csv not correct!")
+            sys.exit(1)
+
+        # go through each session
+        for p in csv.DictReader(sessions_file):
+
+            self.empty_str_to_none(p)
+
+            values = (p["Code"], p["Date"], p["Location"], p["Length of recording"], p["Situation"], p["Content"], p["Comments"])
+
+            self.insert("sessions", db_attributes, values, 7)
+
+
+    def fill_participants(self):
+        """Refresh table 'participants' completely by filling it with data from participants.csv"""
+        # issues:
+        #   - change email field not working
+        #   - what about phone?
+        #   - main language -> only one value allowed? -> adaption in metadata.py? / remove n/a
+
+        db_attributes = """(
+            short_name, first_name, last_name, birthdate, age, gender, education, first_languages,
+            second_languages, main_language, language_biography, description, contact_address, email
+        )"""
+
+        self.wipe("participants")
+
+        # try reading participants.csv
+        try:
+            participants_file = open(self.args.participants, "r")
+        except FileNotFoundError:
+            print("Path to participants.csv not correct!")
+            sys.exit(1)
+
+        # go through each participant
+        for p in csv.DictReader(participants_file):
+
+            self.empty_str_to_none(p)
+
+            # extract first-/lastname assuming only the first can consist of more than one word (not foolproof...)
+            *first_name, last_name = p["Full name"].split()
+
+            # create sets for language fields that have more than one value
+            for field in ["Main language", "First languages", "Second languages"]:
+                if p[field] is not None:
+                    p[field] = set(p[field].split("/"))
+
+            values = (
+                p["Short name"], " ".join(first_name), last_name, p["Birth date"], p["Age"], p["Gender"], p["Education"],
+                p["First languages"], p["Second languages"], p["Main language"], p["Language biography"], p["Description"],
+                p["Contact address"], p["E-mail/Phone"]
+            )
+
+            self.insert("participants", db_attributes, values, 14)
 
 
     def fill_database(self):
         self.fill_participants()
+        self.fill_sessions()
+
 
     def test(self):
         """Just for testing"""
@@ -101,12 +137,13 @@ class DB_Manager:
         self.cur.execute("""SELECT * FROM participants;""")
         print(self.cur.fetchone())
 
+
 def main():
 
-    con = db.connect(host="localhost", user="anna", passwd="anna", db="deslas")
+    con = db.connect(host="localhost", user="anna", passwd="anna", db="deslas", charset="utf8")
 
     populator = DB_Manager(con)
-    populator.fill_participants()
+    populator.fill_sessions()
 
     con.close()
 
