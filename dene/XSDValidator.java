81c82e5eab49c53635f121801ecd54796791efb0
Anna Jancso <anna.jancso@uzh.ch>
2016-07-25 13:58:07 +0200

    validator rewritten, validates now all imdi files

--- dene/XSDValidator.java
+++ dene/XSDValidator.java
@@ -5,35 +5,83 @@ import javax.xml.transform.Source;
 import org.xml.sax.SAXException;
 import javax.xml.XMLConstants;
 
-
+/**
+ * Sets up a XSD validator for IMDI files.
+ *
+ * @author Anna Jancso
+ */
 public class XSDValidator {
 
-    private File xsdFile;
+    private Validator validator;
 
-    public XSDValidator(String xsdPath) { xsdFile = new File(xsdPath); }
 
-    private void validate(String xmlPath) throws SAXException, IOException {
-
-        File xmlFile = new File(xmlPath);
-
+    /**
+    * Sets XSD validator with a XSD file.
+    *
+    * @param xsdFile XSD file
+    */
+    private void setValidator(File xsdFile) throws SAXException {
         SchemaFactory factory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
-
         Schema schema = factory.newSchema(xsdFile);
+        validator = schema.newValidator();
+    }
 
-        Validator validator = schema.newValidator();
+    /**
+     * Validates a IMDI files against a XSD file.
+     *
+     * @param imdiFile IMDI file to be checked
+     */
+    public void validate(File imdiFile) throws SAXException, IOException {
 
-        Source source = new StreamSource(xmlFile);
+        Source source = new StreamSource(imdiFile);
+
+        Boolean is_valid = true;
 
         try { validator.validate(source); }
         catch (SAXException exc) {
-            System.out.println(xmlFile.getName() + " is not valid:");
+            System.out.println(imdiFile.getName() + " is not valid:");
             System.out.println(exc.getMessage());
+            is_valid = false;
+        }
+
+        if (is_valid) {
+            System.out.println(imdiFile.getName() + " is valid");
         }
     }
 
-    public static void main(String args[]) throws SAXException, IOException {
-        XSDValidator validator = new XSDValidator("imdi-session.xml");
-        validator.validate("testset/IMDI/deslas-AMM-2015-06-02.cmdi");
-    }
+    /**
+     * Validates all IMDI files against a xsd file.
+     *
+     * @param args first argument: path to XSD file
+     *             second argument: path to directory containing all IMDI files
+     */
+    public static void main(String[] args) throws SAXException, IOException {
 
+        // number of arguments given over command line
+        int argsLength = args.length;
+
+        // default paths
+        String xsdPath = "../Metadata/imdi-session.xsd";
+        String imdiPath = "../Metadata/IMDI";
+
+        // overwrite them if they were given over command line
+        if (argsLength == 1) { xsdPath = args[0]; }
+        else if (argsLength == 2) {
+            xsdPath = args[0];
+            imdiPath = args[1];
+        }
+
+        File xsdFile = new File(xsdPath);
+        File imdiFile = new File(imdiPath);
+
+        // set validator with XSD file
+        XSDValidator validator = new XSDValidator();
+        validator.setValidator(xsdFile);
+
+        // get all files in IMDI directory
+        File[] imdiFiles = imdiFile.listFiles();
+
+        // validate every IMDI file against the XSD
+        for (File file : imdiFiles) { validator.validate(file); }
+    }
 }
