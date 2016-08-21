b2ec6558deaa4269758155905b1ca065a56cafb6
Anna Jancso <anna.jancso@uzh.ch>
2016-08-21 21:37:19 +0200

    update tables if records already exist

--- dene/manage_db.py
+++ dene/manage_db.py
@@ -11,7 +11,6 @@ def commandline_setup():
     parser = argparse.ArgumentParser(description=arg_description)
 
     ####### files
-    parser.add_argument("--metadata", action="store_true")
     parser.add_argument("--sessions", help="path to sessions.csv", default="../Metadata/sessions.csv")
     parser.add_argument("--participants", help="path to participants.csv", default="../Metadata/participants.csv")
     parser.add_argument("--files", help="path to files.csv", default="../Metadata/files.csv")
@@ -30,9 +29,24 @@ class DB_Manager:
         self.args = commandline_setup()
 
 
-    def wipe(self, table):
+    def wipe(self, table, associated_tables=()):
         """Delete all rows of a table"""
+        # disable foreign key checks in all tables associated with this table
+        for associated_table in associated_tables:
+            self.cur.execute("""ALTER TABLE {} NOCHECK CONSTRAINT all""".format(associated_table))
+            self.con.commit()
+
+        # delete all rows
         self.cur.execute("""DELETE FROM {};""".format(table))
+        self.con.commit()
+
+        # enable foreign key checks again in all tables associated with this table
+        for associated_table in associated_tables:
+            self.cur.execute("""ALTER TABLE {} CHECK CONSTRAINT all""".format(associated_table))
+            self.con.commit()
+
+        # set auto-incrementing to 1
+        self.cur.execute("""ALTER TABLE {} AUTO_INCREMENT = 1""".format(table))
         self.con.commit()
 
 
@@ -43,27 +57,69 @@ class DB_Manager:
                 row[field] = None
 
 
-    def insert(self, table, db_attributes, values, len):
-        """Insert values into a table"""
-
-        sql_command = "INSERT INTO " + table + db_attributes + " VALUES (" + ",".join(["%s"]*len) + ")"
+    def execute(self, command, values):
+        """Execute SQL commands and catch any SQL errors"""
 
         try:
-            self.cur.execute(sql_command, values)
-
-        except db.Error:
-            print("\n")
-            traceback.print_exc(limit=0)
-            print("Check the following row in {}:".format(table), values)
+            self.cur.execute(command, values)
+        except db.OperationalError:
+            #print("\n")
+            #print("******************************")
+            #traceback.print_exc(limit=0)
+            #print("Check the following row:", values)
+            pass
+        except db.DataError:
+            #print("\n")
+            #print("******************************")
+            #traceback.print_exc(limit=0)
+            #print("Check the following row:", values)
+            pass
 
         self.con.commit()
 
 
-    def fill_sessions(self):
-        """Refreshes table 'sessions' completely by filling it with data from sessions.csv"""
-        db_attributes = "(name, date, location, duration, situation, content, notes)"
+    def get_insert_command(self, table, db_attributes):
+        """Create INSERT command with given attributes and return it"""
 
-        self.wipe("sessions")
+        insert_command = "INSERT INTO {} ({}) VALUES ({})".format(
+            table,
+            ",".join(db_attributes),
+            ",".join(["%s"]*len(db_attributes))
+        )
+
+        return insert_command
+
+
+    def get_update_command(self, table, db_attributes):
+        """Create UPDATE command with given attributes and return it"""
+
+        update_command = "UPDATE {} SET {} WHERE id = {{}}".format(
+            table,
+            ','.join((attr + "=%s") for attr in db_attributes),
+        )
+
+        return update_command
+
+
+    def get_insert_update_command(self, table, db_attributes):
+
+        insert_update_command = """INSERT INTO {} ({}) VALUES ({}) ON DUPLICATE KEY UPDATE {}""".format(
+            table,
+            ",".join(db_attributes),
+            ",".join(["%s"]*len(db_attributes)),
+            ','.join((attr + "=%s") for attr in db_attributes)
+        )
+
+        return insert_update_command
+
+
+    def import_sessions(self):
+        """Refreshe table 'sessions' completely by filling it with data from sessions.csv"""
+
+        db_attributes = ("name", "date", "location", "duration", "situation", "content", "notes")
+
+        insert_command = self.get_insert_command("sessions", db_attributes)
+        update_command = self.get_update_command("sessions", db_attributes)
 
         # try reading sessions.csv
         try:
@@ -77,24 +133,49 @@ class DB_Manager:
 
             self.empty_str_to_none(p)
 
-            values = (p["Code"], p["Date"], p["Location"], p["Length of recording"], p["Situation"], p["Content"], p["Comments"])
+            values = (
+                p["Code"], p["Date"], p["Location"], p["Length of recording"],
+                p["Situation"], p["Content"], p["Comments"]
+            )
 
-            self.insert("sessions", db_attributes, values, 7)
+            # get index of session (NB: returns None if session code not yet in database)
+            self.cur.execute("SELECT id FROM sessions WHERE name = '{}'".format(p["Code"]))
+            session_index = self.cur.fetchone()
+
+            # if session is already in database, update its metadata
+            if session_index:
+                self.execute(update_command.format(session_index[0]), values)
+            # otherwise create session in the database
+            else:
+                self.execute(insert_command, values)
+
+            #self.update_sessions_participants(p["Participants and roles"], session_index)
 
 
-    def fill_participants(self):
+    def update_sessions_participants(participants_roles):
+
+        part_role_list = participants_roles.split("), ")
+
+        for part_role in part_role_list:
+
+            shortname, role = part_role.split(" (")
+
+            self.cur.execute("""INSERT """)
+
+
+
+
+    def import_participants(self):
         """Refresh table 'participants' completely by filling it with data from participants.csv"""
-        # issues:
-        #   - change email field not working
-        #   - what about phone?
-        #   - main language -> only one value allowed? -> adaption in metadata.py? / remove n/a
 
-        db_attributes = """(
-            short_name, first_name, last_name, birthdate, age, gender, education, first_languages,
-            second_languages, main_language, language_biography, description, contact_address, email
-        )"""
+        db_attributes = (
+            "short_name", "first_name", "last_name", "birthdate", "age", "gender",
+            "education", "first_languages", "second_languages", "main_language",
+            "language_biography", "description", "contact_address", "email_phone"
+        )
 
-        self.wipe("participants")
+        insert_command = self.get_insert_command("participants", db_attributes)
+        update_command = self.get_update_command("participants", db_attributes)
 
         # try reading participants.csv
         try:
@@ -106,28 +187,49 @@ class DB_Manager:
         # go through each participant
         for p in csv.DictReader(participants_file):
 
+            # convert empty strings to None
             self.empty_str_to_none(p)
 
-            # extract first-/lastname assuming only the first can consist of more than one word (not foolproof...)
-            *first_name, last_name = p["Full name"].split()
+            # extract first-/lastname assuming only the first can consist of more than one word
+            try:
+                *first_name, last_name = p["Full name"].split()
+            except Exception:
+                first_name = None
+                last_name = None
+            else:
+                first_name = " ".join(first_name)
 
             # create sets for language fields that have more than one value
             for field in ["Main language", "First languages", "Second languages"]:
                 if p[field] is not None:
                     p[field] = set(p[field].split("/"))
 
+            # values must be in same order as its corresponding name attributes
             values = (
-                p["Short name"], " ".join(first_name), last_name, p["Birth date"], p["Age"], p["Gender"], p["Education"],
-                p["First languages"], p["Second languages"], p["Main language"], p["Language biography"], p["Description"],
+                p["Short name"], first_name, last_name, p["Birth date"],
+                p["Age"], p["Gender"], p["Education"],
+                p["First languages"], p["Second languages"], p["Main language"],
+                p["Language biography"], p["Description"],
                 p["Contact address"], p["E-mail/Phone"]
             )
 
-            self.insert("participants", db_attributes, values, 14)
+            # get index of participant (NB: returns None if not in database yet)
+            self.cur.execute("SELECT id FROM participants WHERE short_name = '{}'".format(p["Short name"]))
+            index = self.cur.fetchone()
+
+            # if participant is already in database, update its metadata
+            if index:
+                self.execute(update_command.format(index[0]), values)
+            # otherwise create participant in the database
+            else:
+                self.execute(insert_command, values)
 
 
-    def fill_database(self):
-        self.fill_participants()
-        self.fill_sessions()
+    def refresh_database(self):
+        self.wipe("sessions")
+        self.wipe("participants")
+        self.import_participants()
+        self.import_sessions()
 
 
     def test(self):
@@ -138,13 +240,14 @@ class DB_Manager:
         print(self.cur.fetchone())
 
 
+
 def main():
 
     con = db.connect(host="localhost", user="anna", passwd="anna", db="deslas", charset="utf8")
 
     populator = DB_Manager(con)
-    populator.fill_sessions()
-
+    populator.refresh_database()
+    print("done")
     con.close()
 
 
