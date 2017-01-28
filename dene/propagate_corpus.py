f4c054f157025fa3660f804f3ccc1c21837d3149
Anna Jancso <anna.jancso@uzh.ch>
2017-01-28 18:27:06 +0100

    check ids in words in logs

--- dene/propagate_corpus.py
+++ dene/propagate_corpus.py
@@ -23,15 +23,12 @@ class CorpusPropagater:
         self.dic = {}
         self.logs = {}
         self.vtlogs = {}
-   
-        # create directory for the new corpus files
-        if not os.path.isdir(self.new_cp_path):
-            try:
-                os.mkdir(self.new_cp_path)
-            except FileNotFoundError:
-                print("Corpus directory couldn't be created at",
-                      self.new_cp_path)
-                sys.exit(1)
+
+        # save data of one utterance using a sorted dict because order is
+        # important; key is the fieldmarker, value the whole line (including
+        # field marker label) except for \glo, \seg, \id which are
+        # temporarily saved in a special data structure under "words"
+        self.ref_dict = OrderedDict()
 
     def activate_logger(self):
         """Activate logger."""
@@ -100,7 +97,6 @@ class CorpusPropagater:
             for line in log_file:
                 # get log as a list of data
                 log = line.rstrip().split("|")
-
                 # UPDATE/MERGE/SPLIT
                 action = log[0].upper()
                 # affected id (log) or lemma (vtlog)
@@ -116,10 +112,10 @@ class CorpusPropagater:
                     logs[id].append({"action": action, "tier": log[2],
                                      "old": old, "new": new})
                 elif action == "MERGE":
-                    logs[id].append({"action": action, "merged_id": log[2]})
+                    logs[id].append({"action": action, "merging_id": log[2]})
 
                 elif action == "SPLIT":
-                    logs[id].append({"action": action, "split_id": log[2],
+                    logs[id].append({"action": action, "splitting_id": log[2],
                                      "tier": log[3], "word": log[4]})
                 else:
                     self.logger.error("Action {} not defined.".format(action))
@@ -132,16 +128,24 @@ class CorpusPropagater:
         self.get_log_data(is_vtlog=True)
 
     def process_corpus(self):
-        """Adapt corpus data if necessary."""
+        """Process every corpus file."""
+        # first create directory for the new corpus files
+        if not os.path.isdir(self.new_cp_path):
+            try:
+                os.mkdir(self.new_cp_path)
+            except FileNotFoundError:
+                print("Corpus directory couldn't be created at",
+                      self.new_cp_path)
+                sys.exit(1)
 
         # go through each original corpus file
         for file in os.listdir(self.org_cp_path):
 
-            # get path of this original file
-            org_file_path = os.path.join(self.org_cp_path, file)
+            # if it is a toolbox file
+            if file.endswith(".tbt"):
 
-            # check if it is really a file, not a dictionary
-            if os.path.isfile(org_file_path):
+                # get path of this original file
+                org_file_path = os.path.join(self.org_cp_path, file)
 
                 # get path for new corpus file
                 new_file_path = os.path.join(self.new_cp_path, file)
@@ -150,116 +154,135 @@ class CorpusPropagater:
                 with open(org_file_path, "r") as org_file, \
                      open(new_file_path, "w") as new_file:
 
-                    # save data of one utterance using a sorted
-                    # dictionary because order is important
-                    # key is the fieldmarker, value the whole line (including
-                    # field marker label) except for \glo, \seg, \id which are
-                    # are temporarily saved in a special data structure under
-                    # "words" (see below)
-                    ref_dict = OrderedDict()
+                    self.process_file(org_file, new_file)
 
-                    # go through all data from the corpus file
-                    # and save data per utterance unter ref_dict
-                    for line in org_file:
+    def process_file(self, org_file, new_file):
+        """"""
+        has_errors = False
+        # go through all data fomr a corpus file
+        # and save data per utterance unter ref_dict
+        for line in org_file:
 
-                        # throw away the newline at the end of a line
-                        line = line.rstrip("\n")
+            # throw away the newline at the end of a line
+            line = line.rstrip("\n")
 
-                        # if new utterance starts
-                        if line.startswith("\\ref"):
+            # if new utterance starts
+            if line.startswith("\\ref"):
+                # check previous utterance for any necessary changes
+                self.check_utterance(has_errors=has_errors)
+                # reset to False
+                has_errors = False
+                # delete data of previous utterance
+                self.ref_dict.clear()
+                # add \ref to hash
+                self.ref_dict["\\ref"] = line
 
-                            # check if there is a previous utterance
-                            if ref_dict:
-                                # check it for any changes
-                                self.check_utterance()
+            # if fieldmarker \full of utterance starts
+            elif line.startswith("\\full"):
+                # save morpheme data in a special data structure
+                # words: [(word1, {"\\seg": [seg1, seg2]
+                #                  "\\glo": [glo1, glo2],
+                #                  "\\id": [id1, id2]}), (word2, {...})]
+                self.ref_dict["words"] = []
 
-                            # delete data of previous utterance
-                            ref_dict.clear()
-                            # add \ref to hash
-                            ref_dict["\\ref"] = line
+                # extract words splitting the processed line at whitespaces
+                word_iterator = re.finditer(r"\S+", line)
+                # skip the field marker label
+                next(word_iterator)
 
-                        # if fieldmarker \full of utterance starts
-                        elif line.startswith("\\full"):
-                            # create "words" in ref-dict containing morpheme
-                            # data for each word in a list structure because
-                            # the same word can occur several times, ex.:
-                            # [(word1, {"\\seg": [seg1, seg2]
-                            #           "\\glo": [glo1, glo2],
-                            #           "\\id": [id1, id2]})
-                            #  (word2, {...})]
-                            ref_dict["words"] = []
+                # count the words
+                n_words = 0
+                # add every word to the hash
+                for word in word_iterator:
+                    self.ref_dict["words"].append((word.group(),
+                                                   {}))
+                    n_words += 1
 
-                            word_iterator = re.finditer(r"\S+", line)
-                            # skip the field marker label
-                            next(word_iterator)
+            # add morpheme data from \seg, \glo and \id
+            elif (line.startswith("\\seg") or
+                  line.startswith("\\glo") or
+                  line.startswith("\\id")):
 
-                            n_words = 0
-                            # add every word from this utterance
-                            for word in word_iterator:
-                                ref_dict["words"].append((word.group(), {}))
-                                n_words += 1
+                # get regex & iterator for extracting units per word
+                regex = re.compile(r"((\S+-\s+)*\S+(\s+-\S+)*)")
+                unitgroup_iterator = regex.finditer(line)
 
-                        # add morpheme data from \seg, \glo and \id
-                        elif (line.startswith("\\seg") or
-                              line.startswith("\\glo") or
-                              line.startswith("\\id")):
+                # get field marker label which is the first unit
+                label = next(unitgroup_iterator).group()
 
-                            # get regex & iterator for getting units per word
-                            regex = re.compile(r"((\S+-\s+)*\S+(\s+-\S+)*)")
-                            unitgroup_iterator = regex.finditer(line)
+                # count number of unit groups (i.e. glos, segs, ids)
+                n_unitgroups = 0
+                # go over those units per word
+                for i, unitgroup in enumerate(unitgroup_iterator):
 
-                            # get field marker label which is the first unit
-                            label = next(unitgroup_iterator).group()
+                    # add units to the right word (via index)
+                    # to morpheme dictionary (at index 1)
+                    # under the right label (\seg, \glo or \id)
+                    self.ref_dict["words"][i][1][label] = \
+                        re.split("\s+", unitgroup.group())
 
-                            n_unitgroups = 0
-                            # go over those units per word
-                            for i, unitgroup in enumerate(unitgroup_iterator):
+                    n_unitgroups += 1
 
-                                # add units to the right word (via index)
-                                # to morpheme dictionary (-> 1)
-                                # under the right label (\seg, \glo or \id)
-                                ref_dict["words"][i][1][label] = \
-                                    re.split("\s+", unitgroup.group())
+                if n_words != n_unitgroups:
+                    self.logger.error(
+                        "number of {}s and words unequal for {}".
+                        format(label, self.ref_dict["\\ref"]))
 
-                                n_unitgroups += 1
+                    has_errors = True
 
-                            if n_words != n_unitgroups:
-                                self.logger.error(
-                                    "number of {}s and words unequal for {}".
-                                    format(label, ref_dict["\\ref"]))
+            # if any other new fieldmarker starts
+            elif line.startswith("\\"):
+                # extract field marker label and its data
+                match = re.match(r"(\\\w+)(.*)", line)
+                if match:
+                    label = match.group(1)
+                    data = match.group(2)
 
-                        # if any other new fieldmarker starts
-                        elif line.startswith("\\"):
-                            # extract field marker label and its data
-                            match = re.match(r"(\\\w+)(.*)", line)
-                            if match:
-                                label = match.group(1)
-                                data = match.group(2)
+                    # check if there are several defintions of the
+                    # same fieldmarker in one \ref
+                    if label in self.ref_dict:
+                        # concatenate with hash content
+                        self.ref_dict[label] += data
+                    else:
+                        # add to hash
+                        self.ref_dict[label] = line
 
-                                # check if there are several defintions of the
-                                # same fieldmarker in one \ref
-                                if label in ref_dict:
-                                    # concatenate with hash content
-                                    ref_dict[label] += data
-                                else:
-                                    # add to hash
-                                    ref_dict[label] = line
+            # if line does not start with \\, its data must belong
+            # to the fieldmarker that directly comes before
+            elif line:
+                # get last added fieldmarker
+                label = next(reversed(self.ref_dict))
+                # concatenate content
+                self.ref_dict[label] += line
 
-                        # if line does not start with \\, its data must belong
-                        # to the fieldmarker that directly comes before
-                        elif line:
-                            # get last added fieldmarker
-                            label = next(reversed(ref_dict))
-                            # concatenate content
-                            ref_dict[label] += line
+        # check last ref inserted
+        self.check_utterance(has_errors=has_errors)
 
-                    # check last ref inserted
-                    self.check_utterance()
+    def check_utterance(self, has_errors=False):
+        """Check utterance for ids in the log."""
+        # if utterance has errors or no words in it
+        if has_errors or "words" not in self.ref_dict:
+            # write unchanged to file
+            self.write_file()
+        else:
+            # check all words for ids in log
+            for word, morpheme_data in self.ref_dict["words"]:
 
-    def check_utterance(self):
-        pass
+                # if there is morpheme data for this word
+                if morpheme_data:
 
-    def write_file(self, ref_dict):
+                    # go through every id of this word
+                    for id in morpheme_data["\\id"]:
+
+                        # strip '-' left and right of id (if there is)
+                        stripped_id = id.strip("-")
+
+                        # check if this id is in the log
+                        if stripped_id in self.logs:
+
+                            print(stripped_id)
+
+    def write_file(self):
         """Write data of a utterance to a file."""
         pass
 
