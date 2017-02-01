ccf5b60f10d30a19cce5e7972a38b9c9951ca9a2
rabart <robert.schikowski@uzh.ch>
2017-02-01 14:57:25 +0100

    adjusted R export to nw db structure

--- database/csv2r.R
+++ database/csv2r.R
@@ -18,42 +18,29 @@ for (table in tables){
 print("merging to big flat table...")
 
 # change "id" columns to unique names -> change once fixed in DB!
-names(utterances)[names(utterances)=="id"] <- "utterance_id"
-names(words)[names(words)=="id"] <- "word_id"
-names(morphemes)[names(morphemes)=="id"] <- "morpheme_id"
-names(speakers)[names(speakers)=="id"] <- "speaker_id"
-names(uniquespeakers)[names(uniquespeakers)=="id"] <- "unique_speaker_id"
-names(sessions)[names(sessions)=="id"] <- "session_id"
-# other renamers
-names(utterances)[names(utterances)=="source_id"] <- "utterance_id_source"
-names(utterances)[names(utterances)=="morpheme"] <- "utterance_morphemes"
-names(utterances)[names(utterances)=="gloss_raw"] <- "utterance_glosses_raw"
-names(utterances)[names(utterances)=="pos_raw"] <- "utterance_poses_raw"
-names(words)[names(words)=="pos"] <- "pos_word_stem"
-names(sessions)[names(sessions)=="source_id"] <- "session_id_source"
-names(morphemes)[names(morphemes)=="type"] <- "morpheme_type"
+utterances <- utterances %>% rename(utterance_id=id, utterance_id_source=source_id, utterance_morphemes=morpheme, utterance_gloses_raw=gloss_raw, utterance_poses_raw=pos_raw)
+words <- words %>% rename(word_id=id, pos_word_stem=pos)
+morphemes <- morphemes %>% rename(morpheme_id=id, morpheme_language=language, morpheme_type=type)
+speakers <- speakers %>% rename(speaker_id=id)
+uniquespeakers <- uniquespeakers %>% rename(unique_speaker_id=id)
+sessions <- sessions %>% rename(session_id=id, session_id_source=source_id)
 
 # create alternative tables without columns that would only create duplicates, including warnings
-utterances_slim <- utterances[,c("utterance_id","session_id_fk","utterance_id_source","corpus","language","speaker_label","addressee","utterance","translation","utterance_morphemes","start","end","comment","warning")]
-words_slim <- words[,c("word_id","utterance_id_fk","word","pos_word_stem","word_actual","word_target")]
-morphemes_slim <- morphemes[,c("morpheme_id","word_id_fk","morpheme_type","morpheme","gloss_raw","gloss","pos_raw","pos")]
-morphemes_slim <- morphemes_slim[!is.na(morphemes_slim$word_id_fk),] # take out NA foreign keys for easier merging
-sessions_slim <- sessions[,c("session_id","session_id_source","date","media","media_type")]
-speakers_slim <- speakers[,c("speaker_id","session_id_fk","uniquespeaker_id_fk","speaker_label","name","age_raw","age","age_in_days","gender_raw","gender","role_raw","role","macrorole","languages_spoken","birthdate")]
+utterances_slim <- utterances %>% select(utterance_id, session_id_fk, utterance_id_source, corpus, language, speaker_id_fk, addressee, childdirected, utterance, translation, utterance_morphemes, start, end, comment, warning)
+words_slim <- words %>% select(word_id, utterance_id_fk, word, pos_word_stem, word_actual, word_target)
+morphemes_slim <- morphemes %>% select(morpheme_id, word_id_fk, morpheme_type, morpheme, gloss_raw, gloss, pos_raw, pos, morpheme_language)
+morphemes_slim <- morphemes_slim %>% filter(!is.na(word_id_fk)) # take out NA foreign keys for easier merging
+sessions_slim <- sessions %>% select(session_id, session_id_source, date, target_child_fk)
+speakers_slim <- speakers %>% select(speaker_id, session_id_fk, uniquespeaker_id_fk, speaker_label, name, age_raw, age, age_in_days, gender_raw, gender, role_raw, role, macrorole, languages_spoken, birthdate)
 
 # merge tables one by one
 left_join(utterances_slim, words_slim, by=c("utterance_id"="utterance_id_fk")) -> u_w
 left_join(u_w, morphemes_slim, by=c("word_id"="word_id_fk")) -> u_w_m
 left_join(sessions_slim, u_w_m, by=c("session_id"="session_id_fk")) -> u_w_m_s
-# speakers can only be merged in by composite key -> change once fixed in DB
-left_join(u_w_m_s, speakers_slim, by=c("session_id"="session_id_fk","speaker_label"="speaker_label")) -> all_data
+left_join(u_w_m_s, speakers_slim, by=c("speaker_id_fk"="speaker_id")) -> all_data
 
-# merge(utterances_slim, words_slim, by.x="utterance_id", by.y="utterance_id_fk", all.x=TRUE, all.y=FALSE) -> u_w
-# merge(u_w, morphemes_slim, by.x="word_id", by.y="word_id_fk", all.x=TRUE, all.y=FALSE) -> u_w_m
-# merge(sessions_slim, u_w_m, by.x="session_id", by.y="session_id_fk", all.x=FALSE, all.y=TRUE) -> u_w_m_s
-# # speakers can only be merged in by composite key -> change once fixed in DB
-# merge(u_w_m_s, speakers_slim, by.x=c("session_id","speaker_label"), by.y=c("session_id_fk","speaker_label"), all.x=TRUE, all.y=FALSE) -> all_data
-all_data <- all_data[,c("corpus", "language", "session_id", "session_id_source", "date", "speaker_id", "speaker_label", "name", "birthdate", "age_raw", "age", "age_in_days", "gender_raw", "gender", "role_raw", "role", "macrorole", "addressee", "utterance_id", "utterance_id_source", "start", "end", "utterance", "translation", "utterance_morphemes", "comment", "word_id", "word", "word_actual", "word_target", "pos_word_stem", "morpheme_id", "morpheme_type", "morpheme", "gloss_raw", "gloss", "pos_raw", "pos")]
+# final set of columns
+all_data <- all_data %>% select(corpus, language, session_id, session_id_source, target_child_fk, date, speaker_id_fk, speaker_label, name, birthdate, age_raw, age, age_in_days, gender_raw, gender, role_raw, role, macrorole, addressee, childdirected, utterance_id, utterance_id_source, start, end, utterance, translation, utterance_morphemes, comment, word_id, word, word_actual, word_target, pos_word_stem, morpheme_id, morpheme_type, morpheme, gloss_raw, gloss, pos_raw, pos, morpheme_language)
 
 # drop slim tables
 rm(words_slim, morphemes_slim, sessions_slim, speakers_slim)
@@ -64,5 +51,4 @@ all_data <- all_data[order(all_data$corpus, all_data$session_id, all_data$uttera
 # save all tables to an R object named "acqdiv_corpus-YYYY-MM-DD.rda"
 date = format(Sys.time(), "%Y-%m-%d")
 path_to_R = paste("acqdiv_corpus_",date,".rda",sep="")
-
 save(file=path_to_R, list=c(tables,"all_data"))
