0b02aacfed4c1cc7e5a9e8d690e9a6e633ab182a
bambooforest@gmail.com <bambooforest@gmail.com>
2017-03-10 15:46:30 +0100

    removes postgres settings file, never used

--- pipeline/settings.py
+++
@@ -1,9 +0,0 @@
-# example settings file if we use postgres
-DATABASE = {
-    'drivername': 'postgres',
-    'host': 'localhost',
-    'port': '5432',
-    'username': 'stiv',
-    'password': '',
-    'database': 'acqdiv'
-}
