98fd9344d9f8502c212e5e1b3abb48e64a9eda6e
bambooforest@gmail.com <bambooforest@gmail.com>
2018-04-04 11:33:19 +0200

    regarding issue #546

--- pipeline/database/dump_production.sh
+++ pipeline/database/dump_production.sh
@@ -1,14 +1,56 @@
 #!/bin/bash
-sqlite3 acqdiv_corpus_2017-09-16.sqlite3 << EOF
+sqlite3 production.sqlite3 << EOF
 .headers on
 .mode csv
-.output acqdiv_corpus_2017-09-16.csv
-select * 
-from morphemes as m
-join utterances as u on m.utterance_id_fk = u.id
-left join words as w on m.word_id_fk = w.id
-join sessions s on u.session_id_fk = s.id
-left join speakers sp on u.speaker_id_fk = sp.id
-left join uniquespeakers on sp.uniquespeaker_id_fk = uniquespeakers.id
-order by s.id, u.id, w.id, m.id;
+.output production.csv
+SELECT 
+	utterances.id as utterance_id, 
+	utterances.source_id as utterance_source_id,
+	utterances.corpus,
+	utterances.language as corpus_language,
+	utterances.speaker_label,
+	speakers.id as speaker_id,
+	speakers.uniquespeaker_id_fk as uniquespeaker_id,
+	speakers.name,
+	speakers.age_raw,
+	speakers.age,
+	speakers.age_in_days,
+	speakers.gender,
+	speakers.role,
+	speakers.macrorole,
+	speakers.birthdate,
+	utterances.addressee,
+	utterances.sentence_type,	
+	utterances.childdirected,
+	utterances.utterance,
+	utterances.translation,
+	utterances.morpheme as utterance_morphemes,
+	utterances.gloss_raw as utterance_glosses_raw,
+	utterances.pos_raw as utterances_poses_raw,
+	utterances.start,
+	utterances.end,
+	utterances.comment,
+	words.id as word_id,
+	words.word_language,
+	words.word,
+	words.pos as pos_word_stem,
+	words.word_actual,
+	words.word_target,
+	morphemes.id as morpheme_id,
+	morphemes.type as morpheme_type,
+	morphemes.morpheme,
+	morphemes.gloss_raw,
+	morphemes.gloss,
+	morphemes.pos_raw,
+	morphemes.pos,
+	morphemes.morpheme_language,
+	sessions.id as session_id,
+	sessions.source_id as session_source_id,
+	sessions.date
+FROM utterances
+LEFT JOIN words ON utterances.id = words.utterance_id_fk
+LEFT JOIN morphemes ON words.id = morphemes.word_id_fk
+LEFT JOIN sessions ON utterances.session_id_fk = sessions.id
+LEFT JOIN speakers ON utterances.speaker_id_fk = speakers.id
+ORDER BY sessions.id, utterances.id, words.id, morphemes.id;
 EOF
--- pipeline/database/dump_test.sh
+++ pipeline/database/dump_test.sh
@@ -3,53 +3,54 @@ sqlite3 test.sqlite3 << EOF
 .headers on
 .mode csv
 .output test.csv
-select
-	s.id as session_id,
-	s.source_id as session_source_id,
-	s.date,
-	u.id as utterance_id,
-	u.source_id as utterance_source_id,
-	u.corpus,
-	u.language as language,
-	u.speaker_label,
-	sp.id as speaker_id,
-	sp.uniquespeaker_id_fk as uniquespeaker_id,
-	sp.name,
-	sp.age_raw,
-	sp.age,
-	sp.age_in_days,
-	sp.gender,
-	sp.role,
-	sp.macrorole,
-	sp.birthdate,
-	u.addressee,
-	u.childdirected,
-	u.utterance,
-	u.translation,
-	u.morpheme as utterance_morphemes,
-	u.gloss_raw as utterance_glosses_raw,
-	u.pos_raw as utterances_poses_raw,
-	u.start,
-	u.end,
-	u.comment,
-	w.id as word_id,
-	w.word,
-	w.pos as pos_word_stem,
-	w.word_actual,
-	w.word_target,
-	m.id as morpheme_id,
-	m.type as morpheme_type,
-	m.morpheme,
-	m.gloss_raw,
-	m.gloss,
-	m.pos_raw,
-	m.pos,
-	m.morpheme_language
-from morphemes as m
-	join utterances as u on m.utterance_id_fk = u.id
-	left join words as w on m.word_id_fk = w.id
-	join sessions s on u.session_id_fk = s.id
-	left join speakers sp on u.speaker_id_fk = sp.id
-	left join uniquespeakers on sp.uniquespeaker_id_fk = uniquespeakers.id
-order by s.id, u.id, w.id, m.id;
+SELECT 
+	utterances.id as utterance_id, 
+	utterances.source_id as utterance_source_id,
+	utterances.corpus,
+	utterances.language as corpus_language,
+	utterances.speaker_label,
+	speakers.id as speaker_id,
+	speakers.uniquespeaker_id_fk as uniquespeaker_id,
+	speakers.name,
+	speakers.age_raw,
+	speakers.age,
+	speakers.age_in_days,
+	speakers.gender,
+	speakers.role,
+	speakers.macrorole,
+	speakers.birthdate,
+	utterances.addressee,
+	utterances.sentence_type,
+	utterances.childdirected,
+	utterances.utterance,
+	utterances.translation,
+	utterances.morpheme as utterance_morphemes,
+	utterances.gloss_raw as utterance_glosses_raw,
+	utterances.pos_raw as utterances_poses_raw,
+	utterances.start,
+	utterances.end,
+	utterances.comment,
+	words.id as word_id,
+	words.word_language,
+	words.word,
+	words.pos as pos_word_stem,
+	words.word_actual,
+	words.word_target,
+	morphemes.id as morpheme_id,
+	morphemes.type as morpheme_type,
+	morphemes.morpheme,
+	morphemes.gloss_raw,
+	morphemes.gloss,
+	morphemes.pos_raw,
+	morphemes.pos,
+	morphemes.morpheme_language,
+	sessions.id as session_id,
+	sessions.source_id as session_source_id,
+	sessions.date
+FROM utterances
+LEFT JOIN words ON utterances.id = words.utterance_id_fk
+LEFT JOIN morphemes ON words.id = morphemes.word_id_fk
+LEFT JOIN sessions ON utterances.session_id_fk = sessions.id
+LEFT JOIN speakers ON utterances.speaker_id_fk = speakers.id
+ORDER BY sessions.id, utterances.id, words.id, morphemes.id;
 EOF
