b156c2c951dd6b5daf8304abeb025a48122fa0cd
Anna Jancso <anna.jancso0808@gmail.com>
2018-05-18 20:31:41 +0200

    complete implementation of CHATRecordParser

--- acqdiv/parsers/xml/CHATRecordParser.py
+++ acqdiv/parsers/xml/CHATRecordParser.py
@@ -56,7 +56,7 @@ class CHATRecordParser(RecordParser):
                     next_rec_start_pos = rec.start()
 
                     # get the stringified record
-                    rec_str = text[rec_start_pos:next_rec_start_pos]
+                    rec_str = text[rec_start_pos:next_rec_start_pos].decode()
 
                     # add data to record dict
                     self.add_main_line(rec_dict, rec_str)
@@ -71,7 +71,7 @@ class CHATRecordParser(RecordParser):
                     rec_start_pos = next_rec_start_pos
 
                 # handle last record
-                rec_str = text[rec_start_pos:]
+                rec_str = text[rec_start_pos:].decode()
                 self.add_main_line(rec_dict, rec_str)
                 self.add_dependent_tiers(rec_dict, rec_str)
                 yield rec_dict
@@ -86,7 +86,12 @@ class CHATRecordParser(RecordParser):
             record_str (str): The stringified record.
 
         """
-        pass
+        main_line_regex = re.compile(r'\*[A-Z]{3}:\t.*')
+        main_line = main_line_regex.search(record_str).group()
+        self.add_speaker_label(record_dict, main_line)
+        self.add_utterance(record_dict, main_line)
+        self.add_start(record_dict, main_line)
+        self.add_end(record_dict, main_line)
 
     def add_speaker_label(self, record_dict, main_line):
         """Add the speaker label from the main line.
@@ -96,7 +101,21 @@ class CHATRecordParser(RecordParser):
             main_line (str): The stringified main line.
 
         """
-        pass
+        speaker_label_regex = re.compile(r'(?<=^\*)[A-Z]{3}')
+        speaker_label = speaker_label_regex.search(main_line).group()
+        record_dict['speaker_label'] = speaker_label
+
+    def add_utterance(self, record_dict, main_line):
+        """Add the utterance from the main line.
+
+        Args:
+            record_dict (dict): Where the utterance is added.
+            main_line (str): The stringified main line.
+
+        """
+        utterance_regex = re.compile(r'(?<=:\t).*[.!?]')
+        utterance = utterance_regex.search(main_line).group()
+        record_dict['utterance'] = utterance
 
     def add_start(self, record_dict, main_line):
         """Add the start time from the main line.
@@ -106,7 +125,14 @@ class CHATRecordParser(RecordParser):
             main_line (str): The stringified main line.
 
         """
-        pass
+        start_regex = re.compile(r'\d+(?=_\d+)')
+        match = start_regex.search(main_line)
+        # start time might be missing
+        if match is None:
+            start = None
+        else:
+            start = match.group()
+        record_dict['start'] = start
 
     def add_end(self, record_dict, main_line):
         """Add the end time from the main line.
@@ -116,7 +142,15 @@ class CHATRecordParser(RecordParser):
             main_line (str): The stringified main line.
 
         """
-        pass
+        # look-behind does not work here, therefore we use a capturing group
+        end_regex = re.compile(r'(\d+_)(\d+)')
+        match = end_regex.search(main_line)
+        # end time might be missing
+        if match is None:
+            end = None
+        else:
+            end = match.group(2)
+        record_dict['end'] = end
 
     def add_dependent_tiers(self, record_dict, record_str):
         """Add all dependent tiers.
@@ -128,7 +162,10 @@ class CHATRecordParser(RecordParser):
             record_str (str): The stringified record.
 
         """
-        pass
+        dependent_tiers_regex = re.compile(r'\%.*')
+        for match in dependent_tiers_regex.finditer(record_str):
+            dependent_tier = match.group()
+            self.add_dependent_tier(record_dict, dependent_tier)
 
     def add_dependent_tier(self, record_dict, dependent_tier):
         """Add the dependent tier.
@@ -137,4 +174,8 @@ class CHATRecordParser(RecordParser):
             record_dict (dict): Where the dependent tier is added.
             dependent_tier (str): The stringified dependent tier.
         """
-        pass
+        dependent_tier_regex = re.compile(r'\%([a-z]+):\t(.*)')
+        match = dependent_tier_regex.search(dependent_tier)
+        tier_name = match.group(1)
+        tier_content = match.group(2)
+        record_dict[tier_name] = tier_content
