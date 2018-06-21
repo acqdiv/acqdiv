c1a55a0aea00191e7187f18d86a8f9915256e301
Anna Jancso <anna.jancso0808@gmail.com>
2018-06-21 17:01:41 +0200

    merge inference and parsing

--- acqdiv/parsers/xml/CHATParser.py
+++ acqdiv/parsers/xml/CHATParser.py
@@ -139,6 +139,115 @@ class CHATParser:
         else:
             return match.group()
 
+    # ---------- utterance processing ----------
+
+    @staticmethod
+    def get_shortening_actual(utterance):
+        """Get the actual form of shortenings.
+
+        Coding in CHAT: parentheses within word.
+        The part with parentheses is removed.
+        """
+        shortening_regex = re.compile(r'(\S*)\(\S+\)(\S*)')
+        return shortening_regex.sub(r'\1\2', utterance)
+
+    @staticmethod
+    def get_shortening_target(utterance):
+        """Get the target form of shortenings.
+
+        Coding in CHAT: \w+(\w+)\w+ .
+        The part in parentheses is kept, parentheses are removed.
+        """
+        shortening_regex = re.compile(r'(\S*)\((\S+)\)(\S*)')
+        return shortening_regex.sub(r'\1\2\3', utterance)
+
+    @staticmethod
+    def get_replacement_actual(utterance):
+        """Get the actual form of replacements.
+
+        Coding in CHAT: [: <words>] .
+        Keeps replaced words, removes replacing words with brackets.
+        """
+        # several scoped words
+        replacement_regex1 = re.compile(r'<(.*?)> \[: .*?\]')
+        clean = replacement_regex1.sub(r'\1', utterance)
+        # one scoped word
+        replacement_regex2 = re.compile(r'(\S+) \[: .*?\]')
+        return replacement_regex2.sub(r'\1', clean)
+
+    @staticmethod
+    def get_replacement_target(utterance):
+        """Get the target form of replacements.
+
+        Coding in CHAT: [: <words>] .
+        Removes replaced words, keeps replacing words with brackets.
+        """
+        replacement_regex = re.compile(r'(?:<.*?>|\S+) \[: (.*?)\]')
+        return replacement_regex.sub(r'\1', utterance)
+
+    @staticmethod
+    def get_fragment_actual(utterance):
+        """Get the actual form of fragments.
+
+        Coding in CHAT: word starting with &.
+        Keeps the fragment, removes the & from the word.
+        """
+        fragment_regex = re.compile(r'&(\S+)')
+        return fragment_regex.sub(r'\1', utterance)
+
+    @staticmethod
+    def get_fragment_target(utterance):
+        """Get the target form of fragments.
+
+        Coding in CHAT: word starting with &.
+        The fragment is marked as untranscribed (xxx).
+        """
+        fragment_regex = re.compile(r'&\S+')
+        return fragment_regex.sub('xxx', utterance)
+
+    @classmethod
+    def get_actual_form(cls, utterance):
+        """Get the actual form of the utterance."""
+        for actual_method in [cls.get_shortening_actual,
+                              cls.get_fragment_actual,
+                              cls.get_replacement_actual]:
+            utterance = actual_method(utterance)
+
+        return utterance
+
+    @classmethod
+    def get_target_form(cls, utterance):
+        """Get the target form of the utterance."""
+        for target_method in [cls.get_shortening_target,
+                              cls.get_fragment_target,
+                              cls.get_replacement_target]:
+            utterance = target_method(utterance)
+
+        return utterance
+
+    @staticmethod
+    def get_sentence_type(utterance):
+        """Get the sentence type of an utterance.
+
+        The sentence type is inferred from the utterance terminator.
+        """
+        mapping = {'.': 'declarative',
+                   '?': 'question',
+                   '!': 'exclamation',
+                   '+.': 'broken for coding',
+                   '+..': 'trail off',
+                   '+..?': 'trail off of question',
+                   '+!?': 'question with exclamation',
+                   '+/.': 'interruption',
+                   '+/?': 'interruption of a question',
+                   '+//.': 'self-interruption',
+                   '+//?': 'self-interrupted question',
+                   '+"/.': 'quotation follows',
+                   '+".': 'quotation precedes'}
+        terminator_regex = re.compile(r'([+/.!?"]*[!?.])(?=( \[\+|$))')
+        match = terminator_regex.search(utterance)
+        return mapping[match.group(1)]
+
     # ---------- time processing ----------
 
     @staticmethod
@@ -173,6 +282,64 @@ class CHATParser:
             end_regex = re.compile(r'_(\d+)')
             return end_regex.search(rec_time).group(1)
 
+###############################################################################
+
+class InuktitutParser(CHATParser):
+    """Inferences for Inuktitut."""
+
+    @staticmethod
+    def get_actual_alternative(utterance):
+        """Get the actual form of alternatives.
+
+        Coding in CHAT: [=? <words>]
+        The actual form is the alternative given in brackets.
+        """
+        replacement_regex = re.compile(r'(?:<.*?>|\S+) \[=\? (.*?)\]')
+        return replacement_regex.sub(r'\1', utterance)
+
+    @staticmethod
+    def get_target_alternative(utterance):
+        """Get the target form of alternatives.
+
+        Coding in CHAT: [=? <words>]
+        The target form is the original form.
+        """
+        # several scoped words
+        alternative_regex1 = re.compile(r'<(.*?)> \[=\? .*?\]')
+        clean = alternative_regex1.sub(r'\1', utterance)
+        # one scoped word
+        alternative_regex2 = re.compile(r'(\S+) \[=\? .*?\]')
+        return alternative_regex2.sub(r'\1', clean)
+
+    @classmethod
+    def get_actual_form(cls, utterance):
+        """Get the actual form of the utterance.
+
+        Considers alternatives as well.
+        """
+        utterance = super().get_actual_form(utterance)
+        return cls.get_actual_alternative(utterance)
+
+    @classmethod
+    def get_target_form(cls, utterance):
+        """Get the target form of the utterance.
+
+        Considers alternatives as well.
+        """
+        utterance = super().get_target_form(utterance)
+        return cls.get_target_alternative(utterance)
+
+    @classmethod
+    def iter_words(cls, tier):
+        for word in tier.split(' '):
+            yield word
+
+    @classmethod
+    def iter_morphemes(cls, word):
+        morpheme_regex = re.compile(r'(\w+|)+')
+        for morpheme in word.split('+'):
+            pass
+
 
 if __name__ == '__main__':
     import glob
@@ -198,3 +365,27 @@ if __name__ == '__main__':
             end = chat_parser.get_end(rec_time)
 
     print('--- %s seconds ---' % (time.time() - start_time))
+
+    parser = CHATParser()
+    print(repr(parser.get_shortening_actual(
+        'This (i)s a short(e)ned senten(ce)')))
+    print(repr(parser.get_shortening_target(
+        'This (i)s a short(e)ned senten(ce)')))
+    print(repr(parser.get_replacement_actual(
+        'This us [: is] <srane suff> [: strange stuff]')))
+    print(repr(parser.get_replacement_target(
+        'This us [: is] <srane suff> [: strange stuff]')))
+    print(repr(parser.get_fragment_actual('This is &at .')))
+    print(repr(parser.get_fragment_target('This is &at .')))
+    print(repr(parser.get_sentence_type('This is a sent +!?')))
+
+    inuktitut_parser = InuktitutParser()
+    print(repr(inuktitut_parser.get_actual_alternative(
+        'This is the target [=? actual] form.')))
+    print(repr(inuktitut_parser.get_target_alternative(
+        'This is the target [=? actual] form.')))
+
+    test = 'LR|qa^outside+LI|unnga^ALL+VZ|aq^go_by_way_of+VV|VA|' \
+           'tit^CAUS+VV|lauq^POL+VI|nnga^IMP_2sS_1sO VR|' \
+           'nimak^move_around+VV|VA|tit^CAUS+VV|nngit^NEG+VI|' \
+           'lugu^ICM_XxS_3sO? VR|kuvi^pour+NZ|suuq^HAB+NN|AUG|aluk^EMPH?'
