5ecb10fcb4b9ce5db35c839a1e969225392eba76
Anna Jancso <anna.jancso0808@gmail.com>
2018-07-04 23:50:42 +0200

    change structure of chat parser

--- acqdiv/parsers/xml/CHATParser.py
+++ acqdiv/parsers/xml/CHATParser.py
@@ -1,98 +1,68 @@
+from acqdiv.parsers.xml import CHATReader
+from acqdiv.parsers.xml import CHATCleaner
+
 
 class CHATParser:
     """Gathers all data for the DB for a given CHAT session file.
 
-    Uses the CHATParser for parsing and inferring data from the CHAT file and
+    Uses the CHATReader for reading and inferring data from the CHAT file and
     CHATCleaner for cleaning these data.
     """
 
-    def __init__(self, session_path, parser, cleaner):
-        """Initialize the path of the CHAT session file.
-
-        Args:
-            session_path (str): The path to the CHAT session path.
-            parser (CHATParser): The CHAT parser.
-            cleaner (CHATCleaner): The CHAT cleaner.
-        """
+    def __init__(self, session_path):
         self.session_path = session_path
-        self.p = parser
-        self.c = cleaner
+        self.reader = self.get_reader()
+        self.cleaner = self.get_cleaner()
+
+    @staticmethod
+    def get_reader():
+        return CHATReader.CHATReader()
+
+    @staticmethod
+    def get_cleaner():
+        return CHATCleaner.CHATCleaner()
 
     def next_utterance(self):
-        """Yields the next utterance of a session.
-
-        Requires the implementation of the following methods:
-
-            CHATParser:
-                iter_records
-                get_uid
-                get_main_line
-                get_speaker_label
-                get_addressee
-                get_utterance
-                get_actual_form
-                get_target_form
-                get_translation
-                get_sentence_type
-                get_time
-                get_start_raw
-                get_end_raw
-                get_words
-                get_seg_tier
-                get_gloss_tier
-                get_pos_tier
-                get_seg_words
-                get_gloss_words
-                get_pos_words
-                get_segments
-                get_glosses
-                get_poses
-
-            CHATCleaner
-                clean_utterance
-                clean_seg_tier
-                clean_gloss_tier
-                clean_pos_tier
-                clean_segment
-                clean_gloss
-                clean_pos
-        """
+        """Yields the next utterance of a session."""
         # go through each record in the session
-        for rec in self.p.iter_records(self.session_path):
+        for rec in self.reader.iter_records(self.session_path):
 
             # get source ID
-            source_id = self.p.get_uid()
+            source_id = self.reader.get_uid()
 
             # get fields from record
-            addressee = self.p.get_addressee(rec)
-            translation = self.p.get_translation(rec)
-            comment = self.p.get_comments(rec)
+            addressee = self.reader.get_addressee(rec)
+            translation = self.reader.get_translation(rec)
+            comment = self.reader.get_comments(rec)
 
             # get fields from main line
-            main_line = self.p.get_main_line(rec)
-            speaker_label = self.p.get_speaker_label(main_line)
-            utterance_raw = self.p.get_utterance(main_line)
+            main_line = self.reader.get_main_line(rec)
+            speaker_label = self.reader.get_speaker_label(main_line)
+            utterance_raw = self.reader.get_utterance_raw(main_line)
 
             # get fields from time
-            rec_time = self.p.get_time(main_line)
-            start_raw = self.p.get_start(rec_time)
-            end_raw = self.p.get_end(rec_time)
+            rec_time = self.reader.get_time(main_line)
+            start_raw = self.reader.get_start(rec_time)
+            end_raw = self.reader.get_end(rec_time)
 
-            # get actual and target utterances and clean them
-            actual_utterance = self.c.clean_utterance(
-                self.p.get_actual_form(utterance_raw))
-            target_utterance = self.c.clean_utterance(
-                self.p.get_target_form(utterance_raw))
-            # set actual or target utterance as standard
-            utterance = actual_utterance
+            # get actual, target and standard utterance and clean them
+            actual_utterance = self.cleaner.clean_utterance(
+                self.reader.get_actual_utterance(utterance_raw))
+            target_utterance = self.cleaner.clean_utterance(
+                self.reader.get_target_utterance(utterance_raw))
+            utterance = self.cleaner.clean_utterance(
+                self.reader.get_utterance(utterance_raw))
 
             # get sentence type from utterance
-            sentence_type = self.p.get_sentence_type(utterance)
+            sentence_type = self.reader.get_sentence_type(utterance)
 
             # get morphology tiers and clean them
-            seg_tier = self.c.clean_seg_tier(self.p.get_seg_tier(rec))
-            gloss_tier = self.c.clean_gloss_tier(self.p.get_gloss_tier(rec))
-            pos_tier = self.c.clean_pos_tier(self.p.get_pos_tier(rec))
+            seg_tier = self.cleaner.clean_seg_tier(
+                            self.reader.get_seg_tier(rec))
+            gloss_tier = self.cleaner.clean_gloss_tier(
+                            self.reader.get_gloss_tier(rec))
+            pos_tier = self.cleaner.clean_pos_tier(
+                            self.reader.get_pos_tier(rec))
 
             utterance_dict = {
                 'source_id': source_id,
@@ -112,8 +82,8 @@ class CHATParser:
             }
 
             # get actual and target words from the respective utterances
-            actual_words = self.p.get_words(actual_utterance)
-            target_words = self.p.get_words(target_utterance)
+            actual_words = self.reader.get_words(actual_utterance)
+            target_words = self.reader.get_words(target_utterance)
 
             # collect all words of the utterance
             words = []
@@ -129,9 +99,9 @@ class CHATParser:
                 words.append(word_dict)
 
             # get morpheme words from the respective morphology tiers
-            wsegs = self.p.get_seg_words(seg_tier)
-            wglosses = self.p.get_gloss_words(gloss_tier)
-            wposes = self.p.get_pos_words(pos_tier)
+            wsegs = self.reader.get_seg_words(seg_tier)
+            wglosses = self.reader.get_gloss_words(gloss_tier)
+            wposes = self.reader.get_pos_words(pos_tier)
 
             # collect morpheme data of an utterance
             morphemes = []
@@ -142,17 +112,17 @@ class CHATParser:
                 wmorphemes = []
 
                 # get morphemes from the morpheme words
-                segments = self.p.get_segments(wseg)
-                glosses = self.p.get_glosses(wgloss)
-                poses = self.p.get_poses(wpos)
+                segments = self.reader.get_segments(wseg)
+                glosses = self.reader.get_glosses(wgloss)
+                poses = self.reader.get_poses(wpos)
 
                 # go through morphemes
                 for seg, gloss, pos in zip(segments, glosses, poses):
 
                     # clean the morphemes
-                    seg = self.c.clean_segment(seg)
-                    gloss = self.c.clean_gloss(gloss)
-                    pos = self.c.clean_pos(pos)
+                    seg = self.cleaner.clean_segment(seg)
+                    gloss = self.cleaner.clean_gloss(gloss)
+                    pos = self.cleaner.clean_pos(pos)
 
                     morpheme_dict = {
                         'morpheme_language': None,
@@ -166,3 +136,22 @@ class CHATParser:
 
             yield utterance_dict, words, morphemes
 
+
+class CreeParser(CHATParser):
+    @staticmethod
+    def get_reader():
+        return CHATReader.CreeReader()
+
+    @staticmethod
+    def get_cleaner():
+        return CHATCleaner.CreeCleaner()
+
+
+class InuktitutParser(CHATParser):
+    @staticmethod
+    def get_reader():
+        return CHATReader.InuktitutReader()
+
+    @staticmethod
+    def get_cleaner():
+        return CHATCleaner.InuktitutCleaner()
