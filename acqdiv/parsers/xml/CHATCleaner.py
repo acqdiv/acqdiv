43af40bb0cdd13bfee114c8bf417ef2492697c22
Anna Jancso <anna.jancso0808@gmail.com>
2018-07-11 13:40:46 +0200

    add empty string case for date cleaner

--- acqdiv/parsers/xml/CHATCleaner.py
+++ acqdiv/parsers/xml/CHATCleaner.py
@@ -255,9 +255,12 @@ class CHATCleaner:
         mapping = {'JAN': '01', 'FEB': '02', 'MAR': '03', 'APR': '04',
                    'MAY': '05', 'JUN': '06', 'JUL': '07', 'AUG': '08',
                    'SEP': '09', 'OCT': '10', 'NOV': '11', 'DEC': '12'}
-        day, month, year = date.split('-')
-        month_clean = mapping[month]
-        return '-'.join([year, month_clean, day])
+        if not date:
+            return ''
+        else:
+            day, month, year = date.split('-')
+            month_clean = mapping[month]
+            return '-'.join([year, month_clean, day])
 
     # ---------- utterance cleaning ----------
 
