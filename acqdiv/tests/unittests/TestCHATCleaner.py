70b75518dee02a5e6fec739605afd5a78147295e
Janis Goldzycher <janisgoldzycher@gmail.com>
2018-07-21 16:43:08 +0200

    implement TestInuktutCleaner

--- acqdiv/tests/unittests/TestCHATCleaner.py
+++ acqdiv/tests/unittests/TestCHATCleaner.py
@@ -1,5 +1,6 @@
 import unittest
 from acqdiv.parsers.xml.CHATCleaner import CHATCleaner
+from acqdiv.parsers.xml.CHATCleaner import InuktitutCleaner
 
 
 class TestCHATCleaner(unittest.TestCase):
@@ -487,7 +488,145 @@ class TestCHATCleaner(unittest.TestCase):
 
 class TestInuktutCleaner(unittest.TestCase):
     """class to test the InuktutCleaner."""
-    pass
+
+    # Tests for the remove_dashes-method.
+
+    def test_remove_dashes_standard_case(self):
+        actual_output = InuktitutCleaner.remove_dashes('Taku-xxx-nga')
+        desired_output = 'Takuxxxnga'
+        self.assertEqual(actual_output, desired_output)
+
+    def test_remove_dashes_empty_string(self):
+        actual_output = InuktitutCleaner.remove_dashes('')
+        desired_output = ''
+        self.assertEqual(actual_output, desired_output)
+
+    # Tests for the clean_word-method.
+
+    def test_clean_word_standard(self):
+        str_input = 'majuratualui'
+        actual_output = InuktitutCleaner.clean_word(str_input)
+        desired_output = 'majuratualui'
+        self.assertEqual(actual_output, desired_output)
+
+    def test_clean_word_utt_with_with_form_markers(self):
+        str_input = 'majuratualui@k'
+        actual_output = InuktitutCleaner.clean_word(str_input)
+        desired_output = 'majuratualui'
+        self.assertEqual(actual_output, desired_output)
+
+    def test_clean_word_utt_with_drawls(self):
+        str_input = 'maju:ratua:lui'
+        actual_output = InuktitutCleaner.clean_word(str_input)
+        desired_output = 'majuratualui'
+        self.assertEqual(actual_output, desired_output)
+
+    def test_clean_word_utt_with_blocking(self):
+        str_input = '^majuratualui'
+        actual_output = InuktitutCleaner.clean_word(str_input)
+        desired_output = 'majuratualui'
+        self.assertEqual(actual_output, desired_output)
+
+    def test_clean_word_utt_with_pauses_between_words(self):
+        str_input = 'maj^uratua^lui'
+        actual_output = InuktitutCleaner.clean_word(str_input)
+        desired_output = 'majuratualui'
+        self.assertEqual(actual_output, desired_output)
+
+    def test_clean_word_utt_mixed_markers(self):
+        str_input = '^maj^uratua:lui@l'
+        actual_output = InuktitutCleaner.clean_word(str_input)
+        desired_output = 'majuratualui'
+        self.assertEqual(actual_output, desired_output)
+
+    def test_clean_word_empty_string(self):
+        actual_output = InuktitutCleaner.clean_word('')
+        desired_output = ''
+        self.assertEqual(actual_output, desired_output)
+
+    # Tests for the clean_xmor-method.
+    # Although the clean_xmor-method removes separators from the
+    # xmor-tier such separators are not tested here, since there
+    # are no occurences of separators in the xmor-tier in the
+    # Inuktut-corpus.
+
+    def test_clean_xmor_untranscribed_and_scoped_symbols_and_terminator(self):
+        str_input = 'xxx ! [+ UI]'
+        actual_output = InuktitutCleaner.clean_xmor(str_input)
+        desired_output = ''
+        self.assertEqual(actual_output, desired_output)
+
+    def test_clean_xmor_scoped_symbol_and_terminator(self):
+        str_input = ('NR|unatartusaq^soldier+NN|AUG|aluk^EMPH+IACT|'
+                     'ai^greetings . [+ IM]')
+        actual_output = InuktitutCleaner.clean_xmor(str_input)
+        desired_output = ('NR|unatartusaq^soldier+NN|AUG|aluk^EMPH+'
+                          'IACT|ai^greetings')
+        self.assertEqual(actual_output, desired_output)
+
+    def test_clean_xmor_empty_string(self):
+        str_input = ''
+        actual_output = InuktitutCleaner.clean_xmor(str_input)
+        desired_output = ''
+        self.assertEqual(actual_output, desired_output)
+
+    # Tests for the clean_xmor-method.
+
+    def test_remove_english_marker_single_marker(self):
+        str_input = 'NR|bag@e^bag NR|killak^hole+NN|lik^item_having .'
+        actual_output = InuktitutCleaner.remove_english_marker(str_input)
+        desired_output = 'NR|bag^bag NR|killak^hole+NN|lik^item_having .'
+        self.assertEqual(actual_output, desired_output)
+
+    def test_remove_english_marker_multiple_markers(self):
+        str_input = 'PRO|you@e^you [/] PRO|you@e^you . [+SR]'
+        actual_output = InuktitutCleaner.remove_english_marker(str_input)
+        desired_output = 'PRO|you^you [/] PRO|you^you . [+SR]'
+        self.assertEqual(actual_output, desired_output)
+
+    def test_remove_english_marker_empty_string(self):
+        str_input = ''
+        actual_output = InuktitutCleaner.remove_english_marker(str_input)
+        desired_output = ''
+        self.assertEqual(actual_output, desired_output)
+
+    def test_replace_stem_gram_gloss_connector_single_connector(self):
+        input_str = 'FIL|am^um DR|u^here&SG_ST+DI|minga^MOD_SG [*] ?'
+        actual_output = InuktitutCleaner.replace_stem_gram_gloss_connector(
+            input_str)
+        desired_output = 'FIL|am^um DR|u^here.SG_ST+DI|minga^MOD_SG [*] ?'
+        self.assertEqual(actual_output, desired_output)
+
+    def test_replace_stem_gram_gloss_connector_multiple_connectors(self):
+        input_str = 'NR|amaama^baby_bottle&BW NR|amaama^baby_bottle&BW'
+        actual_output = InuktitutCleaner.replace_stem_gram_gloss_connector(
+            input_str)
+        desired_output = 'NR|amaama^baby_bottle.BW NR|amaama^baby_bottle.BW'
+        self.assertEqual(actual_output, desired_output)
+
+    def test_replace_stem_gram_gloss_connector_empty_string(self):
+        actual_output = InuktitutCleaner.replace_stem_gram_gloss_connector('')
+        desired_output = ''
+        self.assertEqual(actual_output, desired_output)
+
+    def test_replace_pos_separator_single_separator(self):
+        input_str = 'EXCL|ai^ACT ?'
+        actual_output = InuktitutCleaner.replace_pos_separator(
+            input_str)
+        desired_output = 'EXCL.ai^ACT ?'
+        self.assertEqual(actual_output, desired_output)
+
+    def test_replace_pos_separator_mutiple_separators(self):
+        input_str = 'FIL|am^um DR|u^here&SG_ST+DI|minga^MOD_SG [*] ?'
+        actual_output = InuktitutCleaner.replace_pos_separator(
+            input_str)
+        desired_output = 'FIL.am^um DR.u^here&SG_ST+DI.minga^MOD_SG [*] ?'
+        self.assertEqual(actual_output, desired_output)
+
+    def test_replace_pos_separator_empty_string(self):
+        actual_output = InuktitutCleaner.replace_pos_separator('')
+        desired_output = ''
+        self.assertEqual(actual_output, desired_output)
 
 
 if __name__ == '__main__':
