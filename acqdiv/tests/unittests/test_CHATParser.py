b397ae31ade45490a3062983447811b7a9179208
Janis Goldzycher <janisgoldzycher@gmail.com>
2018-07-30 08:11:33 +0200

    add first batch of methods to TestCHATParser

---
+++ acqdiv/tests/unittests/test_CHATParser.py
@@ -0,0 +1,216 @@
+import unittest
+from acqdiv.parsers.xml.CHATParser import CHATParser
+from acqdiv.parsers.xml.CHATReader import *
+
+
+class TestCHATParser(unittest.TestCase):
+    """Class to test the CHATParser."""
+
+    def setUp(self):
+        self.session_file_path = './test.cha'
+        self.parser = CHATParser(self.session_file_path)
+        self.maxDiff = None
+
+    def test_get_reader(self):
+        """Test get_reader with test.cha."""
+        actual_reader = self.parser.get_reader()
+        desired_reader = ACQDIVCHATReader(self.session_file_path)
+        actual_output = [type(actual_reader), actual_reader.session_file]
+        desired_output = [type(desired_reader), desired_reader.session_file]
+        self.assertEqual(actual_output, desired_output)
+
+    def test_get_cleaner(self):
+        """Test get_cleaner with test.cha."""
+        actual_cleaner = self.parser.get_reader()
+        desired_cleaner = ACQDIVCHATReader(self.session_file_path)
+        actual_output = [type(actual_cleaner), actual_cleaner.session_file]
+        desired_output = [type(desired_cleaner), desired_cleaner.session_file]
+        self.assertEqual(actual_output, desired_output)
+
+    def test_get_session_metadata(self):
+        """Test get_session_metadata with test.cha."""
+        actual_output = self.parser.get_session_metadata()
+        desired_output = {
+            'date': '1997-09-12',
+            'media_filename': 'h2ab'
+        }
+        self.assertEqual(actual_output, desired_output)
+
+    def test_next_speaker(self):
+        """Test next_speaker with test.cha."""
+        actual_output = list(self.parser.next_speaker())
+        mem_dict = {
+            'speaker_label': 'MEM',
+            'name': 'Mme_Manyili',
+            'age_raw': None,
+            'gender_raw': 'female',
+            'role_raw': 'Grandmother',
+            'languages_spoken': 'sme',
+            'birthdate': '1974-10-11'
+        }
+        chi_dict = {
+            'speaker_label': 'CHI',
+            'name': 'Hlobohang',
+            'age_raw': '2;2.',
+            'gender_raw': None,
+            'role_raw': 'Target_Child',
+            'languages_spoken': 'sme',
+            'birthdate': '2006-01-14'
+        }
+        desired_output = [mem_dict, chi_dict]
+        self.assertEqual(actual_output, desired_output)
+
+    def test_next_utterance_test_first_utterance(self):
+        """Test next utterance with the first utterance of test.cha."""
+        actual_output = list(self.parser.next_utterance())[0]
+        utt0_utt_dict = {
+            'source_id': 'u0',
+            'speaker_label': 'MEM',
+            'addressee': '',
+            'utterance_raw': 'ke eng ?',
+            'utterance': 'ke eng',
+            'translation': 'What is it ?',
+            'morpheme': 'ke eng',
+            'gloss_raw': 'cp wh',
+            'pos_raw': None,
+            'sentence_type': 'question',
+            'start_raw': '0',
+            'end_raw': '8551',
+            'comment': None,
+            'warning': None
+        }
+        utt0_word_list = [
+            {
+                'word_language': None,
+                'word': 'ke',
+                'word_actual': 'ke',
+                'word_target': 'ke',
+                'warning': None
+            },
+            {
+                'word_language': None,
+                'word': 'eng',
+                'word_actual': 'eng',
+                'word_target': 'eng',
+                'warning': None
+            }
+        ]
+        utt0_morpheme_list = [
+            {
+                'morpheme_language': None,
+                'morpheme': 'ke',
+                'gloss_raw': 'cp',
+                'pos_raw': None
+            },
+            {
+                'morpheme_language': None,
+                'morpheme': 'eng',
+                'gloss_raw': 'wh',
+                'pos_raw': None
+            }
+        ]
+        desired_output = (utt0_utt_dict, utt0_word_list, utt0_morpheme_list)
+        self.assertEqual(actual_output, desired_output)
+
+    def test_next_utterance_last_utterance_all_tiers_misaligned(self):
+        """Test next_utterance with the last utterance of test.cha.
+
+        The utterance contains three words, but only one gloss word
+        and two words in the coding tier.
+
+        TODO: what should be the number of None-morphemes?
+        """
+        actual_output = list(self.parser.next_utterance())[4]
+        utt4_utt_dict = {
+            'source_id': 'u0',
+            'speaker_label': 'MEM',
+            'addressee': '',
+            'utterance_raw': 'ke eng ?',
+            'utterance': 'ke eng',
+            'translation': 'What is it ?',
+            'morpheme': 'ke eng',
+            'gloss_raw': 'cp wh',
+            'pos_raw': None,
+            'sentence_type': 'question',
+            'start_raw': '0',
+            'end_raw': '8551',
+            'comment': None,
+            'warning': None
+        }
+        utt4_word_list = [
+            {
+                'word_language': None,
+                'word': 'ke',
+                'word_actual': 'ke',
+                'word_target': 'ke',
+                'warning': None
+            },
+            {
+                'word_language': None,
+                'word': 'khomba',
+                'word_actual': 'khomba',
+                'word_target': 'khomba',
+                'warning': None
+            },
+            {
+                'word_language': None,
+                'word': 'khomba',
+                'word_actual': 'khomba',
+                'word_target': 'khomba',
+                'warning': None
+            }
+        ]
+        utt4_morpheme_list = [
+            {
+                'morpheme_language': None,
+                'morpheme': None,
+                'gloss_raw': None,
+                'pos_raw': None
+            },
+            {
+                'morpheme_language': None,
+                'morpheme': None,
+                'gloss_raw': None,
+                'pos_raw': None
+            }
+        ]
+        desired_output = (utt4_utt_dict, utt4_word_list, utt4_morpheme_list)
+        self.assertEqual(actual_output, desired_output)
+
+    def test_next_utterance_misalignment_words_tier(self):
+        """Test next_utterance with a words vs morph misalignment.
+
+        There are two words in the utterance, but 3 segment-, gloss-
+        and pos-words.
+        """
+        self.parser.reader._main_line_fields = ('CHI', 'Iden nüd', '', '')
+        self._dependent_tiers = {
+            'xgls': 'Iden nüd ga',
+            'xcod': 'n^name pra^1sg'
+        }
+
+    def test_next_utterance_misalignment_gloss_tier(self):
+        """Test next_utterance with a gloss vs other tiers misalignment.
+
+        There is one gloss-word but two words on all other tiers.
+        """
+        pass
+
+    def test_next_utterance_misalignments_segments_tier(self):
+        """Test next_utterance with segment vs other tiers misalignemnt.
+
+        There is one segment word but two words on all other tiers.
+        """
+        pass
+
+    def test_next_utterance_misalignment_pos_tier(self):
+        """Test next_utterance with pos vs other tiers misalignment.
+
+        There is one pos-word but two words on all other tiers.
+        """
+        pass
+
+
+
+if __name__ == '__main__':
+    unittest.main()
