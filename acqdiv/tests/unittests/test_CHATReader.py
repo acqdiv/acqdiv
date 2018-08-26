68addb6db9950b9fc308a29e6e4b2762883483e3
Janis Goldzycher <janisgoldzycher@gmail.com>
2018-08-26 19:15:19 +0200

    add TestSesothoReader

--- acqdiv/tests/unittests/test_CHATReader.py
+++ acqdiv/tests/unittests/test_CHATReader.py
@@ -6,6 +6,7 @@ from acqdiv.parsers.xml.CHATReader import InuktitutReader
 from acqdiv.parsers.xml.CHATReader import JapaneseMiiProReader
 from acqdiv.parsers.xml.CHATReader import CreeReader
 from acqdiv.parsers.xml.CHATReader import EnglishManchester1Reader
+from acqdiv.parsers.xml.CHATReader import SesothoReader
 from acqdiv.parsers.xml.CHATReader import TurkishReader
 from acqdiv.parsers.xml.CHATReader import YucatecReader
 
@@ -1811,6 +1812,213 @@ class TestJapaneseMiiProReader(unittest.TestCase):
                           ('=cmpstemtwo', 'cmpgloss', 'CMPPOSTWO'),
                           ('', 'SFXTWO', 'sfx')]
         self.assertEqual(actual_output, desired_output)
+
+###############################################################################
+
+
+class TestSesothoReader(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.reader = SesothoReader()
+        cls.maxDiff = None
+
+    def test_join_morph_to_utt_only_hyphens(self):
+        """Test join_morph_to_utt with standard case of only hyphens."""
+        record = ('*CHI:\tplaceholder\n%gls:\ter-e m-ph-e ntho ena .\n%cod:'
+                  '\tplaceholder\n@End')
+        self.reader.read(io.StringIO(record))
+        self.reader.load_next_record()
+        actual_output = self.reader._join_morph_to_utt()
+        desired_output = 'ere mphe ntho ena .'
+        self.assertEqual(actual_output, desired_output)
+
+    def test_join_morph_to_utt_hyphens_and_parentheses(self):
+        """Test join_morph_to_utt with hyphens and parentheses."""
+        record = ('*CHI:\tplaceholder\n%gls:\ter-e m-ph-e (ag)ntho ena .\n%cod:'
+                  '\tplaceholder\n@End')
+        self.reader.read(io.StringIO(record))
+        self.reader.load_next_record()
+        actual_output = self.reader._join_morph_to_utt()
+        desired_output = 'ere mphe (ag)ntho ena .'
+        self.assertEqual(actual_output, desired_output)
+
+    def test_join_morph_to_utt_empty_string(self):
+        """Test join_morph_to_utt with an empty string.
+
+        The wrong naming of the gloss tier leads to an empty string
+        to be processed.
+        """
+        record = ('*CHI:\tplaceholder\n%gla:\ter-e m-ph-e (ag)ntho ena .\n%cod:'
+                  '\tplaceholder\n@End')
+        self.reader.read(io.StringIO(record))
+        self.reader.load_next_record()
+        actual_output = self.reader._join_morph_to_utt()
+        desired_output = ''
+        self.assertEqual(actual_output, desired_output)
+
+    def test_get_utterance(self):
+        """Test get_utterance with standard case of only hyphens."""
+        record = ('*CHI:\tplaceholder\n%gls:\ter-e m-ph-e ntho ena .\n%cod:'
+                  '\tplaceholder\n@End')
+        self.reader.read(io.StringIO(record))
+        self.reader.load_next_record()
+        actual_output = self.reader.get_utterance()
+        desired_output = 'ere mphe ntho ena .'
+        self.assertEqual(actual_output, desired_output)
+
+    def test_get_actual_utterance(self):
+        """Test get_actual_utterance with standard case of only hyphens."""
+        record = ('*CHI:\tplaceholder\n%gls:\ter-e m-ph-e ntho ena .\n%cod:'
+                  '\tplaceholder\n@End')
+        self.reader.read(io.StringIO(record))
+        self.reader.load_next_record()
+        actual_output = self.reader.get_utterance()
+        desired_output = 'ere mphe ntho ena .'
+        self.assertEqual(actual_output, desired_output)
+
+    def test_get_target_utterance(self):
+        """Test get_target_utterance with standard case of only hyphens."""
+        record = ('*CHI:\tplaceholder\n%gls:\ter-e m-ph-e ntho ena .\n%cod:'
+                  '\tplaceholder\n@End')
+        self.reader.read(io.StringIO(record))
+        self.reader.load_next_record()
+        actual_output = self.reader.get_utterance()
+        desired_output = 'ere mphe ntho ena .'
+        self.assertEqual(actual_output, desired_output)
+
+    def test_infer_poses_noun(self):
+        """Test infer_poses with a noun containing a prefix."""
+        morph_word = 'n^6-eye(5 , 6)'
+        actual_output = self.reader.infer_poses(morph_word)
+        desired_output = ['pfx', 'n']
+        self.assertEqual(actual_output, desired_output)
+
+    def test_infer_poses_verb(self):
+        """Test infer_poses with a verb.
+
+        The verb contains 2 suffixes and one prefix.
+        """
+        morph_word = 'sm2s-t^f1-v^say-m^in'
+        actual_output = self.reader.infer_poses(morph_word)
+        desired_output = ['pfx', 'pfx', 'v', 'sfx']
+        self.assertEqual(actual_output, desired_output)
+
+    def test_infer_poses_nominal_concord(self):
+        """Test infer_poses with a nominal concord."""
+        morph_word = 'obr3'
+        actual_output = self.reader.infer_poses(morph_word)
+        desired_output = ['obr']
+        self.assertEqual(actual_output, desired_output)
+
+    def test_infer_poses_particle(self):
+        """Test infer_poses with a particle."""
+        morph_word = 'loc'
+        actual_output = self.reader.infer_poses(morph_word)
+        desired_output = ['loc']
+        self.assertEqual(actual_output, desired_output)
+
+    def test_infer_poses_free_person_marker(self):
+        """Test infer_poses with a free person marker."""
+        morph_word = 'sm1s'
+        actual_output = self.reader.infer_poses(morph_word)
+        desired_output = ['afx.detached']
+        self.assertEqual(actual_output, desired_output)
+
+    def test_infer_poses_copula(self):
+        """Test infer_poses with a copula."""
+        morph_word = 'cp'
+        actual_output = self.reader.infer_poses(morph_word)
+        desired_output = ['cop']
+        self.assertEqual(actual_output, desired_output)
+
+    def test_infer_poses_ideophone(self):
+        """Test infer_poses with an ideophone."""
+        morph_word = 'id^jump'
+        actual_output = self.reader.infer_poses(morph_word)
+        desired_output = ['ideoph']
+        self.assertEqual(actual_output, desired_output)
+
+    def test_infer_poses_untranscibed(self):
+        """Test infer_poses with an untranscribed morpheme word."""
+        morph_word = 'xxx'
+        actual_output = self.reader.infer_poses(morph_word)
+        desired_output = ['none']
+        self.assertEqual(actual_output, desired_output)
+
+    def test_infer_poses_empty_string(self):
+        """Test infer_poses with an empty string."""
+        morph_word = ''
+        actual_output = self.reader.infer_poses(morph_word)
+        desired_output = []
+        self.assertEqual(actual_output, desired_output)
+
+    def test_iter_morphemes_single(self):
+        """Test iter_morphemes with a morpheme word containing one morpheme."""
+        morph_word = 'id^jump'
+        actual_output = list(self.reader.iter_morphemes(morph_word))
+        desired_output = [('', 'id^jump', 'ideoph')]
+        self.assertEqual(actual_output, desired_output)
+
+    def test_iter_morphemes_multiple(self):
+        """Test iter_morphemes with a morpheme word containing 4 morphemes."""
+        morph_word = 'sm1-t^p-v^say-m^in'
+        actual_output = list(self.reader.iter_morphemes(morph_word))
+        desired_output = [('', 'sm1', 'pfx'),
+                          ('', 't^p', 'pfx'),
+                          ('', 'v^say', 'v'),
+                          ('', 'm^in', 'sfx')]
+        self.assertEqual(actual_output, desired_output)
+
+    def test_iter_morphemes_empty_string(self):
+        """Test iter_morphemes with an empty string."""
+        morph_word = ''
+        actual_output = list(self.reader.iter_morphemes(morph_word))
+        desired_output = [('', '', '')]
+        self.assertEqual(actual_output, desired_output)
+
+    def test_get_segments_standard_case(self):
+        """Test get_segments with hyphen separated segment word."""
+        seg_word = 'prefix-stem-affix'
+        actual_output = self.reader.get_segments(seg_word)
+        desired_output = ['prefix', 'stem', 'affix']
+        self.assertEqual(actual_output, desired_output)
+
+    def test_get_segments_empty_string(self):
+        """Test get_segments with an empty string."""
+        seg_word = ''
+        actual_output = self.reader.get_segments(seg_word)
+        desired_output = ['']
+        self.assertEqual(actual_output, desired_output)
+
+    def test_get_glosses_standard_case(self):
+        """Test get_glosses with  hyphen separated gloss word."""
+        seg_word = 'sm1-t^p-v^say-m^in'
+        actual_output = self.reader.get_glosses(seg_word)
+        desired_output = ['sm1', 't^p', 'v^say', 'm^in']
+        self.assertEqual(actual_output, desired_output)
+
+    def test_get_glosses_empty_string(self):
+        """Test get_glosses with an empty string."""
+        seg_word = ''
+        actual_output = self.reader.get_segments(seg_word)
+        desired_output = ['']
+        self.assertEqual(actual_output, desired_output)
+
+    def test_get_poses_standard_case(self):
+        """Test get_poses with a hyphen separated pos word."""
+        seg_word = 'sm1-t^p-v^say-m^in'
+        actual_output = self.reader.get_poses(seg_word)
+        desired_output = ['pfx', 'pfx', 'v', 'sfx']
+        self.assertEqual(actual_output, desired_output)
+
+    def test_get_poses_empty_string(self):
+        """Test get_poses with an empty string."""
+        seg_word = ''
+        actual_output = self.reader.get_poses(seg_word)
+        desired_output = ['']
+        self.assertEqual(actual_output, desired_output)
+
 
 ###############################################################################
 
