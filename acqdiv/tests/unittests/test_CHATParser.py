64c8b5b32a89a6ce3a9fc79fd74a719bcf8829dd
Anna Jancso <anna.jancso0808@gmail.com>
2018-09-07 14:33:38 +0200

    Chintang parser tests

--- acqdiv/tests/unittests/test_CHATParser.py
+++ acqdiv/tests/unittests/test_CHATParser.py
@@ -3,6 +3,8 @@ import unittest
 from acqdiv.parsers.xml.CHATParser import *
 from acqdiv.parsers.xml.CHATReader import *
 from acqdiv.parsers.xml.CHATCleaner import *
+from acqdiv.parsers.parsers import ToolboxParser
+from acqdiv.parsers.parsers import CorpusConfigParser
 
 
 class TestCHATParser(unittest.TestCase):
@@ -1556,6 +1558,103 @@ class TestSesothoParser(unittest.TestCase):
             ]
         ]
         desired_output = (utt_dict, words_list, morpheme_list)
+        self.assertEqual(actual_output, desired_output)
+
+
+###############################################################################
+
+
+class ChintangParser(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        config = CorpusConfigParser()
+        config.read('../../ini/Chintang.ini')
+        file_path = 'test_files/Chintang.txt'
+        cls.parser = ToolboxParser(config, file_path)
+
+    def test_get_session_metadata(self):
+        actual_output = self.parser.get_session_metadata()
+        desired_output = {
+            'id': 'Chintang',
+            'date': 'session date',
+            'genre': 'genre',
+            'location': {},
+            'situation': 'situation',
+            'media_type': None
+        }
+        self.assertEqual(actual_output, desired_output)
+
+    def test_next_speaker(self):
+        actual_output = next(self.parser.next_speaker())
+        desired_output = {
+            'role': 'actor family social role',
+            'name': 'actor name',
+            'fullname': 'actor fullname',
+            'code': 'actor code',
+            'familysocialrole': 'actor family social role',
+            'languages': 'actor language id',
+            'ethnicgroup': 'actor ethnic group',
+            'age': 'actor age',
+            'birthdate': 'actor birthdate',
+            'sex': 'actor sex',
+            'education': 'actor education'}
+        self.assertEqual(actual_output, desired_output)
+
+    def test_next_utterance(self):
+        actual_output = next(self.parser.next_utterance())
+
+        utterance = {
+            'source_id': 'session_name.001',
+            'start_raw': '00:50:11.150',
+            'end_raw': '00:50:22.350',
+            'speaker_label': 'MAR',
+            'childdirected': True,
+            'utterance_raw': 'w1 w2',
+            'utterance': 'w1 w2',
+            'sentence_type': 'question',
+            'lemma_id': 'w1pfxID- w1stemID -w1sfxID w2stemID',
+            'morpheme': 'w1pfxseg- w1stemseg -w1sfxseg w2stemseg',
+            'gloss_raw': 'w1pfxgloss- w1stemgloss -w1sfxgloss w2stemgloss',
+            'pos_raw': 'w1pfxpos- w1stempos -w1sfxpos w2stempos',
+            'translation': 'This is the translation.'}
+
+        words = [
+            {'word': 'w1', 'word_actual': 'w1', 'word_language': 'Chintang'},
+            {'word': 'w2', 'word_actual': 'w2', 'word_language': 'Nepali'}]
+
+        morphemes = [
+            [
+                {'morpheme': 'w1pfxseg-',
+                 'gloss_raw': 'w1pfxgloss-',
+                 'pos_raw': 'w1pfxpos-',
+                 'morpheme_language': 'Chintang',
+                 'lemma_id': 'w1pfxID-',
+                 'type': 'target',
+                 'warning': None},
+                {'morpheme': 'w1stemseg',
+                 'gloss_raw': 'w1stemgloss',
+                 'pos_raw': 'w1stempos',
+                 'morpheme_language': 'Chintang',
+                 'lemma_id': 'w1stemID',
+                 'type': 'target',
+                 'warning': None},
+                {'morpheme': '-w1sfxseg',
+                 'gloss_raw': '-w1sfxgloss',
+                 'pos_raw': '-w1sfxpos',
+                 'morpheme_language': 'Chintang',
+                 'lemma_id': '-w1sfxID',
+                 'type': 'target',
+                 'warning': None}],
+            [
+                {'morpheme': 'w2stemseg',
+                 'gloss_raw': 'w2stemgloss',
+                 'pos_raw': 'w2stempos',
+                 'morpheme_language': 'Nepali',
+                 'lemma_id': 'w2stemID',
+                 'type': 'target',
+                 'warning': None}]]
+        desired_output = (utterance, words, morphemes)
         self.assertEqual(actual_output, desired_output)
 
 
