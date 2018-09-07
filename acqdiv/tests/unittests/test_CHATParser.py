df0044ca91841cb70a36d672bd404571451ef9e4
Anna Jancso <anna.jancso0808@gmail.com>
2018-09-07 17:17:17 +0200

    Russian parser tests

--- acqdiv/tests/unittests/test_CHATParser.py
+++ acqdiv/tests/unittests/test_CHATParser.py
@@ -1564,7 +1564,7 @@ class TestSesothoParser(unittest.TestCase):
 ###############################################################################
 
 
-class ChintangParser(unittest.TestCase):
+class TestChintangParser(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
@@ -1602,7 +1602,7 @@ class ChintangParser(unittest.TestCase):
         self.assertEqual(actual_output, desired_output)
 
     def test_next_utterance(self):
-        actual_output = next(self.parser.next_utterance())
+        actual_output = list(self.parser.next_utterance())
 
         utterance = {
             'source_id': 'session_name.001',
@@ -1654,13 +1654,13 @@ class ChintangParser(unittest.TestCase):
                  'lemma_id': 'w2stemID',
                  'type': 'target',
                  'warning': None}]]
-        desired_output = (utterance, words, morphemes)
+        desired_output = [(utterance, words, morphemes)]
         self.assertEqual(actual_output, desired_output)
 
 ###############################################################################
 
 
-class IndonesianParser(unittest.TestCase):
+class TestIndonesianParser(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
@@ -1746,6 +1746,99 @@ class IndonesianParser(unittest.TestCase):
         desired_output = [(None, None, None),
                           (None, None, None),
                           (utterance, words, morphemes)]
+        self.assertEqual(actual_output, desired_output)
+
+###############################################################################
+
+
+class TestRussianParser(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        config = CorpusConfigParser()
+        config.read('../../ini/Russian.ini')
+        file_path = 'test_files/Russian.txt'
+        cls.parser = ToolboxParser(config, file_path)
+
+    def test_get_session_metadata(self):
+        actual_output = self.parser.get_session_metadata()
+        desired_output = {
+            'id': 'Russian',
+            'date': 'session date',
+            'genre': 'genre',
+            'location': {},
+            'situation': 'situation',
+            'media_type': None
+        }
+        self.assertEqual(actual_output, desired_output)
+
+    def test_next_speaker(self):
+        actual_output = next(self.parser.next_speaker())
+        desired_output = {
+            'role': 'actor role',
+            'name': 'actor name',
+            'fullname': 'actor fullname',
+            'code': 'actor code',
+            'familysocialrole': 'actor family social role',
+            'languages': 'actor language id',
+            'ethnicgroup': 'actor ethnic group',
+            'age': 'actor age',
+            'birthdate': 'actor birthdate',
+            'sex': 'actor sex',
+            'education': 'actor education'}
+        self.assertEqual(actual_output, desired_output)
+
+    def test_next_utterance(self):
+        actual_output = list(self.parser.next_utterance())
+
+        utterance = {
+            'source_id': 'source_id',
+            'start_raw': 'start_raw',
+            'end_raw': 'end_raw',
+            'speaker_label': 'speaker_label',
+            'utterance_raw': 'w1 "," w2 w3 .',
+            'utterance': 'w1 w2 w3',
+            'sentence_type': 'default',
+            'morpheme': 'lem1 "," lem2 lem3 .',
+            'gloss_raw': 'PST:SG:F:IRREFL:IPFV NOM:SG PCL',
+            'pos_raw': 'V-PST:SG:F:IRREFL:IPFV PUNCT PRO-DEM-NOUN:NOM:SG PCL '
+                       'PUNCT'}
+
+        words = [
+            {'word': 'w1', 'word_actual': 'w1', 'word_language': 'Russian'},
+            {'word': 'w2', 'word_actual': 'w2', 'word_language': 'Russian'},
+            {'word': 'w3', 'word_actual': 'w3', 'word_language': 'Russian'}]
+
+        morphemes = [
+            [
+                {'morpheme': 'lem1',
+                 'gloss_raw': 'PST:SG:F:IRREFL:IPFV',
+                 'pos_raw': 'V',
+                 'morpheme_language': 'Russian',
+                 'lemma_id': None,
+                 'type': 'actual',
+                 'warning': None}
+            ],
+            [
+                {'morpheme': 'lem2',
+                 'gloss_raw': 'NOM:SG',
+                 'pos_raw': 'PRO-DEM-NOUN',
+                 'morpheme_language': 'Russian',
+                 'lemma_id': None,
+                 'type': 'actual',
+                 'warning': None}
+            ],
+            [
+                {'morpheme': 'lem3',
+                 'gloss_raw': 'PCL',
+                 'pos_raw': 'PCL',
+                 'morpheme_language': 'Russian',
+                 'lemma_id': None,
+                 'type': 'actual',
+                 'warning': None}
+            ]
+        ]
+        desired_output = [(utterance, words, morphemes)]
         self.assertEqual(actual_output, desired_output)
 
 
