043f9817ee64c0f42953003f33672d3657b67bc2
Anna Jancso <anna.jancso0808@gmail.com>
2018-09-07 14:35:33 +0200

    Indonesian parser tests

--- acqdiv/tests/unittests/test_CHATParser.py
+++ acqdiv/tests/unittests/test_CHATParser.py
@@ -1657,6 +1657,97 @@ class ChintangParser(unittest.TestCase):
         desired_output = (utterance, words, morphemes)
         self.assertEqual(actual_output, desired_output)
 
+###############################################################################
+
+
+class IndonesianParser(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        config = CorpusConfigParser()
+        config.read('../../ini/Indonesian.ini')
+        file_path = 'test_files/Indonesian.txt'
+        cls.parser = ToolboxParser(config, file_path)
+
+    def test_get_session_metadata(self):
+        actual_output = self.parser.get_session_metadata()
+        desired_output = {
+            'Cname': 'Indonesian',
+            'Corpus': 'Corpus',
+            'Date': 'Date',
+            'Id': 'Id',
+            'Lang': 'Lang',
+            'PID': 'PID'}
+        self.assertEqual(actual_output, desired_output)
+
+    def test_next_speaker(self):
+        actual_output = next(self.parser.next_speaker())
+        desired_output = {
+            'age': 'age',
+            'birthday': 'birthday',
+            'id': 'id',
+            'language': 'language',
+            'name': 'name',
+            'role': 'role',
+            'sex': 'sex'}
+        self.assertEqual(actual_output, desired_output)
+
+    def test_next_utterance(self):
+        actual_output = list(self.parser.next_utterance())
+
+        utterance = {
+            'source_id': 'source_id',
+            'start_raw': 'start_raw',
+            'speaker_label': 'speaker_label',
+            'utterance_raw': 'w(ord)1 w2.',
+            'utterance': 'w(ord)1 w2',
+            'sentence_type': 'default',
+            'morpheme': 'w1pfxseg- w1stemseg -w1sfxseg w2stemseg',
+            'gloss_raw': 'w1pfxgloss- w1stemgloss -w1sfxgloss w2stemgloss',
+            'translation': 'This is the translation.'}
+
+        words = [
+            {'word': 'w1', 'word_actual': 'w1', 'word_target': 'word1',
+             'word_language': 'Indonesian'},
+            {'word': 'w2', 'word_actual': 'w2', 'word_target': 'w2',
+             'word_language': 'Indonesian'}]
+
+        morphemes = [
+            [
+                {'morpheme': 'w1pfxseg-',
+                 'gloss_raw': 'w1pfxgloss-',
+                 'pos_raw': None,
+                 'morpheme_language': 'Indonesian',
+                 'lemma_id': None,
+                 'type': 'target',
+                 'warning': None},
+                {'morpheme': 'w1stemseg',
+                 'gloss_raw': 'w1stemgloss',
+                 'pos_raw': None,
+                 'morpheme_language': 'Indonesian',
+                 'lemma_id': None,
+                 'type': 'target',
+                 'warning': None},
+                {'morpheme': '-w1sfxseg',
+                 'gloss_raw': '-w1sfxgloss',
+                 'pos_raw': None,
+                 'morpheme_language': 'Indonesian',
+                 'lemma_id': None,
+                 'type': 'target',
+                 'warning': None}],
+            [
+                {'morpheme': 'w2stemseg',
+                 'gloss_raw': 'w2stemgloss',
+                 'pos_raw': None,
+                 'morpheme_language': 'Indonesian',
+                 'lemma_id': None,
+                 'type': 'target',
+                 'warning': None}]]
+        desired_output = [(None, None, None),
+                          (None, None, None),
+                          (utterance, words, morphemes)]
+        self.assertEqual(actual_output, desired_output)
+
 
 if __name__ == '__main__':
     unittest.main()
