397421c2651c2eef5625173387cc7de410c4fde3
Anna Jancso <anna.jancso0808@gmail.com>
2018-09-14 23:07:44 +0200

    russian refactor done

--- acqdiv/parsers/ToolboxReader.py
+++ acqdiv/parsers/ToolboxReader.py
@@ -40,6 +40,7 @@ class ToolboxReader(object):
     _separator = re.compile(b'\r?\n\r?\n(\r?\n)')
     _record_marker = re.compile(br'\\ref')
     _word_boundary = re.compile('(?<![\-\s])\s+(?![\-\s])')
+    warnings = []
 
     def __init__(self, config, file_path):
         """ Initializes a Toolbox file object
@@ -51,7 +52,6 @@ class ToolboxReader(object):
         self.config = config
         self.path = file_path
         self.tier_separator = re.compile(b'\n')
-        self.warnings = []
 
         # get database column names
         self.field_markers = []
@@ -169,7 +169,7 @@ class ToolboxReader(object):
         if utterance['utterance'] is None:
             morphemes = []
         else:
-            morphemes = self.get_morphemes(utterance)
+            morphemes = self.get_all_morphemes(utterance)
 
         for i in range(len(words)):
             try:
@@ -987,6 +987,115 @@ class RussianReader(ToolboxReader):
             utterance = utterance.strip()
 
         return utterance
+
+    # ---------- tier ----------
+
+    @staticmethod
+    def remove_seg_punctuation(seg_tier):
+        return re.sub('[‘’\'“”\".!,:\-?+/]', '', seg_tier)
+
+    @staticmethod
+    def unify_unknown(seg_tier):
+        return re.sub('xxx?|www', '???', seg_tier)
+
+    @classmethod
+    def clean_seg_tier(cls, seg_tier):
+        for cleaning_method in [cls.remove_seg_punctuation, cls.unify_unknown]:
+            seg_tier = cleaning_method(seg_tier)
+
+        return seg_tier
+
+    @classmethod
+    def get_gloss_tier(cls, utterance):
+        return utterance.get('pos_raw', '')
+
+    @staticmethod
+    def clean_gloss_pos_punctuation(gloss_pos_tier):
+        return gloss_pos_tier.replace('PUNCT', '').replace('ANNOT', '').\
+            replace('<NA: lt;> ', '')
+
+    @classmethod
+    def clean_gloss_tier(cls, gloss_tier):
+        return cls.clean_gloss_pos_punctuation(gloss_tier)
+
+    @classmethod
+    def clean_pos_tier(cls, pos_tier):
+        return cls.clean_gloss_pos_punctuation(pos_tier)
+
+    @classmethod
+    def get_lang_tier(cls, utterance):
+        tier = utterance.get('pos_raw')
+        # TODO: consider adding cleaners for language morpheme tier
+        return cls.clean_gloss_pos_punctuation(tier)
+
+    # ---------- morpheme words ----------
+
+    @classmethod
+    def get_morpheme_words(cls, morpheme_tier):
+        return morpheme_tier.split()
+
+    @classmethod
+    def iter_gloss_pos_words(cls, gloss_pos_tier):
+        """Iter gloss and POS tag of a word.
+
+        Tier \mor contains both glosses and POS, # separated by "-" or ":".
+        """
+        if not gloss_pos_tier:
+            cls.warnings.append('not glossed')
+        else:
+            words = cls.get_morpheme_words(gloss_pos_tier)
+            for word in words:
+                # 1) If there is no ":" in a word string, gloss and POS are
+                # identical (most frequently the case with PCL 'particle').
+                if ':' not in word:
+                    gloss_word = word
+                    pos_word = word
+                # 2) Sub-POS are always separated by "-" (e.g. PRO-DEM-NOUN),
+                # subglosses are always separated by ":" (e.g. PST:SG:F).
+                # What varies, though, is the character that separates POS from
+                # glosses in the word: If the POS is V ('verb') or ADJ
+                # ('adjective'), the glosses start behind the first "-", e.g.
+                # V-PST:SG:F:IRREFL:IPFV -> POS V, gloss PST.SG.F.IRREFL.IPFV
+                elif word.startswith('V') or word.startswith('ADJ'):
+                    match_verb_adj = re.search('(V|ADJ)-(.*$)', word)
+                    if match_verb_adj:
+                        gloss_word = match_verb_adj.group(2)
+                        pos_word = match_verb_adj.group(1)
+                    else:
+                        continue
+                # 3) For all other POS, the glosses start behind the first ":",
+                # e.g. PRO-DEM-NOUN:NOM:SG -> POS PRO.DEM.NOUN, gloss NOM.SG
+                else:
+                    match_gloss_pos = re.search('(^[^(V|ADJ)].*?):(.*$)',word)
+                    if match_gloss_pos:
+                        gloss_word = match_gloss_pos.group(2)
+                        pos_word = match_gloss_pos.group(1)
+                    else:
+                        continue
+
+                yield gloss_word, pos_word
+
+    @classmethod
+    def get_gloss_words(cls, gloss_tier):
+        return [gloss_w for gloss_w, _ in cls.iter_gloss_pos_words(gloss_tier)]
+
+    @classmethod
+    def get_pos_words(cls, pos_tier):
+        return [pos_w for _, pos_w in cls.iter_gloss_pos_words(pos_tier)]
+
+    # ---------- morpheme ----------
+
+    @classmethod
+    def get_morphemes(cls, morpheme_word):
+        return [morpheme_word]
+
+    @classmethod
+    def get_langs(cls, morpheme_lang_word):
+        if 'FOREIGN' in morpheme_lang_word:
+            # TODO: does it make sense to NULL this?
+            return [None]
+        else:
+            return ['Russian']
 
 
 def main():
