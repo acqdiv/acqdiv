f3dff427c18db4b3039d109f87613f5f99d02e9d
Janis Goldzycher <janisgoldzycher@gmail.com>
2018-09-15 14:24:34 +0200

    refactor and adjust inference of pos tags

--- acqdiv/parsers/xml/CHATReader.py
+++ acqdiv/parsers/xml/CHATReader.py
@@ -1509,6 +1509,10 @@ class SesothoReader(ACQDIVCHATReader):
     words and those to the utterance.
     """
 
+    def __init__(self):
+        super().__init__()
+        self._passed_stem = False
+
     def _join_morph_to_utt(self):
         """Create the utterance by joining the morphemes to words.
 
@@ -1550,115 +1554,106 @@ class SesothoReader(ACQDIVCHATReader):
     def get_pos_tier(self):
         return self._dependent_tiers.get('cod', '')
 
-    @staticmethod
-    def infer_poses(morph_word):
-        """Infer the pos-tags for a morpheme word.
+    def infer_pos(self, gloss, num_morphemes):
+        """Infer the pos-tag for a gloss.
 
         Args:
-            morph_word: str, the morphemes of a word
+            gloss: str, the gloss
+            num_morphemes: int, the number of morphemes in the
+                morpheme word
         Return:
-            str, the pos-tags
+            str, the pos-tag
         """
-        if not morph_word:
-            return []
+        if not gloss:
+            return ''
 
-        morphemes = morph_word.split('-')
-        passed_stem = False
-        poses = []
+        pos = ''
+        # Check for prefixes and suffixes.
+        if num_morphemes == 1 or (re.search(r'(v|id)\^|\(\d', gloss)
+                                  or re.match(r'(aj$|nm$|ps\d+)', gloss)):
+            self._passed_stem = True
+            # Check for verbs: verbs have v^, one typo as s^.
+            if re.search(r'[vs]\^', gloss):
+                pos = 'v'
 
-        for mor in morphemes:
-            pos = ''
-            # Check for prefixes and suffixes.
-            if len(morphemes) == 1 or (re.search(r'(v|id)\^|\(\d', mor)
-                                       or re.match(r'(aj$|nm$|ps\d+)', mor)):
-                passed_stem = True
-            elif not passed_stem:
-                pos = 'pfx'
-            elif passed_stem:
-                pos = 'sfx'
+            # Check for nouns: nouns contains "(\d)" (default) or "ps/"
+            elif re.search('\(\d+', gloss) or re.search('^ps/', gloss):
+                pos = 'n'
 
-            if pos != 'pfx' and pos != 'sfx':
-                # Check for verbs: verbs have v^, one typo as s^.
-                if re.search(r'[vs]\^', mor):
-                    pos = 'v'
+            # Check for words with nominal concord.
+            elif re.search(r'^(d|lr|obr|or|pn|ps|sr)\d+', gloss):
+                pos_match = re.search('^(d|lr|obr|or|pn|ps|sr)\d+', gloss)
+                pos = pos_match.group(1)
 
-                # Check for nouns: nouns contains "(\d)" (default) or "ps/"
-                elif re.search('\(\d+', mor) or re.search('^ps/', mor):
-                    pos = 'n'
+            # Check for particles: mostly without a precise gloss.
+            elif re.search(
+                    (r'^(aj|av|cd|cj|cm|ht|ij|loc|lr|ng|nm|obr|or|pr|q|sr'
+                     r'|wh)$'),
+                    gloss):
+                pos = gloss
 
-                # Check for words with nominal concord.
-                elif re.search(r'^(d|lr|obr|or|pn|ps|sr)\d+', mor):
-                    pos_match = re.search('^(d|lr|obr|or|pn|ps|sr)\d+', mor)
-                    pos = pos_match.group(1)
+            # Check for free person markers.
+            elif re.search(r'^sm\d+[sp]?$', gloss):
+                pos = 'afx.detached'
 
-                # Check for particles: mostly without a precise gloss.
-                elif re.search(
-                        (r'^(aj|av|cd|cj|cm|ht|ij|loc|lr|ng|nm|obr|or|pr|q|sr'
-                         r'|wh)$'),
-                        mor):
-                    pos = mor
+            # Check for copulas.
+            elif re.search(r'^cp|cp$', gloss):
+                pos = 'cop'
 
-                # Check for free person markers.
-                elif re.search(r'^sm\d+[sp]?$', mor):
-                    pos = 'afx.detached'
+            # Check for ideophones.
+            elif re.search(r'id\^', gloss):
+                pos = 'ideoph'
 
-                # Check for copulas.
-                elif re.search(r'^cp|cp$', mor):
-                    pos = 'cop'
+            # Check for meaningless and unclear words. Note that
+            # "xxx" in the Sesotho coding tier is not the same as
+            # CHAT "xxx" in the transcription tier - it does not
+            # stand for words that could not be transcribed but for
+            # words with unclear meaning.
+            elif gloss == 'word' or gloss == 'xxx':
+                pos = 'none'
+            else:
+                pos = '???'
 
-                # Check for ideophones.
-                elif re.search(r'id\^', mor):
-                    pos = 'ideoph'
+        elif not self._passed_stem:
+            pos = 'pfx'
+        elif self._passed_stem:
+            pos = 'sfx'
 
-                # Check for meaningless and unclear words. Note that
-                # "xxx" in the Sesotho coding tier is not the same as
-                # CHAT "xxx" in the transcription tier - it does not
-                # stand for words that could not be transcribed but for
-                # words with unclear meaning.
-                elif mor == 'word' or mor == 'xxx':
-                    pos = 'none'
-                else:
-                    pos = '???'
+        return pos
 
-            poses.append(pos)
-
-        return poses
-
-    @classmethod
-    def iter_morphemes(cls, morph_word):
-        """Iter morphemes of a morph. word (encoding gloss and pos).
+    def iter_gloss_pos(self, gloss_word):
+        """Iter glosses and poses of a gloss_word.
 
         This method only returns glosses and poses, but not segments,
         since segments are on a different tier. They are retrieved
         directly by get_segments().
 
-        Morphemes (stems and affixes) are separated by hyphens.
+        Glosses in a gloss_words (stems and affixes) are separated
+        by hyphens.
 
         Args:
-            morph_word: str, the morphemes of a word
+            gloss_word: str, the gloss of a word
         Returns:
-            tuple: (<empty_string>, gloss, pos).
+            tuple: (gloss, pos).
         """
-        if not morph_word:
-            yield ('', '', '')
+        if not gloss_word:
+            yield ('', '')
         else:
-            poses = cls.infer_poses(morph_word)
-            morphemes = morph_word.split('-')
-
-            for i in range(len(morphemes)):
-                yield ('', morphemes[i], poses[i])
+            glosses = gloss_word.split('-')
+            self._passed_stem = False
+            for gloss in glosses:
+                pos = self.infer_pos(gloss, len(glosses))
+                yield (gloss, pos)
 
     @classmethod
     def get_segments(cls, seg_word):
         return seg_word.split('-')
 
-    @classmethod
-    def get_glosses(cls, gloss_word):
-        return [gloss for _, gloss, _ in cls.iter_morphemes(gloss_word)]
+    def get_glosses(self, gloss_word):
+        return [gloss for gloss, _ in self.iter_gloss_pos(gloss_word)]
 
-    @classmethod
-    def get_poses(cls, pos_word):
-        return [pos for _, _, pos in cls.iter_morphemes(pos_word)]
+    def get_poses(self, pos_word):
+        return [pos for _, pos in self.iter_gloss_pos(pos_word)]
 
     # TODO: decide if remove
     # @staticmethod
