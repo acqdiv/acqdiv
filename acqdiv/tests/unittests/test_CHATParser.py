c0d4f3013776f548a490fc47a00ac56f282497c3
Janis Goldzycher <janisgoldzycher@gmail.com>
2018-09-21 16:59:23 +0200

    add testing method to TestYucatecParser

--- acqdiv/tests/unittests/test_CHATParser.py
+++ acqdiv/tests/unittests/test_CHATParser.py
@@ -2078,6 +2078,53 @@ class TestYucatecParser(unittest.TestCase):
         actual_cleaner = self.parser.get_cleaner()
         self.assertTrue(isinstance(actual_cleaner, YucatecCleaner))
 
+    def test_next_utterance_no_misalignments_single_word_no_mor(self):
+        """Test next_utterance with utt containing no misalignemnts. (Turkish)
+
+        Test with a one-word utterance without morphology.
+        """
+        session_str = ('*LOR:\tbaʼax .\n%xpho:\tbaaʼx\n%xmor:\tINT|baʼax .\n'
+                       '%xspn:\tqué .\n@End')
+        self.parser.reader.read(io.StringIO(session_str))
+        actual_output = list(self.parser.next_utterance())[0]
+        utt_dict = {
+            'source_id': 'u0',
+            'speaker_label': 'LOR',
+            'addressee': None,
+            'utterance_raw': 'baʼax .',
+            'utterance': 'baʼax',
+            'translation': None,
+            'morpheme': 'INT|baʼax .',
+            'gloss_raw': 'INT|baʼax .',
+            'pos_raw': 'INT|baʼax .',
+            'sentence_type': 'default',
+            'start_raw': None,
+            'end_raw': None,
+            'comment': None,
+            'warning': None
+        }
+        words_list = [
+            {
+                'word_language': None,
+                'word': 'baʼax',
+                'word_actual': 'baʼax',
+                'word_target': 'baʼax',
+                'warning': None
+            }
+        ]
+        morpheme_list = [
+            [
+                {
+                    'gloss_raw': None,
+                    'morpheme': 'baʼax',
+                    'morpheme_language': None,
+                    'pos_raw': 'INT'
+                }
+            ]
+        ]
+        desired_output = (utt_dict, words_list, morpheme_list)
+        self.assertEqual(actual_output, desired_output)
+
 ###############################################################################
 
 
