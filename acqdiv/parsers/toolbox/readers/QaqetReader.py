1523610cd940abb4baef4588605083a9702f7b44
Anna Jancso <anna.jancso0808@gmail.com>
2018-10-22 23:31:14 +0200

    add cleaners for morpheme language and utterance

--- acqdiv/parsers/toolbox/readers/QaqetReader.py
+++ acqdiv/parsers/toolbox/readers/QaqetReader.py
@@ -1,3 +1,5 @@
+import re
+
 from acqdiv.parsers.toolbox.readers.ToolboxReader import ToolboxReader
 
 
@@ -40,6 +42,57 @@ class QaqetReader(ToolboxReader):
             result.append(d)
         return result
 
+    # ---------- clean utterance ----------
+
+    @classmethod
+    def remove_events_utterance(cls, utterance):
+        """Remove events in utterance.
+
+        Events are enclosed in square brackets, e.g.
+        [sound], [laugh], [cry], [sings], ...
+        """
+        # do not match unknowns such as [x]
+        event_regex = re.compile(r'\[(?!x+).*\]')
+        utterance = event_regex.sub('', utterance)
+        return cls.remove_redundant_whitespaces(utterance)
+
+    @classmethod
+    def remove_punctuation(cls, utterance):
+        punctuation_regex = re.compile(r'[,?.]')
+        utterance = punctuation_regex.sub('', utterance)
+        return cls.remove_redundant_whitespaces(utterance)
+
+    @classmethod
+    def null_untranscribed_utterance(cls, utterance):
+        """Null untranscribed utterance.
+
+        Marked as [x+].
+        """
+        untranscribed_regex = re.compile(r'\[x+\]')
+        if untranscribed_regex.fullmatch(utterance):
+            return ''
+
+        return utterance
+
+    @classmethod
+    def unify_unknowns_utterance(cls, utterance):
+        """Unify unknowns.
+
+        Marked as [x+].
+        """
+        unk_regex = re.compile(r'\[x+\]')
+        return unk_regex.sub('???', utterance)
+
+    @classmethod
+    def clean_utterance(cls, utterance):
+        for cleaning_method in [
+                cls.remove_events_utterance, cls.remove_punctuation,
+                cls.null_untranscribed_utterance, cls.unify_unknowns_utterance
+                ]:
+            utterance = cleaning_method(utterance)
+
+        return utterance
+
     @classmethod
     def get_translation(cls, rec_dict):
         return rec_dict.get('ft', '')
@@ -61,3 +114,39 @@ class QaqetReader(ToolboxReader):
             return 'question'
         else:
             return 'default'
+
+    @classmethod
+    def remove_morpheme_sep(cls, morpheme):
+        """Remove morpheme and clitic separators.
+
+        Morpheme (-), clitic (=)
+        """
+        return morpheme.strip('-').strip('=')
+
+    @classmethod
+    def null_untranscribed_morpheme(cls, morpheme):
+        if morpheme in {'??', '***'}:
+            return ''
+
+        return morpheme
+
+    @classmethod
+    def lang2lang(cls, lang):
+        """Map the original language label to ACQDIV label."""
+        mapping = {
+            'Q': 'Qaqet',
+            'TP': 'Tok Pisin',
+            'E': 'English',
+            'GE': 'German'}
+
+        return mapping[lang]
+
+    @classmethod
+    def clean_lang(cls, lang):
+        lang = cls.remove_morpheme_sep(lang)
+        lang = cls.null_untranscribed_morpheme(lang)
+
+        if lang:
+            return cls.lang2lang(lang)
+
+        return lang
