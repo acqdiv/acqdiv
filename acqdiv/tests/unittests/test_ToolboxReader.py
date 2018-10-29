9d4fda14deae023dd4305c7656ac86d1934f7143
Janis Goldzycher <janisgoldzycher@gmail.com>
2018-10-29 16:51:14 +0100

    add new batch of tests to ToolBoxReader

--- acqdiv/tests/unittests/test_ToolboxReader.py
+++ acqdiv/tests/unittests/test_ToolboxReader.py
@@ -110,7 +110,61 @@ class TestToolboxReader(unittest.TestCase):
     # ---------- utterance data ----------
 
     def test_get_utterance_data(self):
-        pass
+        rec_dict = {
+            'ref': 'session_name.001',
+            'ELANBegin': '00:50:11.150',
+            'ELANEnd': '00:50:22.350',
+            'ELANParticipant': 'MAR',
+            'tx': 'w1 w2 .',
+            'mb': 'w1pfxseg-   w1stemseg   -w1sfxseg      w2stemseg',
+            'ge': 'w1pfxgloss- w1stemgloss -w1sfxgloss    w2stemgloss',
+            'ps': 'w1pfxpos-   w1stempos   -w1sfxpos      w2stempos',
+            'lg': 'language-    language   -language      language',
+            'eng': 'This is the translation',
+            'tos': 'child directed',
+            'nep': '?'
+        }
+        actual_output = ToolboxReader.get_utterance_data(rec_dict)
+        desired_output = {
+            'speaker_label': 'MAR',
+            'addressee': '',
+            'utterance_raw': 'w1 w2 .',
+            'utterance': 'w1 w2',
+            'sentence_type': 'default',
+            'childdirected': None,
+            'source_id': 'session_name.001',
+            'start_raw': '00:50:11.150',
+            'end_raw': '00:50:22.350',
+            'translation': 'This is the translation',
+            'comment': '',
+            'warning': '',
+            'morpheme': 'w1pfxseg-   w1stemseg   -w1sfxseg      w2stemseg',
+            'gloss_raw': 'w1pfxgloss- w1stemgloss -w1sfxgloss    w2stemgloss',
+            'pos_raw': 'w1pfxpos-   w1stempos   -w1sfxpos      w2stempos'
+        }
+        self.assertEqual(actual_output, desired_output)
+
+    def test_get_utterance_data_empty_dict(self):
+        rec_dict = {}
+        actual_output = ToolboxReader.get_utterance_data(rec_dict)
+        desired_output = {
+            'speaker_label': '',
+            'addressee': '',
+            'utterance_raw': '',
+            'utterance': '',
+            'sentence_type': '',
+            'childdirected': None,
+            'source_id': '',
+            'start_raw': '',
+            'end_raw': '',
+            'translation': '',
+            'comment': '',
+            'warning': '',
+            'morpheme': '',
+            'gloss_raw': '',
+            'pos_raw': ''
+        }
+        self.assertEqual(actual_output, desired_output)
 
     def test_get_source_id(self):
         rec_dict = {'ref': 'session_name.001'}
@@ -184,7 +238,7 @@ class TestToolboxReader(unittest.TestCase):
 
     def test_get_words_data(self):
         rec_dict = {
-            'tx': 'This is a test .'
+            'tx': 'This is a test'
         }
         actual_output = ToolboxReader.get_words_data(rec_dict)
         desired_output = [
@@ -195,6 +249,332 @@ class TestToolboxReader(unittest.TestCase):
         ]
         self.assertEqual(actual_output, desired_output)
 
+    def test_get_words(self):
+        utterance = 'A test utterance'
+        actual_output = ToolboxReader.get_words(utterance)
+        desired_output = ['A', 'test', 'utterance']
+        self.assertEqual(actual_output, desired_output)
+
+    # ---------- morphemes data ----------
+
+    def test_get_morphemes_data_single_morpheme_word(self):
+        rec_dict = {
+            'mb': 'w1pfxseg',
+            'ge': 'w1pfxgloss',
+            'ps': 'w1pfxpos',
+            'lg': 'language'
+        }
+        actual_output = ToolboxReader.get_morphemes_data(rec_dict)
+        desired_output = [
+            [
+                {'morpheme': 'w1pfxseg',
+                 'gloss_raw': 'w1pfxgloss',
+                 'pos_raw': 'w1pfxpos',
+                 'morpheme_language': 'language',
+                 'type': 'target',
+                 'warning': None
+                 }
+            ]
+        ]
+        self.assertEqual(actual_output, desired_output)
+
+    def test_get_morphemes_data_multiple_morpheme_words(self):
+        rec_dict = {
+            'mb': 'w1pfxseg-   w1stemseg   -w1sfxseg      w2stemseg',
+            'ge': 'w1pfxgloss- w1stemgloss -w1sfxgloss    w2stemgloss',
+            'ps': 'w1pfxpos-   w1stempos   -w1sfxpos      w2stempos',
+            'lg': 'language-    language   -language      language'
+        }
+        actual_output = ToolboxReader.get_morphemes_data(rec_dict)
+        desired_output = [
+            [
+                {'morpheme': 'w1pfxseg-',
+                 'gloss_raw': 'w1pfxgloss-',
+                 'pos_raw': 'w1pfxpos-',
+                 'morpheme_language': 'language-',
+                 'type': 'target',
+                 'warning': None
+                 },
+                {'morpheme': 'w1stemseg',
+                 'gloss_raw': 'w1stemgloss',
+                 'pos_raw': 'w1stempos',
+                 'morpheme_language': 'language',
+                 'type': 'target',
+                 'warning': None},
+                {'morpheme': '-w1sfxseg',
+                 'gloss_raw': '-w1sfxgloss',
+                 'pos_raw': '-w1sfxpos',
+                 'morpheme_language': '-language',
+                 'type': 'target',
+                 'warning': None}
+            ],
+            [
+                {'morpheme': 'w2stemseg',
+                 'gloss_raw': 'w2stemgloss',
+                 'pos_raw': 'w2stempos',
+                 'morpheme_language': 'language',
+                 'type': 'target',
+                 'warning': None}
+            ]
+        ]
+        self.assertEqual(actual_output, desired_output)
+
+    def test_get_morphemes_data_empty_dict(self):
+        rec_dict = {}
+        actual_output = ToolboxReader.get_morphemes_data(rec_dict)
+        desired_output = []
+        self.assertEqual(actual_output, desired_output)
+
+    def test_get_morphology_data_single_morpheme_word(self):
+        rec_dict = {
+            'mb': 'w1pfxseg-   w1stemseg   -w1sfxseg',
+            'ge': 'w1pfxgloss- w1stemgloss -w1sfxgloss',
+            'ps': 'w1pfxpos-   w1stempos   -w1sfxpos',
+            'lg': 'language-    language   -language'
+        }
+        actual_output = ToolboxReader.get_morphology_data(rec_dict)
+        segments = [['w1pfxseg-', 'w1stemseg', '-w1sfxseg']]
+        glosses = [['w1pfxgloss-', 'w1stemgloss', '-w1sfxgloss']]
+        poses = [['w1pfxpos-', 'w1stempos', '-w1sfxpos']]
+        langs = [['language-', 'language', '-language']]
+        desired_output = (segments, glosses, poses, langs)
+        self.assertEqual(actual_output, desired_output)
+
+    def test_get_morphology_data_multiple_morpheme_words(self):
+        rec_dict = {
+            'mb': 'w1pfxseg-   w1stemseg   -w1sfxseg      w2stemseg',
+            'ge': 'w1pfxgloss- w1stemgloss -w1sfxgloss    w2stemgloss',
+            'ps': 'w1pfxpos-   w1stempos   -w1sfxpos      w2stempos',
+            'lg': 'language-    language   -language      language'
+        }
+        actual_output = ToolboxReader.get_morphology_data(rec_dict)
+        segments = [['w1pfxseg-', 'w1stemseg', '-w1sfxseg'], ['w2stemseg']]
+        glosses = [['w1pfxgloss-', 'w1stemgloss', '-w1sfxgloss'],
+                   ['w2stemgloss']]
+        poses = [['w1pfxpos-', 'w1stempos', '-w1sfxpos'], ['w2stempos']]
+        langs = [['language-', 'language', '-language'], ['language']]
+        desired_output = (segments, glosses, poses, langs)
+        self.assertEqual(actual_output, desired_output)
+
+    def test_get_morphology_data_empty_dict(self):
+        rec_dict = {}
+        actual_output = ToolboxReader.get_morphology_data(rec_dict)
+        desired_output = ([], [], [], [])
+        self.assertEqual(actual_output, desired_output)
+
+    # ---------- morpheme tier ----------
+
+    def test_get_seg_tier(self):
+        rec_dict = {
+            'mb': 'w1pfxseg-   w1stemseg   -w1sfxseg      w2stemseg'
+        }
+        actual_output = ToolboxReader.get_seg_tier(rec_dict)
+        desired_output = 'w1pfxseg-   w1stemseg   -w1sfxseg      w2stemseg'
+        self.assertEqual(actual_output, desired_output)
+
+    def test_get_gloss_tier(self):
+        rec_dict = {
+            'ge': 'w1pfxgloss- w1stemgloss -w1sfxgloss    w2stemgloss'
+        }
+        actual_output = ToolboxReader.get_gloss_tier(rec_dict)
+        desired_output = 'w1pfxgloss- w1stemgloss -w1sfxgloss    w2stemgloss'
+        self.assertEqual(actual_output, desired_output)
+
+    def test_pos_tier(self):
+        rec_dict = {
+            'ps': 'w1pfxpos-   w1stempos   -w1sfxpos      w2stempos'
+        }
+        actual_output = ToolboxReader.get_pos_tier(rec_dict)
+        desired_output = 'w1pfxpos-   w1stempos   -w1sfxpos      w2stempos'
+        self.assertEqual(actual_output, desired_output)
+
+    def test_get_lang_tier(self):
+        rec_dict = {
+            'lg': 'language    language     language      language'
+        }
+        actual_output = ToolboxReader.get_lang_tier(rec_dict)
+        desired_output = 'language    language     language      language'
+        self.assertEqual(actual_output, desired_output)
+
+    # ---------- morpheme words ----------
+
+    def test_get_morpheme_words(self):
+        morpheme_tier = 'w1pfxseg-   w1stemseg   -w1sfxseg    w2stemseg'
+        actual_output = ToolboxReader.get_morpheme_words(morpheme_tier)
+        desired_output = ['w1pfxseg-   w1stemseg   -w1sfxseg', 'w2stemseg']
+        self.assertEqual(actual_output, desired_output)
+
+    def test_get_morpheme_words_empty_string(self):
+        morpheme_tier = ''
+        actual_output = ToolboxReader.get_morpheme_words(morpheme_tier)
+        desired_output = []
+        self.assertEqual(actual_output, desired_output)
+
+    def test_get_seg_words(self):
+        morpheme_tier = 'w1pfxseg-   w1stemseg   -w1sfxseg      w2stemseg'
+        actual_output = ToolboxReader.get_seg_words(morpheme_tier)
+        desired_output = ['w1pfxseg-   w1stemseg   -w1sfxseg', 'w2stemseg']
+        self.assertEqual(actual_output, desired_output)
+
+    def test_get_seg_words_empty_string(self):
+        morpheme_tier = ''
+        actual_output = ToolboxReader.get_seg_words(morpheme_tier)
+        desired_output = []
+        self.assertEqual(actual_output, desired_output)
+
+    def test_get_gloss_words(self):
+        morpheme_tier = 'w1pfxgloss- w1stemgloss -w1sfxgloss    w2stemgloss'
+        actual_output = ToolboxReader.get_seg_words(morpheme_tier)
+        desired_output = ['w1pfxgloss- w1stemgloss -w1sfxgloss', 'w2stemgloss']
+        self.assertEqual(actual_output, desired_output)
+
+    def test_get_gloss_words_empty_string(self):
+        morpheme_tier = ''
+        actual_output = ToolboxReader.get_seg_words(morpheme_tier)
+        desired_output = []
+        self.assertEqual(actual_output, desired_output)
+
+    def test_get_pos_words(self):
+        morpheme_tier = 'w1pfxpos-   w1stempos   -w1sfxpos      w2stempos'
+        actual_output = ToolboxReader.get_seg_words(morpheme_tier)
+        desired_output = ['w1pfxpos-   w1stempos   -w1sfxpos', 'w2stempos']
+        self.assertEqual(actual_output, desired_output)
+
+    def test_get_pos_words_empty_string(self):
+        morpheme_tier = ''
+        actual_output = ToolboxReader.get_seg_words(morpheme_tier)
+        desired_output = []
+        self.assertEqual(actual_output, desired_output)
+
+    def test_get_lang_words(self):
+        morpheme_tier = 'language-    language   -language      language'
+        actual_output = ToolboxReader.get_seg_words(morpheme_tier)
+        desired_output = ['language-    language   -language', 'language']
+        self.assertEqual(actual_output, desired_output)
+
+    def test_get_lang_words_empty_string(self):
+        morpheme_tier = ''
+        actual_output = ToolboxReader.get_seg_words(morpheme_tier)
+        desired_output = []
+        self.assertEqual(actual_output, desired_output)
+
+    # ---------- morphemes ----------
+
+    def test_get_morphemes(self):
+        morpheme_word = 'w1pfxseg-   w1stemseg   -w1sfxseg'
+        actual_output = ToolboxReader.get_morphemes(morpheme_word)
+        desired_output = ['w1pfxseg-', 'w1stemseg', '-w1sfxseg']
+        self.assertEqual(actual_output, desired_output)
+
+    def test_get_morphemes_empty_string(self):
+        morpheme_word = ''
+        actual_output = ToolboxReader.get_morphemes(morpheme_word)
+        desired_output = []
+        self.assertEqual(actual_output, desired_output)
+
+    def test_get_segs(self):
+        segment_word = 'w1pfxseg-   w1stemseg   -w1sfxseg'
+        actual_output = ToolboxReader.get_segs(segment_word)
+        desired_output = ['w1pfxseg-', 'w1stemseg', '-w1sfxseg']
+        self.assertEqual(actual_output, desired_output)
+
+    def test_get_glosses(self):
+        gloss_word = 'w1pfxgloss- w1stemgloss -w1sfxgloss'
+        actual_output = ToolboxReader.get_glosses(gloss_word)
+        desired_output = ['w1pfxgloss-', 'w1stemgloss', '-w1sfxgloss']
+        self.assertEqual(actual_output, desired_output)
+
+    def test_get_poses(self):
+        pos_word = 'w1pfxpos-   w1stempos   -w1sfxpos'
+        actual_output = ToolboxReader.get_poses(pos_word)
+        desired_output = ['w1pfxpos-', 'w1stempos', '-w1sfxpos']
+        self.assertEqual(actual_output, desired_output)
+
+    def test_get_langs(self):
+        morpheme_lang_word = 'language-    language   -language'
+        actual_output = ToolboxReader.get_langs(morpheme_lang_word)
+        desired_output = ['language-', 'language', '-language']
+        self.assertEqual(actual_output, desired_output)
+
+    def test_get_list_of_list_morphemes_seg_tier(self):
+        rec_dict = {
+            'mb': 'w1pfxseg-   w1stemseg   -w1sfxseg      w2stemseg',
+            'ge': 'w1pfxgloss- w1stemgloss -w1sfxgloss    w2stemgloss',
+            'ps': 'w1pfxpos-   w1stempos   -w1sfxpos      w2stempos',
+            'lg': 'language-    language   -language      language'
+        }
+        actual_output = ToolboxReader.get_list_of_list_morphemes(
+            rec_dict,
+            ToolboxReader.get_seg_tier,
+            ToolboxReader.get_seg_words,
+            ToolboxReader.get_segs,
+            ToolboxReader.clean_seg_tier,
+            ToolboxReader.clean_seg_word,
+            ToolboxReader.clean_seg
+        )
+        desired_output = [['w1pfxseg-', 'w1stemseg', '-w1sfxseg'], ['w2stemseg']]
+        self.assertEqual(actual_output, desired_output)
+
+    def test_get_list_of_list_morphemes_empty_dict(self):
+        rec_dict = {}
+        actual_output = ToolboxReader.get_list_of_list_morphemes(
+            rec_dict,
+            ToolboxReader.get_seg_tier,
+            ToolboxReader.get_seg_words,
+            ToolboxReader.get_segs,
+            ToolboxReader.clean_seg_tier,
+            ToolboxReader.clean_seg_word,
+            ToolboxReader.clean_seg
+        )
+        desired_output = []
+        self.assertEqual(actual_output, desired_output)
+
+    def test_fix_mm_misalignments_no_missalignment(self):
+        segments = [['w1pfxseg-', 'w1stemseg', '-w1sfxseg'], ['w2stemseg']]
+        glosses = [['w1pfxgloss-', 'w1stemgloss', '-w1sfxgloss'],
+                   ['w2stemgloss']]
+        poses = [['w1pfxpos-', 'w1stempos', '-w1sfxpos'], ['w2stempos']]
+        langs = [['language-', 'language', '-language'], ['language']]
+        morphology_data = (segments, glosses, poses, langs)
+        actual_output = ToolboxReader.fix_mm_misalignments(morphology_data)
+        desired_output = morphology_data
+        self.assertEqual(actual_output, desired_output)
+
+    def test_fix_mm_misalignments_segments_missaligned(self):
+        segments = [['w1pfxseg-', '-w1sfxseg'], ['w2stemseg']]
+        glosses = [['w1pfxgloss-', 'w1stemgloss', '-w1sfxgloss'],
+                   ['w2stemgloss']]
+        poses = [['w1pfxpos-', 'w1stempos', '-w1sfxpos'], ['w2stempos']]
+        langs = [['language-', 'language', '-language'], ['language']]
+        morphology_data = (segments, glosses, poses, langs)
+        actual_output = ToolboxReader.fix_mm_misalignments(morphology_data)
+        desired_output = ([[], []], glosses, poses, langs)
+        self.assertEqual(actual_output, desired_output)
+
+    def test_fix_mm_misalignments_segment_words_missaligned(self):
+        segments = [['w1pfxseg-', 'w1stemseg', '-w1sfxseg']]
+        glosses = [['w1pfxgloss-', 'w1stemgloss', '-w1sfxgloss'],
+                   ['w2stemgloss']]
+        poses = [['w1pfxpos-', 'w1stempos', '-w1sfxpos'], ['w2stempos']]
+        langs = [['language-', 'language', '-language'], ['language']]
+        morphology_data = (segments, glosses, poses, langs)
+        actual_output = ToolboxReader.fix_mm_misalignments(morphology_data)
+        desired_output = ([[], []], glosses, poses, langs)
+        self.assertEqual(actual_output, desired_output)
+
+    def test_fix_mm_misalignments_empty_input(self):
+        morphology_data = ([], [], [], [], [])
+        actual_output = ToolboxReader.fix_mm_misalignments(morphology_data)
+        desired_output = ([], [], [], [], [])
+        self.assertEqual(actual_output, desired_output)
+
+    def test_get_morpheme_dict(self):
+        pass
+
+    def test_get_morpheme_type(self):
+        pass
+
+    # ---------- miscellaneous ----------
 
 
 if __name__ == '__main__':
