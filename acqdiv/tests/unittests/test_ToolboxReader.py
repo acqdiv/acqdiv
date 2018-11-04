be237d6e153f944d78a665c787f36a1d5093284d
Janis Goldzycher <janisgoldzycher@gmail.com>
2018-11-04 19:17:52 +0100

    add first batch of tests for ChintangReader

--- acqdiv/tests/unittests/test_ToolboxReader.py
+++ acqdiv/tests/unittests/test_ToolboxReader.py
@@ -1,5 +1,6 @@
 import unittest
 from acqdiv.parsers.toolbox.readers.ToolboxReader import *
+from acqdiv.parsers.toolbox.readers.ChintangReader import ChintangReader
 
 
 class TestToolboxReader(unittest.TestCase):
@@ -903,6 +904,193 @@ class TestToolboxReader(unittest.TestCase):
         actual_output = reader.__repr__()
         desired_output = "ToolboxReader('./Chintang.txt')"
         self.assertEqual(actual_output, desired_output)
+
+
+class TestChintangReader(unittest.TestCase):
+
+    def test_get_utterance_raw_key_exists(self):
+        rec_dict = {
+            'gw': 'the raw utterance .'
+        }
+        actual_output = ChintangReader.get_utterance_raw(rec_dict)
+        desired_output = 'the raw utterance .'
+        self.assertEqual(actual_output, desired_output)
+
+    def test_get_utterance_raw_key_does_not_exists(self):
+        rec_dict = {
+            'a': 'the raw utterance .'
+        }
+        actual_output = ChintangReader.get_utterance_raw(rec_dict)
+        desired_output = ''
+        self.assertEqual(actual_output, desired_output)
+
+    def test_get_seg_tier_key_exists(self):
+        rec_dict = {
+            'mph': 'the seg tier .'
+        }
+        actual_output = ChintangReader.get_seg_tier(rec_dict)
+        desired_output = 'the seg tier .'
+        self.assertEqual(actual_output, desired_output)
+
+    def test_get_seg_tier_key_does_not_exist(self):
+        rec_dict = {
+            'mp': 'the seg tier .'
+        }
+        actual_output = ChintangReader.get_seg_tier(rec_dict)
+        desired_output = ''
+        self.assertEqual(actual_output, desired_output)
+
+    def test_get_gloss_tier_key_exists(self):
+        rec_dict = {
+            'mgl': 'the gloss tier .'
+        }
+        actual_output = ChintangReader.get_gloss_tier(rec_dict)
+        desired_output = 'the gloss tier .'
+        self.assertEqual(actual_output, desired_output)
+
+    def test_get_gloss_tier_key_does_not_exist(self):
+        rec_dict = {
+            'm': 'the gloss tier .'
+        }
+        actual_output = ChintangReader.get_gloss_tier(rec_dict)
+        desired_output = ''
+        self.assertEqual(actual_output, desired_output)
+
+    def test_get_pos_tier_key_exists(self):
+        rec_dict = {
+            'ps': 'the pos tier .'
+        }
+        actual_output = ChintangReader.get_pos_tier(rec_dict)
+        desired_output = 'the pos tier .'
+        self.assertEqual(actual_output, desired_output)
+
+    def test_get_pos_tier_key_does_not_exist(self):
+        rec_dict = {
+            's': 'the pos tier .'
+        }
+        actual_output = ChintangReader.get_pos_tier(rec_dict)
+        desired_output = ''
+        self.assertEqual(actual_output, desired_output)
+
+    def test_get_lang_tier_key_exists(self):
+        rec_dict = {
+            'lg': 'the lang tier .'
+        }
+        actual_output = ChintangReader.get_lang_tier(rec_dict)
+        desired_output = 'the lang tier .'
+        self.assertEqual(actual_output, desired_output)
+
+    def test_get_lang_tier_key_does_not_exist(self):
+        rec_dict = {
+            'l': 'the lang tier .'
+        }
+        actual_output = ChintangReader.get_lang_tier(rec_dict)
+        desired_output = ''
+        self.assertEqual(actual_output, desired_output)
+
+    def test_get_childdirected_tos_childdirected(self):
+        rec_dict = {
+            'tos': 'this is directed to a child'
+        }
+        output = ChintangReader.get_childdirected(rec_dict)
+        self.assertTrue(output)
+
+    def test_get_childdirected_tos_not_childdirected(self):
+        rec_dict = {
+            'tos': 'this is directed to an adult'
+        }
+        output = ChintangReader.get_childdirected(rec_dict)
+        self.assertFalse(output)
+
+    def test_get_childdirected_TOS_childdirected(self):
+        rec_dict = {
+            'TOS': 'this is directed to a child'
+        }
+        output = ChintangReader.get_childdirected(rec_dict)
+        self.assertTrue(output)
+
+    def test_get_childdirected_TOS_not_childdirected(self):
+        rec_dict = {
+            'TOS': 'this is directed to an adult'
+        }
+        output = ChintangReader.get_childdirected(rec_dict)
+        self.assertFalse(output)
+
+    def test_get_childdirected_key_does_not_exist(self):
+        rec_dict = {}
+        output = ChintangReader.get_childdirected(rec_dict)
+        self.assertIsNone(output)
+
+    def test_get_id_tier_key_exists(self):
+        rec_dict = {
+            'id': 3
+        }
+        actual_output = ChintangReader.get_id_tier(rec_dict)
+        desired_output = 3
+        self.assertEqual(actual_output, desired_output)
+
+    def test_get_id_tier_key_does_not_exist(self):
+        rec_dict = {}
+        actual_output = ChintangReader.get_id_tier(rec_dict)
+        desired_output = ''
+        self.assertEqual(actual_output, desired_output)
+
+    def test_get_id_words_two_morpheme_words(self):
+        id_tier = 'w1pfxseg-   w1stemseg   -w1sfxseg    w2stemseg'
+        actual_output = ChintangReader.get_id_words(id_tier)
+        desired_output = ['w1pfxseg-   w1stemseg   -w1sfxseg', 'w2stemseg']
+        self.assertEqual(actual_output, desired_output)
+
+    def test_get_id_words_empty_string(self):
+        id_tier = ''
+        actual_output = ChintangReader.get_id_words(id_tier)
+        desired_output = []
+        self.assertEqual(actual_output, desired_output)
+
+    def test_get_ids(self):
+        id_word = 'w1pfxseg-   w1stemseg   -w1sfxseg'
+        actual_output = ChintangReader.get_id_words(id_word)
+        desired_output = ['w1pfxseg-', 'w1stemseg', '-w1sfxseg']
+        self.assertEqual(actual_output, desired_output)
+
+    def test_get_utterance_data(self):
+        rec_dict = {
+            'ref': 'session_name.001',
+            'ELANBegin': '00:50:11.150',
+            'ELANEnd': '00:50:22.350',
+            'ELANParticipant': 'MAR',
+            'gw': 'w1 w2 ?',
+            'id': 'w1pfxseg-   w1stemseg   -w1sfxseg      w2stemseg',
+            'mgl': 'w1pfxgloss- w1stemgloss -w1sfxgloss    w2stemgloss',
+            'ps': 'w1pfxpos-   w1stempos   -w1sfxpos      w2stempos',
+            'lg': 'language-    language   -language      language',
+            'eng': 'This is the translation',
+            'tos': 'child directed',
+            'nep': '?'
+        }
+        actual_output = ChintangReader.get_utterance_data(rec_dict)
+        desired_output = {
+            'speaker_label': 'MAR',
+            'addressee': '',
+            'utterance_raw': 'w1 w2 ?',
+            'utterance': 'w1 w2',
+            'sentence_type': 'question',
+            'childdirected': True,
+            'source_id': 'session_name.001',
+            'start_raw': '00:50:11.150',
+            'end_raw': '00:50:22.350',
+            'translation': 'This is the translation',
+            'comment': '',
+            'warning': '',
+            'morpheme': '',
+            'lemma_id': 'w1pfxseg-   w1stemseg   -w1sfxseg      w2stemseg',
+            'gloss_raw': 'w1pfxgloss- w1stemgloss -w1sfxgloss    w2stemgloss',
+            'pos_raw': 'w1pfxpos-   w1stempos   -w1sfxpos      w2stempos'
+        }
+        self.assertEqual(actual_output, desired_output)
+
+    def test_get_sentence_type(self):
+        pass
 
 
 if __name__ == '__main__':
