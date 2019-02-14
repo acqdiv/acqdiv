0bd66d9b9a36428d0d3c6cbf4866368e4f8f3640
Anna Jancso <anna.jancso0808@gmail.com>
2019-02-14 10:27:02 +0100

    add label corrector for Japanese miipro

--- acqdiv/parsers/chat/cleaners/JapaneseMiiProCleaner.py
+++ acqdiv/parsers/chat/cleaners/JapaneseMiiProCleaner.py
@@ -5,6 +5,41 @@ from acqdiv.parsers.chat.cleaners.CHATCleaner import CHATCleaner
 
 class JapaneseMiiProCleaner(CHATCleaner):
 
+    @staticmethod
+    def correct_speaker_label(session_filename, speaker_label):
+        sessions_filenames = {
+            "als19990618.cha",
+            "als19990706.cha",
+            "als19990805.cha",
+            "als19990907.cha",
+            "als19991005.cha",
+            "als19991207.cha",
+            "als20000105.cha",
+            "als20000201.cha",
+            "als20000307.cha",
+            "als20000404.cha",
+            "als20000711.cha",
+            "als20000905.cha",
+            "als20001106.cha",
+            "als20010108.cha",
+            "als20010308.cha",
+            "als20010512.cha",
+            "als20010714.cha"
+        }
+
+        if session_filename in sessions_filenames and speaker_label == 'CHI':
+            return 'ALS'
+
+        return speaker_label
+
+    @classmethod
+    def clean_record_speaker_label(cls, session_filename, speaker_label):
+        speaker_label = cls.correct_speaker_label(
+            session_filename, speaker_label)
+        return speaker_label
+
+    # ---------- morphology tier cleaning ----------
+
     @classmethod
     def remove_non_words(cls, morph_tier):
         """Remove all non-words from the morphology tier.
@@ -39,43 +74,47 @@ class JapaneseMiiProCleaner(CHATCleaner):
 
     @classmethod
     def clean_speaker_metadata(
-            cls, session_filename, speaker_label, name, role,
+            cls, session_filename, label, name, role,
             age, gender, language, birth_date, target_child):
-        role = cls.correct_role(speaker_label, role, target_child)
-        name = cls.correct_name(session_filename, speaker_label, name)
 
-        return speaker_label, name, role, age, gender, language, birth_date
+        tc_label, _ = target_child
+        tc_label = cls.correct_speaker_label(session_filename, tc_label)
+        label = cls.correct_speaker_label(session_filename, label)
+        role = cls.correct_role(label, role, tc_label)
+        name = cls.correct_name(session_filename, label, name)
+
+        return label, name, role, age, gender, language, birth_date
 
     @staticmethod
-    def correct_role(speaker_label, role, target_child):
-        if role == 'Child' and speaker_label == target_child[0]:
+    def correct_role(label, role, tc_label):
+        if role == 'Child' and label == tc_label:
             return 'Target_Child'
 
         return role
 
     @staticmethod
-    def correct_name(session_filename, speaker_label, name):
+    def correct_name(session_filename, label, name):
 
         if session_filename.startswith('als'):
             if name == 'Asatokun':
                 return 'Asato'
             elif name == 'Arichan':
                 return 'Arika'
-            elif speaker_label == 'MOT':
+            elif label == 'MOT':
                 return 'Mother_of_Arika_and_Asato'
-            elif speaker_label == 'ALS':
+            elif label == 'ALS':
                 return 'Asato'
 
         elif session_filename.startswith('aprm'):
             if name == 'Arichan':
                 return 'Arika'
-            elif speaker_label == 'MOT':
+            elif label == 'MOT':
                 return 'Mother_of_Arika_and_Asato'
             elif name == 'Asatokun':
                 return 'Asato'
-            elif speaker_label == 'APR':
+            elif label == 'APR':
                 return 'Arika'
-            elif speaker_label == 'BAA':
+            elif label == 'BAA':
                 return 'Obaachan'
 
         elif session_filename.startswith('njd'):
@@ -85,23 +124,23 @@ class JapaneseMiiProCleaner(CHATCleaner):
                 return 'Nanami'
             elif name == 'Fuyumichan':
                 return 'Fuyumi'
-            elif speaker_label == 'MOT':
+            elif label == 'MOT':
                 return 'Mother_of_Nanami'
             elif name == 'Arikachan':
                 return 'Arika'
             elif name == 'Kantakun':
                 return 'Kanta'
-            elif speaker_label == 'MOT':
+            elif label == 'MOT':
                 return 'Mother_of_Nanami'
-            elif speaker_label == 'MTO':
+            elif label == 'MTO':
                 return 'Mother_of_Tomito'
-            elif speaker_label == 'NJD':
+            elif label == 'NJD':
                 return 'Nanami'
 
         elif session_filename.startswith('tom'):
             if name == 'Honokachan':
                 return 'Honoka'
-            elif speaker_label == 'MOT':
+            elif label == 'MOT':
                 return 'Mother_of_Tomito'
 
         return name
