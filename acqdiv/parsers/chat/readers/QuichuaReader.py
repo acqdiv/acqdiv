a77f253d4ac717c0025ef840901133d288bbe0e0
Anna Jancso <anna.jancso0808@gmail.com>
2019-03-18 17:40:57 +0100

    add readers for Quichua [Phonbank/695]

--- acqdiv/parsers/chat/readers/QuichuaReader.py
+++ acqdiv/parsers/chat/readers/QuichuaReader.py
@@ -2,4 +2,14 @@ from acqdiv.parsers.chat.readers.PhonbankReader import PhonbankReader
 
 
 class QuichuaReader(PhonbankReader):
-    pass
+
+    def get_phon_tier(self):
+        return self._dependent_tiers.get('xpho', '')
+
+    @classmethod
+    def get_phon_words(cls, phon_tier):
+        return [phon_tier]
+
+    @classmethod
+    def get_word_length(cls, word):
+        return word.count(' ') + 1
