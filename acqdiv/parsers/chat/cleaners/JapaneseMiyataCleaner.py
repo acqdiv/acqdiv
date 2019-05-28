707c5e6c58d24db5978935b09811246386a5dddc
Anna Jancso <anna.jancso0808@gmail.com>
2019-05-28 13:55:23 +0200

    Add cleaner for Japanese Miyata

--- acqdiv/parsers/chat/cleaners/JapaneseMiyataCleaner.py
+++ acqdiv/parsers/chat/cleaners/JapaneseMiyataCleaner.py
@@ -3,4 +3,10 @@ from acqdiv.parsers.chat.cleaners.CHATCleaner import CHATCleaner
 
 
 class JapaneseMiyataCleaner(CHATCleaner):
-    pass
+
+    # ---------- morphology tier cleaning ----------
+
+    @classmethod
+    def clean_morph_tier(cls, morph_tier):
+        morph_tier = cls.remove_terminator(morph_tier)
+        return morph_tier
