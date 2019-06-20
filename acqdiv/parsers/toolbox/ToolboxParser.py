b134e999e17e0db54050dd9a13016de79054eaec
Anna Jancso <anna.jancso0808@gmail.com>
2019-06-20 10:49:41 +0200

    adapt file extension in Ku Waru

--- acqdiv/parsers/toolbox/ToolboxParser.py
+++ acqdiv/parsers/toolbox/ToolboxParser.py
@@ -153,3 +153,9 @@ class KuWaruParser(ToolboxParser):
 
     def get_record_reader(self):
         return KuWaruReader(self.toolbox_file)
+
+    def get_metadata_reader(self):
+        temp = self.toolbox_file.replace(self.config['paths']['sessions_dir'],
+                                         self.config['paths']['metadata_dir'])
+        metadata_file_path = temp.replace(".tbt", ".imdi")
+        return CMDI(self.config, metadata_file_path)
