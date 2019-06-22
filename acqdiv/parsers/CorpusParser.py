1968891920f48732d8137181646a3b8abaa6f26d
Anna Jancso <anna.jancso0808@gmail.com>
2019-06-22 12:50:37 +0200

    Clean corpus parser

--- acqdiv/parsers/CorpusParser.py
+++ acqdiv/parsers/CorpusParser.py
@@ -4,34 +4,39 @@ from acqdiv.processors.processors import SessionProcessor
 
 
 class CorpusParser:
-    """ Handler for processing each session file in particular corpus.
-    """
+    """Parses all sessions of a corpus."""
+
     def __init__(self, cfg, engine):
-        """ Initializes a CorpusParser object then calls a SessionProcessor for each session input file.
+        """Initialize config and engine.
 
         Args:
-            cfg: CorpusConfigParser
-            engine: sqlalchemy database engine
+            cfg (CorpusConfigParser): A config instance.
+            engine (Engine): A SQLAlchemy database engine.
         """
         self.cfg = cfg
         self.engine = engine
 
     def get_session_parser(self, session_path):
-        pass
+        """Get a session parser.
+
+        Returns:
+            SessionParser: The session parser.
+        """
+        raise NotImplementedError
 
     def process_corpus(self, test=False):
-        """Process corpus files.
+        """Parse all sessions of a corpus.
 
         Args:
             test (bool): Only process the first file.
         """
-        for session_file in sorted(glob.glob(self.cfg['paths']['sessions'])):
-            print("\t", session_file)
+        for session_path in sorted(glob.glob(self.cfg['paths']['sessions'])):
+            print("\t", session_path)
 
-            session_parser = self.get_session_parser(session_file)
+            session_parser = self.get_session_parser(session_path)
 
             s = SessionProcessor(
-                self.cfg, session_file, session_parser, self.engine)
+                self.cfg, session_path, session_parser, self.engine)
             s.process_session()
 
             if test:
