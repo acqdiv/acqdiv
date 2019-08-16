57fd198ecc486312ebebf51e920f5148894d0f7b
Anna Jancso <anna.jancso0808@gmail.com>
2019-08-16 10:57:57 +0200

    Enhance models of `Morpheme` and `Word`

--- acqdiv/model/Morpheme.py
+++ acqdiv/model/Morpheme.py
@@ -8,15 +8,19 @@ class Morpheme:
 
         utterance (acqdiv.model.Utterance.Utterance): The utterance that
             the morpheme belongs to.
+        word (acqdiv.model.Word.Word): The word that the morpheme belongs to.
         morpheme_language (str): The language that word is in.
         type (str): Whether the morpheme is an actual or target form.
         morpheme (str): The morpheme itself.
         gloss_raw (str): The original gloss of the morpheme.
         pos_raw (str): The original POS tag of the morpheme.
+        pos (str): The POS tag mapped from `pos_raw`.
+        pos_ud (str): The Universal Dependency POS tag mapped from `pos_raw`.
         lemma_id (str): The ID of the lemma.
         warning (str): Warnings regarding morpheme.
         """
         self.utterance = Utterance()
+        self.word = None
         self.morpheme_language = ''
         self.type = ''
         self.morpheme = ''
@@ -24,5 +28,6 @@ class Morpheme:
         self.gloss = ''
         self.pos_raw = ''
         self.pos = ''
+        self.pos_ud = ''
         self.lemma_id = ''
         self.warning = ''
--- acqdiv/model/Word.py
+++ acqdiv/model/Word.py
@@ -12,10 +12,14 @@ class Word:
         word (str): The word itself.
         word_actual (str): The actual form of the word.
         word_target (str): The target form of the word.
+        pos (str): The POS tag of the word.
+        pos_ud (str): The Universal Dependency POS tag of the word.
         """
         self.utterance = Utterance()
         self.word_language = ''
         self.word = ''
         self.word_actual = ''
         self.word_target = ''
+        self.pos = ''
+        self.pos_ud = ''
         self.warning = ''
