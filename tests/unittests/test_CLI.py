8cafe26823f07dc1a64554b7103e902457565f97
Anna Jancso <anna.t.jancso@gmail.com>
2019-11-24 14:30:31 +0100

    Sort out path issues in smoke test

--- tests/unittests/test_CLI.py
+++ tests/unittests/test_CLI.py
@@ -1,20 +1,49 @@
 import unittest
 import subprocess
 import sqlite3
+import os
+import configparser
 import datetime
 from pathlib import Path
 
 
 class CLITest(unittest.TestCase):
 
+    db_path = None
+    cfg_parser = None
+    cfg = None
+    old_corpora_dir = None
+    old_db_dir = None
+
     @classmethod
     def setUpClass(cls) -> None:
         unittest_dir = Path(__file__).parent
-        cfg = unittest_dir / 'resources/config.ini'
-        subprocess.run(f'acqdiv load -c {cfg}', shell=True)
+        resources_dir = unittest_dir / 'resources'
+
+        # Read the config
+        cls.cfg = resources_dir / 'config.ini'
+        cls.cfg_parser = configparser.ConfigParser()
+        cls.cfg_parser.read(cls.cfg)
+
+        # Save old paths in config
+        cls.old_corpora_dir = cls.cfg_parser['.global']['corpora_dir']
+        cls.old_db_dir = cls.cfg_parser['.global']['db_dir']
+
+        # Change paths in config
+        corpora_dir = resources_dir / 'corpora'
+        cls.cfg_parser['.global']['corpora_dir'] = str(corpora_dir)
+        cls.cfg_parser['.global']['db_dir'] = str(resources_dir)
+
+        # Write new paths to config
+        with open(cls.cfg, 'w') as configfile:
+            cls.cfg_parser.write(configfile)
+
+        # Run CLI
+        subprocess.run(f'acqdiv load -c {cls.cfg}', shell=True)
+
         date = datetime.datetime.now().strftime('%Y-%m-%d')
-        path = unittest_dir / f'resources/acqdiv_corpus_{date}.sqlite3'
-        conn = sqlite3.connect(str(path))
+        cls.db_path = resources_dir / f'acqdiv_corpus_{date}.sqlite3'
+        conn = sqlite3.connect(str(cls.db_path))
         cls.cursor = conn.cursor()
 
     def test_n_corpora(self):
@@ -24,6 +53,17 @@ class CLITest(unittest.TestCase):
     def test_n_utterances(self):
         res = self.cursor.execute('SELECT * FROM utterances')
         self.assertEqual(len(res.fetchall()), 14)
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        # Write old paths back to config
+        cls.cfg_parser['.global']['corpora_dir'] = cls.old_corpora_dir
+        cls.cfg_parser['.global']['db_dir'] = cls.old_db_dir
+        with open(cls.cfg, 'w') as configfile:
+            cls.cfg_parser.write(configfile)
+
+        # Delete database
+        os.remove(cls.db_path)
 
 
 if __name__ == '__main__':
